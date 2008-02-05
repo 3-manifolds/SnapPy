80dfa0f5fdd6f250d4d7997b1548f61188e822db
Marc Culler <culler@users.noreply.github.com>
2008-02-04 22:51:49 -0600

    renamed files to SnapPy.XXX

--- setup.py
+++ setup.py
@@ -15,12 +15,13 @@ code  =  base_code + unix_code + addl_code
 
 # for debugging add 'extra_compile_args = ["-g"]' to the below 
 
-SnapPeaCy = Extension("SnapPeaCy",  ["SnapPeaCy.pyx"] + code, 
-                      include_dirs = ["headers", "unix_kit", "pari/include/pari"],
-                      extra_objects = ['pari/lib/libpari.a'])
+SnapPy = Extension(name = "SnapPy",
+                   sources = ["SnapPy.pyx"] + code, 
+                   include_dirs = ["headers", "unix_kit", "pari/include/pari"],
+                   extra_objects = ['pari/lib/libpari.a'])
 
-setup( name = "test",
-       ext_modules = [SnapPeaCy],
+setup( name = "SnapPy",
+       ext_modules = [SnapPy],
        cmdclass = {'build_ext': build_ext}
        )
 

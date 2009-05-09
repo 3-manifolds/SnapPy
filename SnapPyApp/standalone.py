d3b4549525499db1aca14bf1623dbcefc81ec3e2
Nathan Dunfield <nathan@dunfield.info>
2009-05-09 00:41:25 -0500

    Fixed issue with standalone build

--- SnapPyApp/standalone.py
+++ SnapPyApp/standalone.py
@@ -5,14 +5,24 @@ Usage:
     python setup.py py2app
 """
 
-from setuptools import setup
+from setuptools import setup, Command
+import os
+
+class clean(Command):
+    user_options = []
+    def initialize_options(self):
+        pass 
+    def finalize_options(self):
+        pass
+    def run(self):
+        os.system("rm -rf build dist *.pyc")
 
 APP = ['SnapPython.py']
 DATA_FILES = []
 OPTIONS = {'argv_emulation': True,
  'excludes': 'scipy,numpy',
- 'packages': 'SnapPy',
- 'includes': 'IPython.Extensions.ipy_profile_none,gzip,tarfile'
+ 'packages': 'SnapPy,IPython',
+ 'includes': 'gzip,tarfile,readline',
 }
 
 setup(
@@ -20,4 +30,5 @@ setup(
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
+    cmdclass   = {'clean' : clean},
 )

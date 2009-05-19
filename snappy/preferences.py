f9d92b27f4c07f6e911277c2b53b8bb3f0cf83de
culler <culler@marc.local>
2009-05-19 12:06:59 -0500

    Preferences dialog has a font sample.

--- snappy/preferences.py
+++ snappy/preferences.py
@@ -4,11 +4,13 @@ import tkFont
 from string import ascii_letters
 
 class PreferenceDialog(tkSimpleDialog.Dialog):
-    def __init__(self, parent, title='Preferences'):
+    def __init__(self, parent, title='Preferences',
+                 prefs={'font': ('Monaco', 18, 'normal')}):
         Tk_.Toplevel.__init__(self, parent)
         self.title(title)
         self.parent = parent
         self.result = None
+        self.prefs = prefs
         self.navbar()
         self.body_frame = body_frame = Tk_.Frame(self)
         self.initial_focus = self.font_panel(body_frame)
@@ -46,18 +48,43 @@ class PreferenceDialog(tkSimpleDialog.Dialog):
         self.bind("<Escape>", self.cancel)
         box.pack(side=Tk_.BOTTOM)
 
-    def font_panel(self, bodyframe):
-        self.font_list = font_list = Tk_.Listbox(bodyframe)
-        fonts = [x for x in tkFont.families() if x[0] in ascii_letters]
-        fonts.sort()
-        for font in fonts:
-            font_list.insert(Tk_.END, font)
-        self.font_scroller = font_scroller = Tk_.Scrollbar(bodyframe,
+    def font_panel(self, body_frame):
+        self.list_frame = list_frame = Tk_.Frame(body_frame)
+        self.font_list = font_list = Tk_.Listbox(list_frame,
+                                                 selectmode=Tk_.SINGLE)
+        self.families = families = [x for x in tkFont.families()
+                                    if x[0] in ascii_letters]
+        families.sort()
+        for family in families:
+            font_list.insert(Tk_.END, family)
+        self.font_scroller = font_scroller = Tk_.Scrollbar(list_frame,
                                                            command=font_list.yview)
         font_list.config(yscrollcommand=font_scroller.set)
         font_list.grid(row=0, column=0)
         self.font_scroller.grid(row=0, column=1, sticky=Tk_.N + Tk_.S)
+        list_frame.grid(row=0, column=0)
+        self.sample = sample = Tk_.Text(self.body_frame,
+                                        width=50, height=4,
+                                        highlightthickness=0,
+                                        font=self.prefs['font'])
+        self.sample.bind('<Button-1>', lambda event: 'break')
+        self.sample.insert(Tk_.INSERT, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ\n'\
+                                           'abcdefghijklmnopqrstuvwxyz')
+        sample.tag_add('all', '1.0', Tk_.END)
+        sample.tag_config('all', justify=Tk_.CENTER,
+                          font=self.prefs['font'])
+        sample.grid(row=1, column=0, pady=10)
+        font_list.bind('<ButtonRelease-1>', self.set_font_sample)
         return font_list
+
+    def set_font_sample(self, event):
+        index = self.font_list.curselection()[0]
+        family = self.families[int(index)]
+        size = 18
+        weight = 'normal'
+        self.sample.tag_config('all', justify=Tk_.CENTER,
+                               font=(family, size, weight)) 
+        
 
 if __name__ == '__main__':
     parent = Tk_.Tk()

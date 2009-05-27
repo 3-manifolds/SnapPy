528994438bc3d0dd2423e863dc025868ce19a849
culler <culler@threlfall>
2009-05-27 16:04:28 -0500

    SnapPy/darwin-tk8.4 -> snappy/darwin-tk8.4 in build_togl.sh

--- build_togl.sh
+++ build_togl.sh
@@ -2,7 +2,7 @@
 wget -nd http://downloads.sourceforge.net/togl/Togl2.0-src.tar.gz
 tar xvfz Togl2.0-src.tar.gz 
 cd Togl2.0
-export SNAPPY_INSTALL=`pwd`/../SnapPy/darwin-tk8.4
+export SNAPPY_INSTALL=`pwd`/../snappy/darwin-tk8.4
 ./configure --prefix=$SNAPPY_INSTALL --libdir=$SNAPPY_INSTALL
 make CFLAGS='-arch ppc -arch i386' \
 CPPFLAGS='-arch ppc -arch i386 \

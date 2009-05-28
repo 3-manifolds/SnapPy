61c908f8b640d2b8d3934e04c60ba4009e18e39c
Nathan Dunfield <nathan@dunfield.info>
2009-05-28 11:44:24 -0500

    Made Mac .app slighly smaller

--- SnapPyApp/make.py
+++ SnapPyApp/make.py
@@ -17,6 +17,11 @@ os.system("python setup.py install")
 os.chdir("SnapPyApp")
 os.system("python setup.py clean py2app")
 
+# Make things a little smaller.
+
+os.system("rm -rf dist/SnapPy.app/Contents/Frameworks/Tcl.framework/Versions/8.4/Resources/English.lproj/ActiveTcl-8.4")
+os.system("rm -rf dist/SnapPy.app/Contents/Frameworks/Tk.framework/Versions/8.4/Resources/Scripts/demos")
+
 # Then the disk image file.  
 
 os.chdir("dmg-maker")

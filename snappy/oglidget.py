ec9755a803730945e20f7f6a871fb380eebae454
Marc Culler <culler@users.noreply.github.com>
2009-05-30 22:16:16 -0500

    Added "Save As" method.  The session is saved as a python script with results as comments.

--- snappy/oglidget.py
+++ snappy/oglidget.py
@@ -9,7 +9,7 @@ import os, sys
 
 def glTranslateScene(s, x, y, mousex, mousey):
         glMatrixMode(GL_MODELVIEW)
-        mat = glGetDoublev(GL_MODELVIEW_MATRIX)
+        mat = glGetFloatv(GL_MODELVIEW_MATRIX)
         glLoadIdentity()
         glTranslatef(s * (x - mousex), s * (mousey - y), 0.0)
         glMultMatrixd(mat)
@@ -17,7 +17,7 @@ def glTranslateScene(s, x, y, mousex, mousey):
 
 def glRotateScene(s, xcenter, ycenter, zcenter, x, y, mousex, mousey):
         glMatrixMode(GL_MODELVIEW)
-        mat = glGetDoublev(GL_MODELVIEW_MATRIX)
+        mat = glGetFloatv(GL_MODELVIEW_MATRIX)
         glLoadIdentity()
         glTranslatef(xcenter, ycenter, zcenter)
         glRotatef(s * (y - mousey), 1., 0., 0.)

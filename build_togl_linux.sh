36dff931f2c26e4ba41de6228df86974fc059138
Nathan Dunfield <nathan@dunfield.info>
2009-06-09 22:23:08 -0500

    worked on installation docs; simplified build_togl.sh

--- build_togl_linux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#wget -nd http://downloads.sourceforge.net/togl/Togl2.0-src.tar.gz
-tar xvfz Togl2.0-src.tar.gz 
-cd Togl2.0
-export SNAPPY_INSTALL=`pwd`/../snappy/linux2-tk8.5
-./configure --prefix=$SNAPPY_INSTALL --libdir=$SNAPPY_INSTALL --with-tcl=/usr/share/tcltk/tcl8.5/ --with-tk=/usr/share/tcltk/tk8.5/ 
-make
-make install-lib-binaries

ecbb82d66b02bc9103ecb6d3fbfdcc155c33927e
Marc Culler <culler@math.uic.edu>
2010-05-09 14:32:22 -0500

    Changed UCS_hack to do nothing.  If this gets called it is before the module has been fully imported, so no exceptions are available and trying to raise an exception causes a segfault.  By some miracle, this seems to work fine, even when the function does get called.

--- CyOpenGL.pyx
+++ CyOpenGL.pyx
@@ -1,12 +1,9 @@
 include "CyOpenGL.pxi"
 include "CyOpenGLU.pxi"
 
-# This is part of the UCS2 hack.  Of course, for this to work this
-# file should not contain any unicode strings.
-
+# This is part of the UCS2 hack.
 cdef public UCS2_hack (char *string, Py_ssize_t length, char *errors) :   
-    raise RuntimeError, """
-Don't use unicode strings in SnapPy Cython files!"""
+    pass
 
 import Tkinter as Tk_
 import os, sys, platform, tkMessageBox
--- SnapPy.pyx
+++ SnapPy.pyx
@@ -4,12 +4,9 @@ from manifolds import __path__ as manifold_paths
 
 include "SnapPy.pxi"
 
-# This is part of the UCS2 hack.  Of course, for this to work this
-# file should not contain any unicode strings.
-
+# This is part of the UCS2 hack.
 cdef public UCS2_hack (char *string, Py_ssize_t length, char *errors) :   
-    raise RuntimeError, """
-Don't use unicode strings in SnapPy Cython files!"""
+   pass 
 
 # A stream for asynchronous messages
 class MsgIO:

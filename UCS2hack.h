841f202b1a111c571dd82dc659d4bc2827919c77
Nathan Dunfield <nathan@dunfield.info>
2010-05-09 18:32:04 -0500

    Made UCS2 hack only operative on Linux

--- UCS2hack.h
+++ UCS2hack.h
@@ -3,5 +3,7 @@ This hack avoids errors when importing Cython modules which were compiled on
 a system with a different size for unicode characters.  This will have to
 be revisited when we switch to Python 3.
 */
-#define PyUnicode_DecodeUTF8 UCS2_hack
 
+#ifdef __linux__
+#define PyUnicode_DecodeUTF8 UCS2_hack
+#endif

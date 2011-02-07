4514b39f0042367878bc5017d6919a6169b067bc
Marc Culler <culler@math.uic.edu>
2011-02-07 15:42:42 -0600

    Added a declaration for string_triangulation and removed unneeded casts.

--- unix_kit/unix_file_io.h
+++ unix_kit/unix_file_io.h
@@ -10,3 +10,4 @@
 extern Triangulation    *read_triangulation(char *file_name);
 extern Triangulation    *read_triangulation_from_string(char *file_data);
 extern void             write_triangulation(Triangulation *manifold, char *file_name);
+extern char             *string_triangulation(Triangulation *manifold);

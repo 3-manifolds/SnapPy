58eab3aeea94af9742d950fdfddd65ccae8eddf8
Marc Culler <culler@users.noreply.github.com>
2011-02-11 08:28:08 -0600

    Added a 1 second timeout to the HTTPConnection.

--- snappy/phone_home.py
+++ snappy/phone_home.py
@@ -2,7 +2,7 @@ from httplib import HTTPConnection
 
 def get_current():
     try:
-        connection = HTTPConnection('www.math.uic.edu')
+        connection = HTTPConnection('www.math.uic.edu', timeout=1)
         connection.request('GET','/t3m/SnapPy-nest/current.txt')
         response = connection.getresponse()
         result = response.read()

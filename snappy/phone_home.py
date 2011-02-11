e09ffb2f0fcdb31c6ce0e63a769b2d5c402afca1
Nathan Dunfield <nathan@dunfield.info>
2011-02-11 08:50:20 -0600

    removed need for current.txt

--- snappy/phone_home.py
+++ snappy/phone_home.py
@@ -1,15 +1,17 @@
 from httplib import HTTPConnection
+import re
 
 def get_current():
     try:
         connection = HTTPConnection('www.math.uic.edu', timeout=1)
-        connection.request('GET','/t3m/SnapPy-nest/current.txt')
+        connection.request('GET','/t3m/SnapPy/index.html')
         response = connection.getresponse()
         result = response.read()
         connection.close()
+        version = re.search("SnapPy v([0-9.]*) documentation", result).group(1)
     except:
         return None
-    return result.strip()
+    return version 
 
 if __name__ == '__main__':
     current = get_current()

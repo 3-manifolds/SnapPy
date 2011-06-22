a9494c245c741d3898878e4d1d3c8972edaf29a2
Nathan Dunfield <nathan@dunfield.info>
2011-06-22 07:41:11 -0500

    more work on twister

--- Twister/global.cpp
+++ Twister/global.cpp
@@ -1,31 +1,80 @@
 
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
+
+#include "global.h"
 
 // Global options (and their defaults).
 bool GLOBAL_debugging = false;
 bool GLOBAL_warnings = true;
 bool GLOBAL_optimise = true;
 bool GLOBAL_calculate_peripheral_curves = true;
+std::string GLOBAL_message_stream = "";
+
+void set_globals_to_defaults()
+{
+	// Reset the global options to their defaults.
+	GLOBAL_debugging = false;
+	GLOBAL_warnings = true;
+	GLOBAL_optimise = true;
+	GLOBAL_calculate_peripheral_curves = true;
+	GLOBAL_message_stream = "";
+}
+
 
 // Global output management.
 void output_debugging(std::string strn)
 {
-	if (GLOBAL_debugging) std::cerr << strn << " " << std::flush;
+	if (GLOBAL_debugging)  // std::cerr << strn << " " << std::flush;
+	{
+		if (GLOBAL_message_stream != "")
+		{
+			std::ofstream myfile(GLOBAL_message_stream.c_str(), std::ios::app);
+			if (not myfile.is_open()) exit(1);
+			myfile << strn << " " << std::flush;
+			myfile.close();
+		}
+		else
+			std::cerr << strn << " " << std::flush;
+	}
+	
 	return;
 }
 
 void output_warning(std::string strn)
 {
-	if (GLOBAL_warnings) std::cerr << "Warning: " << strn << std::endl;
+	if (GLOBAL_warnings)  // std::cerr << "Warning: " << strn << std::endl;
+	{
+		if (GLOBAL_message_stream != "")
+		{
+			std::ofstream myfile(GLOBAL_message_stream.c_str(), std::ios::app);
+			if (not myfile.is_open()) exit(1);
+			myfile << "Warning: " << strn << std::endl;
+			myfile.close();
+		}
+		else
+			std::cerr << "Warning: " << strn << std::endl;
+	}
 	return;
 }
 
 void output_error(std::string strn)
 {
 	// Errors are never optional.
-	std::cerr << "Error: " << strn << std::endl;
+	// std::cerr << "Error: " << strn << std::endl;
+	
+	if (GLOBAL_message_stream != "")
+	{
+		std::ofstream myfile(GLOBAL_message_stream.c_str(), std::ios::app);
+		if (not myfile.is_open()) exit(1);
+		myfile << "Error: " << strn << std::endl;
+		myfile.close();
+	}
+	else
+		std::cerr << "Error: " << strn << std::endl;
+	
 	exit(1);
 }
 

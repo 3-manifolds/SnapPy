1d1a55f4dc34271c2d0f416897cf79b2111b1ce9
Marc Culler <culler@users.noreply.github.com>
2012-01-19 23:39:21 -0600

    Made build_pari.sh build for x86_64, tinkered with history.

--- build_pari.sh
+++ build_pari.sh
@@ -13,31 +13,48 @@
 # also work around the missing symbols (esp. time and localtime) that
 # are listed in libmsvcr90.a but are not exported from msvcr90.dll .
 
-
 if [ ! -e pari-2.3.4.tar.gz ]; then
     echo "Downloading Pari 2.3.4..."
     curl --remote-name http://pari.math.u-bordeaux.fr/pub/pari/unix/pari-2.3.4.tar.gz
 fi
-echo "Untaring Pari..."
+echo "Untarring Pari..."
 tar xzf pari-2.3.4.tar.gz
 cd pari-2.3.4
 
 echo "Building Pari libary..." 
 if [ "$(uname)" = "Darwin" ] ; then  # OS X
-    export CFLAGS='-arch i386 -mmacosx-version-min=10.4 '
+# 
+   export CFLAGS='-arch ppc -mmacosx-version-min=10.4 '
     ./Configure --prefix=`pwd` --host=ppc-darwin
     cd Odarwin-ppc
-    make CFLAGS='-arch ppc -mmacosx-version-min=10.4 ' install-lib-sta
-    mv ../lib/libpari.a ../lib/ppc-libpari.a
+    make install-lib-sta
     cd ..
+    mv lib/libpari.a lib/ppc-libpari.a
+#
     export CFLAGS='-arch i386 -mmacosx-version-min=10.4 '
     ./Configure --prefix=`pwd` --host=i386-darwin
     cd Odarwin-i386
-    make CFLAGS='-arch i386 -mmacosx-version-min=10.4' install-lib-sta
-    mv ../lib/libpari.a ../lib/i386-libpari.a
-    lipo ../lib/ppc-libpari.a ../lib/i386-libpari.a -create -output ../lib/libpari.a
-    ranlib ../lib/*.a
+    make install-lib-sta
     make install-include
+    cd ..
+    mv lib/libpari.a lib/i386-libpari.a
+    mv include include32
+#
+    export CFLAGS='-arch x86_64'
+    ./Configure --prefix=`pwd` --host=x86_64-darwin
+    cd Odarwin-x86_64
+    make install-lib-sta
+    make install-include
+    cd ..
+    mv lib/libpari.a lib/x86_64-libpari.a
+    mv include include64
+#
+    lipo lib/ppc-libpari.a lib/i386-libpari.a lib/x86_64-libpari.a -create -output lib/libpari.a
+    ranlib lib/*.a
+    ln -s include32 include
+    echo "******************* NOTICE! *********************"
+    echo "Pari is configured to build CyPari for 32-bit architectures."
+    echo "Change the symlink pari-2.3.4/include if you want to build for x86_64."
 elif [ "$(uname)" = "MINGW32_NT-6.1" ] ; then # MinGW on Windows
     patch -p1 < ../mingw-pari.patch
     ./Configure --prefix=`pwd` --host=i386-mingw

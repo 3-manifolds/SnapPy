1dd2fe7893ca74f6d4d4303a1e5b4dba18d15288
Marc Culler <culler@math.uic.edu>
2012-01-22 11:40:10 -0600

    Gave up on png files for PhotoImage - some Tk 8.6's don't like it. Fought with Tk about how set the background color for infodialog.

--- snappy/infodialog.py
+++ snappy/infodialog.py
@@ -1,47 +1,56 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-try:
+try:  # Python 2
     import Tkinter as Tk_
     from tkSimpleDialog import Dialog
-    import ttk
 except ImportError: # Python 3
     import tkinter as Tk_
     from tkinter.simpledialog import Dialog
-    from tkinter import ttk
+
+# Try to find a native button
+Button = Tk_.Button # fallback
+
+try:
+    from ttk import Button  # Python 2 with ttk
+except ImportError:
+    pass
+
+try:
+    from tkinter.ttk import Button  # Python 3 with ttk
+except ImportError:
+    pass
+
 import sys, os
 from snappy import __file__ as snappy_file
 snappy_path = os.path.dirname(snappy_file)
-# Once we switch to Tk 8.6, get rid of this.
-if Tk_.TkVersion < 8.6:
-    icon_file = os.path.join(snappy_path, 'info_icon.gif')
-else:
-    icon_file = os.path.join(snappy_path, 'info_icon.png')
-
-# We need to figure how to set the background for Windows and linux
-if sys.platform =='darwin':
-    background = 'systemButtonFace'
-else:
-    background = 'lightgray'
+icon_file = os.path.join(snappy_path, 'info_icon.gif')
 
 class InfoDialog(Dialog):
     def __init__(self, master, title='', content=''):
         self.content = content
+        # We need to figure how to set the background for Windows
+        if sys.platform == 'darwin':
+            self.bg = 'systemButtonFace'
+        elif sys.platform == 'linux2':
+            self.bg = Button(master).cget('background')
+        else:
+            self.bg = 'lightgray'
         self.image = Tk_.PhotoImage(file=icon_file)
         Dialog.__init__(self, master, title=title)
         
     def body(self, master):
-        self.config(bg=background)
+        self.config(bg=self.bg)
         self.resizable(False, False)
-        box = Tk_.Frame(self, bg=background)
-        icon = Tk_.Label(box, image=self.image, bg=background)
+        box = Tk_.Frame(self, bg=self.bg)
+        icon = Tk_.Label(box, image=self.image, bg=self.bg)
         icon.pack(side=Tk_.LEFT, pady=30, anchor=Tk_.N)
-        message = Tk_.Message(box, text=self.content, bg=background)
+        message = Tk_.Message(box, text=self.content, bg=self.bg)
         message.pack(side=Tk_.LEFT, padx=20, pady=10)
         box.pack()
 
     def buttonbox(self):
-        box = Tk_.Frame(self, bg=background)
-        button = ttk.Button(box, text="OK", width=5,
+        box = Tk_.Frame(self, bg=self.bg)
+        button = Button(box, text="OK", width=5,
                        command=self.ok, default=Tk_.ACTIVE)
         button.pack(side=Tk_.RIGHT, ipadx=10, padx=20, pady=20)
         self.bind("<Return>", self.ok)

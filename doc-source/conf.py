4d743907c6dcb2bfdefd18d4f5c6c676fa910afc
Nathan Dunfield <nathan@dunfield.info>
2012-02-20 11:24:03 -0600

    Version 1.5.0

--- doc-source/conf.py
+++ doc-source/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'SnapPy'
-copyright = u'2009-2012, by Marc Culler, Nathan Dunfield, Jeffrey Weeks and others.'
+copyright = u'2009-2012, by Marc Culler, Nathan Dunfield, Jeffrey Weeks and others'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

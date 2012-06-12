bfa8709e344a0a83d94f61386a32ee4f231f5c45
Nathan Dunfield <nathan@dunfield.info>
2012-06-12 07:39:45 -0500

    snappari working now!

--- dev/snappari.py
+++ dev/snappari.py
@@ -1,22 +1,22 @@
+from __future__ import print_function
 import pari.gen as pari
 import snappy
-
-def pari_matrix(list_of_lists):
-    return  pari.pari( '[' + '; '.join([repr(L)[1:-1] for L in list_of_lists]) + ']' )
 
 def pari_matrix(A):
     return pari.pari.matrix( len(A), len(A[0]), [ pari.pari(x) for x in sum(A, []) ] )
 
-def pari_vector(v):
-    return pari.pari.matrix(len(v), 1, v)
+def pari_row_vector(v):
+    return pari.pari(v).Vec()
+
+def pari_column_vector(v):
+    return pari.pari(v).Col()
 
 def pari_vector_to_list(v):
-    return v[0].list()
+    return v.Vec().python_list()
 
-    
-def pari_int_matrix_to_lists(A):
+def pari_matrix_to_lists(A):
     'Return the entries of A in *column major* order'
-    return [pari.vecsmall_to_intlist(v.Vecsmall()) for v in A.list()]
+    return [pari_vector_to_list(v) for v in A.list()]
 
 def eval_gluing_equation(eqn, shapes):
     if isinstance(shapes, pari.gen):
@@ -49,7 +49,7 @@ def enough_gluing_equations(manifold):
     edge_eqns_with_RHS = pari_matrix( [a + b + [(1-c)//2] for a,b,c in eqns[:n_tet]] )
     H, U = edge_eqns.mattranspose().mathnf(flag=1)
     assert H.ncols() == n_tet - n_cusps
-    edge_eqns_with_RHS = pari_int_matrix_to_lists((edge_eqns_with_RHS.mattranspose() * U))[n_cusps:]
+    edge_eqns_with_RHS = pari_matrix_to_lists((edge_eqns_with_RHS.mattranspose() * U))[n_cusps:]
     edge_eqns_with_RHS = [ (e[:n_tet], e[n_tet:2*n_tet], (-1)**e[-1]) for e in edge_eqns_with_RHS]
 
     cusp_eqns = []
@@ -64,33 +64,35 @@ def enough_gluing_equations(manifold):
     assert len(ans_eqns) == n_tet and len(ans_matrix.mattranspose().matkerint()) == 0
     return ans_eqns
 
-def float_to_pari(x, bits_prec):
-    return pari.pari(x).precision(pari.prec_bits_to_dec(bits_prec))
+def float_to_pari(x, dec_prec):
+    return pari.pari(x).precision(dec_prec)
 
-def complex_to_pari(z, bits_prec):
-    return pari.pari.complex( float_to_pari(z.real, bits_prec), float_to_pari(z.imag, bits_prec) )
+def complex_to_pari(z, dec_prec):
+    return pari.pari.complex( float_to_pari(z.real, dec_prec), float_to_pari(z.imag, dec_prec) )
 
-def polished_tetrahedra_shapes(manifold, bits_prec = 200, ignore_solution_type=False):
+def polished_tetrahedra_shapes(manifold, dec_prec=50, bits_prec=None, ignore_solution_type=False):
     """
     Refines the current solution to the gluing equations to one with
-    'bits_prec' accuracy.
+    the specified accuracy.  
     """
-    pari.pari.set_real_precision(pari.prec_bits_to_dec(bits_prec))
-    
-    target_espilon = float_to_pari(2.0, bits_prec + 10)**-bits_prec
+    if dec_prec is None:
+        dec_prec = pari.prec_bits_to_dec(bits_prec)
+    working_prec = dec_prec + 10
+    target_espilon = float_to_pari(10.0, working_prec)**-dec_prec
     
     # This is a potentially long calculation, so we cache the result
-    # if "polished_shapes" in manifold._cache.keys():
-    #   curr_sol = manifold._cache["polished_shapes"]
-    #   if curr_sol.base_ring().precision() >= CC.precision():
-    #       return curr_sol.change_ring(CC)
+
+    if "polished_shapes" in manifold._cache.keys():
+        curr_sol = manifold._cache["polished_shapes"]
+        if curr_sol[0].precision() >= pari.prec_dec_to_words(dec_prec):
+            return [s.precision(dec_prec) for s in curr_sol]
 
     # Check and make sure initial solution is reasonable
 
     if not ignore_solution_type and not manifold.solution_type() in ['all tetrahedra positively oriented' , 'contains negatively oriented tetrahedra']:
         raise ValueError, 'Initial solution to gluing equations has flat or degenerate tetrahedra'
 
-    init_shapes = pari_vector( [complex_to_pari(z, bits_prec + 10) for z in manifold.tetrahedra_shapes('rect')] )
+    init_shapes = pari_column_vector( [complex_to_pari(z, working_prec) for z in manifold.tetrahedra_shapes('rect')] )
     init_equations = manifold.gluing_equations('rect')
     if gluing_equation_error(init_equations, init_shapes) > pari.pari(0.000001):
         raise ValueError, 'Initial solution not very good'
@@ -103,29 +105,36 @@ def polished_tetrahedra_shapes(manifold, bits_prec = 200, ignore_solution_type=F
         if infinity_norm(errors) < target_espilon:
             break
         derivative = pari_matrix( [ [  eqn[0][i]/z  - eqn[1][i]/(1 - z)  for i, z in enumerate(pari_vector_to_list(shapes))] for eqn in eqns] )
-        gauss = derivative.matsolve(pari_vector(errors))
+        gauss = derivative.matsolve(pari_column_vector(errors))
         shapes = shapes - gauss
 
     # Check to make sure things worked out ok.
     error = gluing_equation_error(init_equations, shapes)
     total_change = infinity_norm(init_shapes - shapes)
     if error > 1000*target_espilon or total_change > pari.pari(0.0000001):
-        raise ValueError, "Didn't find a good solution to the gluing equations"
+        raise ValueError('Did not find a good solution to the gluing equations')
 
 
-    #shapes = shapes.change_ring(ComplexField(bits_prec))
-    # manifold._cache["polished_shapes"] = shapes
+    manifold._cache["polished_shapes"] = shapes
     return pari_vector_to_list(shapes)
 
 
-def test_polished():
-    def test(manifold):
+def test_polished(dec_prec=200):
+    def test_manifold(manifold):
         eqns = manifold.gluing_equations('rect')
-        shapes = polished_tetrahedra_shapes(manifold, 1000)
+        shapes = polished_tetrahedra_shapes(manifold, dec_prec)
         return gluing_equation_error(eqns, shapes)
-    
-    print max([test(M) for M in snappy.OrientableCuspedCensus(filter='cusps>1')])
-    print max([test(M) for M in snappy.OrientableClosedCensus()[:1000]])
-    print max([test(M) for M in snappy.LinkExteriors(num_cusps=4) if M.volume() > 3])
 
-#test_polished()    
+    def test_census(name, census):
+        manifolds = [M for M in census]
+        print('Checking gluing equations for %d %s manifolds' % (len(manifolds), name))
+        max_error = pari.pari(0)
+        for i, M in enumerate(manifolds):
+            max_error = max(max_error, test_manifold(M))
+            print('\r   ' + repr( (i, M) ).ljust(35) + '   Max error so far: ' + repr(max_error), end = '')
+        print()
+              
+    test_census('cusped census', snappy.OrientableCuspedCensus(filter='cusps>1'))
+    test_census('closed census', snappy.OrientableClosedCensus()[:1000])
+    test_census('4-component links', [M for M in snappy.LinkExteriors(num_cusps=4) if M.solution_type() == 'all tetrahedra positively oriented'])
+

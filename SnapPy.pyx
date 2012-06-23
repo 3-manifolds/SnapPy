e828835ef77fd85cf63ca382a122a88a843dcb0e
Marc Culler <culler@math.uic.edu>
2012-06-23 10:12:31 -0500

    Finally figured out how to keep the UI responding and make SnapPy interruptible during long SnapPea computations, without fancy signals or other things that cause segfaults.

--- SnapPy.pyx
+++ SnapPy.pyx
@@ -1,6 +1,6 @@
 import os, sys, operator, types, re, gzip, struct, tempfile
 import tarfile, atexit, math, string
-import signal as pysignal
+from signal import signal, SIGINT, default_int_handler
 from manifolds import __path__ as manifold_paths
 import database
 
@@ -181,42 +181,22 @@ cdef public void uFatalError(char *function, char *file) except *:
     raise SnapPeaFatalError('SnapPea crashed in function %s(), '
                             'defined in %s.c.'%(function, file))
 
-# Signal handling
-# Note: Python's signal module only calls signal handlers
-# between atomic interpreter operations.  So to make C code
-# interruptible we have to use signal.h
-
 # global variables for interrupt processing 
 cdef public Boolean gLongComputationInProgress
 cdef public Boolean gLongComputationCancelled
-cdef sig_t old_sigint_handler
-cdef sig_t old_sigalrm_handler
 
-cdef void SnapPea_sigint_handler(int signum):
+# If not None, this will be called in gLongComputationContinues.
+# This enables a GUI to do updates during long computations.
+UI_callback = None
+
+#cdef void SnapPea_sigint_handler(int signum, frame=None):
+def SnapPea_sigint_handler(int signum, frame=None):
     """
     A SIGINT handler which cancels the SnapPea computation.
     """
     global gLongComputationCancelled
-    global old_sigint_handler, old_sigalrm_handler
     gLongComputationCancelled = True
-    signal(SIGINT, old_sigint_handler)
-    old_sig_handler = NULL
-#    signal(SIGALRM, old_sigalrm_handler)
-#    old_sigalrm_handler = NULL
     sys.stderr.write('\nSnapPea computation aborted!\n')
-
-# A python callback for the ALRM handler
-def snappy_alarm_handler():
-    sys.stderr.write('tock ')
-
-cdef void SnapPea_sigalrm_handler(int signum):
-    """
-    A SIGALRM handler which calls a python callback.
-    Can be used to update a GUI during a long computation,
-    if an interval timer is set up to send ALRM signals.
-    """
-    global snappy_alarm_handler
-    snappy_alarm_handler()
 
 def SnapPea_interrupt():
     """
@@ -234,9 +214,8 @@ cdef public void uLongComputationBegins(char *message, Boolean is_abortable):
     # Set SnapPea's flags
     gLongComputationCancelled = False
     gLongComputationInProgress = True
-    # Install the SnapPea handlers on SIGINT and SIGALRM
-    old_sigint_handler = signal(SIGINT, SnapPea_sigint_handler)
-#    old_sigalrm_handler = signal(SIGALRM, SnapPea_sigalrm_handler)
+    # Install our sigint handler
+    signal(SIGINT, SnapPea_sigint_handler)
 
 cdef public c_FuncResult uLongComputationContinues() except *:
     global gLongComputationCancelled
@@ -244,21 +223,18 @@ cdef public c_FuncResult uLongComputationContinues() except *:
     if gLongComputationCancelled:
         gLongComputationCancelled = False
         gLongComputationInProgress = False
-        MakingPendingCalls = False
         return func_cancelled
     else:
+        if UI_callback is not None:
+            # Let the GUI update itself
+            UI_callback()
         return func_OK
 
 cdef public void uLongComputationEnds():
     global gLongComputationCancelled
     global gLongComputationInProgress
-    global old_sigint_handler, old_sigalrm_handler
-    # Restore signal handlers on SIGINT and SIGALRM if they
-    # weren't already reset.
-    if old_sigint_handler != NULL:
-        signal(SIGINT, old_sigint_handler)
-#    if old_sigalrm_handler != NULL:
-#        signal(SIGALRM, old_sigalrm_handler)
+    # Restore the Python sigint handler
+    signal(SIGINT, default_int_handler)
     # Reset SnapPea's flags
     gLongComputationCancelled = False
     gLongComputationInProgress = False
@@ -295,11 +271,9 @@ def smith_form(M):
         if not isinstance(M, matrix):
             M = matrix(M)
         m, n = M.shape
-###        result = CyPari.smith_form(M)
         result = [int(x) for x in pari.matrix(m, n, M.entries()).matsnf()]
 
     # PARI views the input to matsnf0 as square.
-### Still needed?
     if m < n:
         result = result + [0]*(n-m)
     if m > n:
@@ -329,7 +303,6 @@ SolutionType = ['not attempted', 'all tetrahedra positively oriented',
 # global functions
 def check_SnapPea_memory():
     verify_my_malloc_usage()
-
 
 # convert and free an integer matrix from C
 
@@ -380,8 +353,6 @@ class NeumannZagierTypeEquations(object):
                               '\n  '.join(repr(self.matrix).split('\n')),
                               format_explain_list(self.explain_columns),
                               format_explain_list(self.explain_rows))
-
-    
 
 # Derivatives of basic classes; when these are called as a function
 # they return self.  This means that they can be accessed either as

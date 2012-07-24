8c501155caf81ebf8ba29133cc211dc6a16a8ba8
Nathan Dunfield <nathan@dunfield.info>
2012-07-23 23:03:22 -0500

    make release_simple.py work on Windows

--- release_tools/release_simple.py
+++ release_tools/release_simple.py
@@ -5,8 +5,7 @@ General script for building binary eggs for a t3m Python module.
 import os, sys, re, glob, subprocess, time
 
 def run(command):
-    print( subprocess.Popen(command.split(),
-                     stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read() )
+    os.system(command)
 
 
 # Update (or get) the module source code from the repository
@@ -22,6 +21,8 @@ run('hg up')
 
 # Build and install the module into the Python that called this script
 
+if sys.platform.startswith('win'):
+    run( sys.executable + ' setup.py build -c mingw32')
 run( sys.executable + ' setup.py install' )
 
 # For OS X, we need fake PPC/intel "fat" eggs.  
@@ -35,6 +36,8 @@ for file in glob.glob("dist/*-intel.egg"):
 cut_time = time.time() - 24*60*60
 eggs = [egg for egg in glob.glob('dist/*.egg') if os.path.getmtime(egg) > cut_time]
 
+if sys.platform.startswith('win'):
+    eggs = [e.replace('\\', '/') for e in eggs]
 
 try: input = raw_input
 except: pass

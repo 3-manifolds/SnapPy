0a3c6f455044a6a2bcd87c9a96f1871335a1d930
Nathan Dunfield <nathan@dunfield.info>
2012-07-24 20:27:05 -0500

    windows fix to test_eggs.py

--- release_tools/test_eggs.py
+++ release_tools/test_eggs.py
@@ -13,8 +13,7 @@ else:
 os.system('rm -rf ' + vir_env_dir)
 #Note:  --no-site-packages  is implicit here.
 os.system(sys.executable + ' virtualenv.py --distribute ' + vir_env_dir)
-mypy = vir_env_dir + '/bin/python'
-
+mypy = vir_env_dir + os.sep + 'bin' + os.sep + 'python'
 def test_module(mod_name, test_code, install=True):
     print( '\n\n' + 30*'*' + '\n* ' + mod_name + '\n' + 30*'*' + '\n')
     if install:

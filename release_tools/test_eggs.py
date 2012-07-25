784bfa1170caa07998ee702d044df227078cb2d1
Nathan Dunfield <nathan@dunfield.info>
2012-07-24 20:23:57 -0500

    windows fix to test_eggs.py

--- release_tools/test_eggs.py
+++ release_tools/test_eggs.py
@@ -4,10 +4,15 @@ Build a temporary virtual environment and install SnapPy into it.
 
 import os, sys, re
 
-# Create virtual environment. 
-vir_env_dir = '/tmp/SnapPyTestVE'
+# Create virtual environment.
+
+if sys.platform.startswith('win'):
+    vir_env_dir = 'c:\SnapPyTestVE'
+else: 
+    vir_env_dir = '/tmp/SnapPyTestVE'
 os.system('rm -rf ' + vir_env_dir)
-os.system(sys.executable + ' virtualenv.py --distribute --no-site-packages ' + vir_env_dir)
+#Note:  --no-site-packages  is implicit here.
+os.system(sys.executable + ' virtualenv.py --distribute ' + vir_env_dir)
 mypy = vir_env_dir + '/bin/python'
 
 def test_module(mod_name, test_code, install=True):
@@ -16,7 +21,7 @@ def test_module(mod_name, test_code, install=True):
         os.system(mypy + ' -m easy_install -f http://snappy.computop.org/get ' + mod_name)
     os.system(mypy + ' ' + test_code) 
 
-os.chdir('/tmp')
+os.chdir(vir_env_dir)
 test_module('SnapPy', '-m snappy.test')
 test_module('CyPari', '-m cypari.test')
 test_module('Spherogram', '-m spherogram.links.test')

3718ed096c8d782fddc3790227ebda4d6922d936
Nathan Dunfield <nathan@dunfield.info>
2012-07-30 13:09:41 -0500

    Addedd in t3mlite

---
+++ snappy/snap/t3mlite/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup (
+    name = "t3m",
+    version = "",
+    description = "Tinker toys for topologists",
+    author = "Marc Culler and Nathan Dunfield",
+    author_email = "culler@math.uic.edu, dunfield@caltech.edu",
+    url = "http://www.math.uic.edu/~t3m",
+    packages = ['t3m'],
+    package_dir = {'t3m':''}
+    )
+

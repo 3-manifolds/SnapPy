6f0e89810f2ae1b76eee31ea95372d09b6818944
Nathan Dunfield <nathan@dunfield.info>
2012-08-16 11:28:31 -0500

    sage tweak

--- setup.py
+++ setup.py
@@ -189,7 +189,7 @@ ext_modules = [SnapPyC, CyOpenGL, TwisterCore]
 
 try:
     import sage
-    install_requires = ['plink>=1.2', 'ipython', 'pypng', 'spherogram>=1.0']
+    install_requires = ['plink>=1.2', 'ipython', 'pypng', 'spherogram']
 except ImportError:
     install_requires = ['plink>=1.2', 'ipython>=0.13', 'pypng', 'spherogram>=1.0', 'pyttk', 'cypari>=1.0']
     if sys.platform == 'win32':

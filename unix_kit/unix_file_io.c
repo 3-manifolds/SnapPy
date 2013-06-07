7457d91d4ffeb86fac852723d6176166cb3c0e6c
Marc Culler <culler@users.noreply.github.com>
2013-06-07 17:32:55 -0500

    Fixed return value error by returning NULL when file can't be read.

--- unix_kit/unix_file_io.c
+++ unix_kit/unix_file_io.c
@@ -231,7 +231,7 @@ static TriangulationData *ReadNewFileFormat(
 		* But why bother?  We are toast anyway.
 		*/
 	       uFatalError("ReadNewFileFormat 2", "unix file io");
-	       return;
+	       return NULL;
 	   }
        }
        ptr[i++] = *buffer++;

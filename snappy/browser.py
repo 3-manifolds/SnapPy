da142c06d74d7c5a6ae0923aab9172208d7cd03b
Marc Culler <culler@users.noreply.github.com>
2013-09-15 20:55:03 -0400

    Driller lets you choose max_segments.

--- snappy/browser.py
+++ snappy/browser.py
@@ -538,38 +538,51 @@ class Driller(SimpleDialog):
     def __init__(self, master, manifold):
         self.manifold = manifold
         self.num = 0 # make the superclass happy
+        self.max_segments = 6
         self.result = []
         self.root = root = Tk_.Toplevel(master, class_='SnapPy')
         title = 'Drill'
         root.title(title)
         root.iconname(title)
-        root.bind('<Return>', self.drill)
-        frame = Tk_.Frame(self.root)
-        frame.grid_columnconfigure(0, weight=1)
-        frame.grid_rowconfigure(1, weight=1)
+        root.bind('<Return>', self.handle_return)
+        top_frame = Tk_.Frame(self.root)
+        top_frame.grid_columnconfigure(0, weight=1)
+        top_frame.grid_rowconfigure(2, weight=1)
         msg_font = Font(family=font_info['family'],
                         weight='bold',
                         size=int(font_info['size']*1.2))
-        msg = ttk.Label(frame, font=msg_font,
+        msg = ttk.Label(top_frame, font=msg_font,
                         text='Choose which curves to drill out:')
         msg.grid(row=0, column=0, pady=10)
-        curve_list = [(x['parity'], x['filled_length'])
-                      for x in self.manifold.dual_curves()]
+        segment_frame = Tk_.Frame(top_frame)
+        self.segment_var = segment_var = Tk_.StringVar(root)
+        segment_var.set(str(self.max_segments))
+        ttk.Label(segment_frame, text='Max segments: ').pack(
+            side=Tk_.LEFT, padx=4)
+        self.segment_entry = segment_entry = ttk.Entry(
+            segment_frame,
+            takefocus=False,
+            width=2,
+            textvariable=segment_var,
+            validate='focusout',
+            validatecommand=(root.register(self.validate_segments),'%P')
+            )
+        segment_entry.pack(side=Tk_.LEFT)
+        segment_frame.grid(row=1, column=0, pady=2)
         self.curves = curves = ttk.Treeview(
-            frame,
+            top_frame,
             selectmode='extended',
-            columns=['parity', 'length'],
+            columns=['index', 'parity', 'length'],
             show='headings')
+        curves.heading('index', text='#')
+        curves.column('index', stretch=False, width=12)
         curves.heading('parity', text='Parity')
         curves.column('parity', stretch=False, width=80)
         curves.heading('length', text='Length')
         curves.column('length', stretch=True, width=400)
-        for curve in self.manifold.dual_curves():
-            parity = '+' if curve['parity'] == 1 else '-'
-            length = curve['filled_length']
-            curves.insert( '', 'end', values=(parity, length) )
-        self.curves.grid(row=1, column=0, padx=6, pady=6, sticky=Tk_.NSEW)
-        frame.pack(fill=Tk_.BOTH, expand=1) 
+        self.curves.grid(row=2, column=0, padx=6, pady=6, sticky=Tk_.NSEW)
+        self.show_curves()
+        top_frame.pack(fill=Tk_.BOTH, expand=1) 
         button_frame = Tk_.Frame(self.root)
         button = ttk.Button(button_frame, text='Drill', command=self.drill,
                             default='active')
@@ -580,12 +593,39 @@ class Driller(SimpleDialog):
         self.root.protocol('WM_DELETE_WINDOW', self.wm_delete_window)
         self._set_transient(master)
 
-    def drill(self, event=None):
+    def show_curves(self):
+        self.curves.delete(*self.curves.get_children())
+        for curve in self.manifold.dual_curves(max_segments=self.max_segments):
+            n = curve['index']
+            parity = '+' if curve['parity'] == 1 else '-'
+            length = curve['filled_length']
+            self.curves.insert( '', 'end', values=(n, parity, length) )
+
+    def handle_return(self, event):
+        if event.widget != self.segment_entry:
+            self.drill()
+        else:
+            self.curves.focus_set()
+
+    def drill(self):
         self.result = [self.curves.index(x) for x in self.curves.selection()]
         self.root.quit()
     
     def cancel(self):
         self.root.quit()
+
+    def validate_segments(self, P):
+        try:
+            new_max = int(P)
+            if self.max_segments != new_max:
+                self.max_segments = new_max
+                self.segment_var.set(str(self.max_segments))
+                self.show_curves()
+        except ValueError:
+            self.root.after_idle( 
+                self.segment_var.set, str(self.max_segments))
+            return False
+        return True
 
 if __name__ == '__main__':
     from snappy import Manifold

f6eee09ef7579407b2219bbe8df9fb34eeda73a7
Nathan Dunfield <nathan@dunfield.info>
2013-09-28 17:40:26 -0400

    Fixed inclusion of PyX in Windows Installer bundle

--- SnapPyExe/setup.py
+++ SnapPyExe/setup.py
@@ -3,6 +3,7 @@ import py2exe
 import glob
 import IPython
 import os
+import pyx
 
 APP = [{
         'script' : 'SnapPy.py',
@@ -29,16 +30,21 @@ glob.glob('../snappy/doc/_images/*.*')),
 ('snappy/doc/_sources',
 glob.glob('../snappy/doc/_sources/*.*')),
 ('IPython/config/profile',
-[os.path.join(IPython.__path__[0], 'config', 'profile', 'README_STARTUP')] )
+[os.path.join(IPython.__path__[0], 'config', 'profile', 'README_STARTUP')],
+ ),
+('pyx/data', [os.path.dirname(pyx.__file__) + '/data/pyxrc']), 
+('pyx/data/afm', glob.glob(os.path.dirname(pyx.__file__) + '/data/afm/*')), 
+('pyx/data/def', glob.glob(os.path.dirname(pyx.__file__) + '/data/def/*')), 
+('pyx/data/lfs', glob.glob(os.path.dirname(pyx.__file__) + '/data/lfs/*')), 
 ]
 
 OPTIONS = {
 'excludes':
   'scipy,numpy',
 'packages': 
-  'snappy,snappy.manifolds,snappy.twister,IPython,plink,readline,pyreadline',
+  'snappy,snappy.manifolds,snappy.twister,IPython,plink,readline,pyreadline,pyx',
 'includes':
-  'gzip,tarfile,pydoc,pyx',
+  'gzip,tarfile,pydoc',
 'skip_archive':
   1,
 'dist_dir':

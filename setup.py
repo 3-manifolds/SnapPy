41d6e297c1a708a2e5656947dcb5b3993242acd6
Marc Culler <culler@users.noreply.github.com>
2013-10-22 08:38:28 -0400

    Building the build system.

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ except:
 
 # Build the qd library if necessary
 
-if not os.path.exists('qd'):
+if not os.path.exists('qd') and 'clean' not in sys.argv:
     os.system('cd qd_src ; bash build_qd.bash ')
 
 from distutils.extension import Extension
@@ -77,7 +77,8 @@ class clean(Command):
         os.system('rm -rf build dist *.pyc')
         os.system('rm -rf snappy*.egg-info')
         os.system('rm -rf snappy/doc')
-        os.system('rm */Cy*.c */Cy*.h */Cy*.cpp SnapPy.c SnapPy.h')
+        os.system('rm */Cy*.c */Cy*.h */Cy*.cpp')
+        os.system('rm SnapPy.c SnapPy.h SnapPyHP.cpp SnapPyHP.h')
 
 class build_docs(Command):
     user_options = []
@@ -92,6 +93,7 @@ class build_docs(Command):
         sphinx_cmd(sphinx_args)
 
 # C source files we provide
+
 base_code = glob.glob(os.path.join('kernel_code','*.c'))
 unix_code = glob.glob(os.path.join('unix_kit','*.c'))
 unix_code.remove(os.path.join('unix_kit','unix_UI.c'))
@@ -99,11 +101,47 @@ unix_code.remove(os.path.join('unix_kit','decode_new_DT.c'))
 addl_code = glob.glob(os.path.join('addl_code', '*.c')) + glob.glob(os.path.join('addl_code', '*.cc'))
 code  =  base_code + unix_code + addl_code
 
+# Symlinks for building the high precision version
+
+def make_symlinks(source_files, target_dir):
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
+    for path in source_files:
+        filename, ext = os.path.splitext(os.path.basename(path))
+        new_ext = '.cpp' if ext == '.c' else ext
+        link_name = os.path.join(target_dir, filename + new_ext)
+        source = os.path.join('..', path)
+        if not os.path.exists(link_name):
+            print 'linking %s -> %s'%(link_name, source) 
+            os.symlink(source, link_name)
+
+def setup_symlinks():
+    make_symlinks(base_code, 'hp_kernel_code')
+    make_symlinks(unix_code, 'hp_unix_kit')                                 
+    unix_headers = glob.glob(os.path.join('unix_kit', '*.h'))
+    make_symlinks(unix_headers, 'hp_unix_kit')
+    make_symlinks(addl_code, 'hp_addl_code')
+    addl_headers = glob.glob(os.path.join('addl_code', '*.h'))
+    make_symlinks(addl_headers, 'hp_addl_code')
+    headers = glob.glob(os.path.join('headers', '*.h'))
+    make_symlinks(headers, 'hp_headers')                                 
+
+class build_symlinks(Command):
+    user_options = []
+    def initialize_options(self):
+        pass 
+    def finalize_options(self):
+        pass
+    def run(self):
+        setup_symlinks()
+
 # C++ source files we provide
+
+if not os.path.exists('hp_kernel_code') and 'clean' not in sys.argv:
+    setup_symlinks()
+
 hp_base_code = glob.glob(os.path.join('hp_kernel_code','*.cpp'))
 hp_unix_code = glob.glob(os.path.join('hp_unix_kit','*.cpp'))
-#hp_unix_code.remove(os.path.join('hp_unix_kit','unix_UI.cpp'))
-#hp_unix_code.remove(os.path.join('hp_unix_kit','decode_new_DT.cpp'))
 hp_addl_code = glob.glob(os.path.join('hp_addl_code', '*.cpp'))# + glob.glob(os.path.join('addl_code', '*.cc'))
 hp_code  =  hp_base_code + hp_unix_code + hp_addl_code
 
@@ -211,7 +249,10 @@ setup( name = 'snappy',
         },
        package_dir = {'snappy/twister':'Twister/lib'},
        ext_modules = ext_modules,
-       cmdclass =  {'build_ext': build_ext, 'clean' : clean, 'build_docs': build_docs},
+       cmdclass =  {'build_ext': build_ext,
+                    'clean' : clean,
+                    'build_symlinks': build_symlinks,
+                    'build_docs': build_docs},
        entry_points = {'console_scripts': ['SnapPy = snappy.app:main']},
        author = 'Marc Culler and Nathan Dunfield',
        author_email = 'culler@math.uic.edu, nmd@illinois.edu',

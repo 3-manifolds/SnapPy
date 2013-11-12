97e9c9b4ab52e4947edccfc1a257af5f5dbf23a0
Marc Culler <culler@users.noreply.github.com>
2013-11-12 16:36:14 -0500

    Added include hooks for compiling the kernel in a C++ namespace.

---
+++ headers/end_namespace.h
@@ -0,0 +1,4 @@
+/*
+ * If you declared a namespace block in kernel_namespace.h then
+ * you can close the block here.
+ */
---
+++ headers/kernel_namespace.h
@@ -0,0 +1,4 @@
+/*
+ * If you wish to build the snappea kernel within a C++ namespace
+ * you can declare the namespace block in this file.
+ */

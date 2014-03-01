5f50b19a531d23082b5832e5da976bd26718f173
Marc Culler <culler@users.noreply.github.com>
2014-03-01 13:49:22 -0600

    Added pyx==0.12.1 as a requirement.

--- setup.py
+++ setup.py
@@ -244,7 +244,7 @@ try:
     import sage
     install_requires = ['plink>=1.6', 'ipython', 'pypng', 'spherogram>=1.3']
 except ImportError:
-    install_requires = ['plink>=1.6', 'ipython>=0.13', 'pypng', 'spherogram>=1.3', 'cypari>=1.0']
+    install_requires = ['plink>=1.6', 'ipython>=0.13', 'pypng', 'spherogram>=1.3', 'cypari>=1.0', 'pyx==0.12.1']
     if sys.platform == 'win32':
         install_requires.append('pyreadline>=2.0')
 

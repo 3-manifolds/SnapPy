1df3a330cdba2eddf1cd893f72af357d13e81c87
Nathan Dunfield <nathan@dunfield.info>
2014-03-02 21:30:27 +1100

    Removed PyX from main setup.py, added in SnapPyExe/make.py

--- SnapPyExe/make.py
+++ SnapPyExe/make.py
@@ -2,8 +2,13 @@
 
 import os, sys, re
 
-python26 = "c:\Python26\python.exe "
-python27 = "c:\Python27\python.exe " 
+python27 = "c:\Python27\python.exe "
+
+try:
+    import pyx
+except ImportError:
+    print "ERROR: Need to install PyX!"
+    sys.exit()
 
 os.chdir("../SnapPyExe/../")
 os.system("hg pull")
@@ -11,7 +16,6 @@ os.system("hg update")
 os.system("rm dist/*.egg")
 
 for python in [python27]:
-    os.system(python + "setup.py build -c mingw32")
     os.system(python + "setup.py install")
     os.system(python + "setup.py build_docs")
     os.system(python + "setup.py install")

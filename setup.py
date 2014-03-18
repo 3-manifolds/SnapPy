76ff79b042533183353f10bfb5da0dacf928c977
Marc Culler <culler@math.uic.edu>
2014-03-18 17:32:33 -0500

    Little things to make the SnapPy app build on Mavericks.

--- setup.py
+++ setup.py
@@ -203,7 +203,7 @@ CyOpenGL_libs = []
 CyOpenGL_extras = []
 CyOpenGL_extra_link_args = []
 if sys.platform == 'darwin':
-    CyOpenGL_includes += ['/System/Library/Frameworks/OpenGL.framework/Versions/Current/Headers/']
+    CyOpenGL_includes += ['/System/Library/Frameworks/OpenGL.framework/Versions/Current/Headers/', '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/System/Library/Frameworks/OpenGL.framework/Versions/A/Headers/']
     CyOpenGL_extra_link_args = ['-framework', 'OpenGL']
 elif sys.platform == 'linux2':
     CyOpenGL_includes += ['/usr/include/GL']

6cde3903232b01a38881ef6e2d5f0a9d4e1889b8
Matthias Goerner <enischte@gmail.com>
2014-07-24 17:34:06 -0700

    In ptolemy module, adding wrapping to compute matrices for the generators of the fundamental group of a regina triangulation.

--- snappy/ptolemy/reginaWrapper.py
+++ snappy/ptolemy/reginaWrapper.py
@@ -627,3 +627,79 @@ class NTriangulationForPtolemy(NTriangulation):
                 identifications.append((sign, power, ptolemy, other_ptolemy))
 
         return identifications
+
+    def _choose_generators(self, *args, **kwargs):
+        """
+        Not necessary. We call maximalForestInDualSkeleton instead in
+        _choose_generators_info.
+        """
+        pass
+
+    def _choose_generators_info(self):
+        """
+        Provides information about the choices mades when regina computed the
+        simplified fundamental group in a structure similar to SnapPy.
+        """
+        
+        # maximalForestInDualSkeleton was not exposed in earlier regina
+        # versions
+        if not hasattr(self, 'maximalForestInDualSkeleton'):
+            raise Exception("Version of regina does not have python wrapping "
+                            "for maximalForestInDualSkeleton")
+
+        # Every triangle belonging to the spanning tree is not a generator
+        non_generator_triangles = self.maximalForestInDualSkeleton()
+
+        # All other triangles are
+        generator_triangles = [ triangle for triangle in self.getTriangles()
+                                if not triangle in non_generator_triangles ]
+
+        def get_neighbors(tet):
+            """
+            Given a tetrahedron, return the indices of the 4 neighboring
+            tetrahedra
+            """
+            return [ self.tetrahedronIndex(tet.getAdjacentTetrahedron(face))
+                     for face in range(4) ]
+
+        def get_gluings(tet):
+            """
+            Given a tetrahedron, return the four face gluings.
+            """
+            return [ tet.getAdjacentTetrahedronGluing(face)
+                     for face in range(4) ]
+
+        def get_generator(tet, face):
+            """
+            Given a tetrahedron and a face (0, ..., 3), return whether it
+            corresponds to an inbound or outbound generator.
+            """
+            # Get corresponding triangle
+            triangle = tet.getTriangle(face)
+            if not triangle in generator_triangles:
+                # Not a generator, return 0
+                return 0
+            # Get the index of the generator, first generator is indexed 1
+            # so that a negative index can correspond to the inverse
+            # generator
+            gen = generator_triangles.index(triangle) + 1
+            # regina uses embedding 0 of the triangle as outbound generator
+            canonical_embed = triangle.getEmbedding(0)
+            if (canonical_embed.getTetrahedron() == tet and
+                canonical_embed.getFace() == face):
+                return gen
+            else:
+                return -gen
+
+        def get_generators(tet):
+            """
+            Given a tetrahedron, return for each face which inbound
+            or outbound generator it belongs to.
+            """
+            return [ get_generator(tet, face) for face in range(4) ]
+        
+        return [ { 'index' : index,
+                   'neighbors' : get_neighbors(tet),
+                   'gluings' : get_gluings(tet),
+                   'generators' : get_generators(tet) }
+                 for index, tet in enumerate(self.getTetrahedra()) ]

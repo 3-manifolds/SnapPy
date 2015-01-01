89c492d7bf3a94110259b75817ac2e1e95ef7034
Nathan Dunfield <nathan@dunfield.info>
2014-12-31 19:07:41 -0600

    Working on making PyPI default release location

--- release_tools/release_simple.py
+++ release_tools/release_simple.py
@@ -13,7 +13,7 @@ def run(command):
 module_path = sys.argv[1]
 if not os.path.exists(module_path):
     print('*** Getting module source...')
-    run('hg clone http://math.uic.edu/t3m/hg/' + module_path)
+    run('hg clone https://bitbucket.org/t3m/' + module_path)
 os.chdir(module_path)
 print('*** Updating module source...')
 run('hg pull')
@@ -22,19 +22,26 @@ run('hg up')
 # Build and install the module into all the standard Pythons
 
 if sys.platform.startswith('linux'):
-    pythons = ['/home/dunfield/python' + py + '/bin/python' for py in ['26', '27', '32']]
+    pythons = ['/home/dunfield/python' + py + '/bin/python' for py in ['26', '27']]
+    commands = ['install']
 elif sys.platform.startswith('darwin'):
-    pythons = ['/Library/Frameworks/Python.framework/Versions/2.6/bin/python',
-               '/Library/Frameworks/Python-10.5-intel.framework/Versions/2.7/bin/python2.7',
-               '/Library/Frameworks/Python-10.5-intel.framework/Versions/3.2/bin/python3.2']
+    special_python = '/Library/Frameworks/Python-10.5-intel.framework/Versions/2.7/bin/python2.7'
+    if os.path.exists(special_python):
+        pythons = [special_python]
+        commands = ['install', 'sdist']
+    else:
+        pythons = ['/Library/Frameworks/Python.framework/Versions/2.7/bin/python2.7']
+        commands = ['bdist_wheel']
 elif sys.platform.startswith('win'):
-    pythons = ['c:\Python26\python.exe', 'c:\Python27\python.exe', 'c:\Python32\python.exe']
+    pythons = ['c:\Python27\python.exe']
+    commands = ['install', 'bdist_wheel']
                
 for python in pythons:
     print "\n*** Building version with " + python
     if sys.platform.startswith('win'):
         run( python + ' setup.py build -c mingw32')
-    run(python + ' setup.py install' )
+    for command in commands:
+        run(python + ' setup.py ' + command)
 
 
 # Locate eggs that are less than a day old
@@ -46,18 +53,18 @@ if sys.platform.startswith('win'):
     eggs = [e.replace('\\', '/') for e in eggs]
 
 # For OS X, we need fake PPC/intel "fat" eggs.  
+# 
+#if sys.platform.startswith('darwin'):
+#    for egg in eggs[:]:
+#        copy = egg.replace("-intel", "-fat")
+#        os.system("cp " + egg + " " + copy)
+#        eggs.append(copy)
 
-if sys.platform.startswith('darwin'):
-    for egg in eggs[:]:
-        copy = egg.replace("-intel", "-fat")
-        os.system("cp " + egg + " " + copy)
-        eggs.append(copy)
 
-
-try: input = raw_input
-except: pass
-input('***Hit any key when ready to begin copying to t3m***')
-os.system('scp -p ' + ' '.join(eggs) + ' nmd@shell.math.uic.edu:t3m_web/SnapPy-nest')
+#try: input = raw_input
+#except: pass
+#input('***Hit any key when ready to begin copying to t3m***')
+#os.system('scp -p ' + ' '.join(eggs) + ' nmd@shell.math.uic.edu:t3m_web/SnapPy-nest')
 
 
 

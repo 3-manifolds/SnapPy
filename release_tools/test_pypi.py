eab5237417481adcd003353b605f75e1b6be08e6
Nathan Dunfield <nathan@dunfield.info>
2015-01-02 15:01:31 -0600

    Make release testing script able to switch between pip and easy_install

--- release_tools/test_pypi.py
+++ release_tools/test_pypi.py
@@ -1,8 +1,16 @@
 import sys, os, re
 
-for module in sys.argv[1: ]:
+for module in sys.argv[2: ]:
     py_dir = '/tmp/test_python_' + module
+    if sys.argv[1].startswith('easy'):
+        install_cmd = py_dir + '/bin/easy_install '
+    elif sys.argv[1].startswith('pip'):
+        install_cmd = py_dir + '/bin/pip --no-cache-dir install '
+    else:
+        print('Need to specify install via "pip" or "easy*"')
+        sys.exit(0)
+
     os.system('rm -rf ' + py_dir)
     os.system(sys.executable + ' -m virtualenv ' + py_dir)
-    os.system(py_dir + '/bin/pip install --no-cache-dir ' + module)
+    os.system(install_cmd + module)
     os.system(py_dir + '/bin/python -m ' + module + '.test')

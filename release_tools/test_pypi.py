fc2600d683da40b0089c447f0e4da129ce3f400c
Nathan Dunfield <nathan@dunfield.info>
2015-01-02 15:29:01 -0600

    Fixed script

--- release_tools/test_pypi.py
+++ release_tools/test_pypi.py
@@ -1,16 +1,17 @@
 import sys, os, re
 
+if sys.argv[1].startswith('easy'):
+    install_cmd = '/bin/easy_install '
+elif sys.argv[1].startswith('pip'):
+    install_cmd = '/bin/pip --no-cache-dir install '
+else:
+    print('Need to specify install via "pip" or "easy*"')
+    sys.exit()
+
+
 for module in sys.argv[2: ]:
     py_dir = '/tmp/test_python_' + module
-    if sys.argv[1].startswith('easy'):
-        install_cmd = py_dir + '/bin/easy_install '
-    elif sys.argv[1].startswith('pip'):
-        install_cmd = py_dir + '/bin/pip --no-cache-dir install '
-    else:
-        print('Need to specify install via "pip" or "easy*"')
-        sys.exit(0)
-
     os.system('rm -rf ' + py_dir)
     os.system(sys.executable + ' -m virtualenv ' + py_dir)
-    os.system(install_cmd + module)
+    os.system(py_dir + install_cmd + module)
     os.system(py_dir + '/bin/python -m ' + module + '.test')

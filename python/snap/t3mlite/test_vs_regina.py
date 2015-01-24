acd9a2f866565c5631f388bc07aca140e04f49ff
Nathan Dunfield <nathan@dunfield.info>
2015-01-23 19:49:51 -0600

    Added testing code

--- python/snap/t3mlite/test_vs_regina.py
+++ python/snap/t3mlite/test_vs_regina.py
@@ -1,6 +1,6 @@
-import snappy
+import snappy, regina
 import snappy.snap.t3mlite as t3m
-import regina
+import snappy.snap.t3mlite.spun as spun
 
 def hash_t3m_surface(surface):
     ans = [surface.EulerCharacteristic]
@@ -41,10 +41,41 @@ def compare_closed(snappy_manifold):
 
     all_together = sum(t_hashes, [])
     return t_hashes == r_hashes, len(all_together), sum(all_together)
+
+def regina_boundary_slope(surface):
+    slope = surface.boundaryIntersections()
+    a = int(slope.entry(0,0).stringValue())
+    b = int(slope.entry(0,1).stringValue())
+    return (b, -a)
+    
+def compare_cusped(snappy_manifold):
+    tri_data = snappy_manifold._to_string()
+    T = spun.Manifold(tri_data)
+    t_hashes = sorted(sorted(S.quad_vector()) for S in T.normal_surfaces())
+    t_slopes = sorted(tuple(S.boundary_slopes()) for S in T.normal_surfaces())
+
+    R = regina.NSnapPeaTriangulation(tri_data)
+    t = R.getNumberOfTetrahedra()
+    regina_surfaces = list(vertex_surfaces(R))
+    r_hashes = sorted(
+        sorted(sorted(int(S.getQuadCoord(i, j).stringValue())
+                      for i in range(t) for j in range(3)))
+                      for S in regina_surfaces)
+    r_slopes = sorted(map(regina_boundary_slope, regina_surfaces))
+    
+    assert t_hashes == r_hashes and t_slopes == r_slopes
+    
     
 def closed_test(N = 10):
     for M in snappy.OrientableClosedCensus[:N]:
         print M, compare_closed(M)
+
+def cusped_test(N = 100):
+    for census in [snappy.CensusKnots, snappy.OrientableCuspedCensus(cusps=1)]:
+        for M in census:
+            print M
+            compare_cusped(M)
+            
 
 
         

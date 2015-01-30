b266c2fcae2f5910b084a1f580e1350dd89aa80b
Nathan Dunfield <nathan@dunfield.info>
2015-01-30 15:58:19 -0600

    Added 'brasile slopes' option to Manifold.normal_boundary.slopes

--- python/snap/t3mlite/spun.py
+++ python/snap/t3mlite/spun.py
@@ -126,7 +126,7 @@ class SpunSurface:
         return self._boundary_slopes
     
     def is_compatible(self, other):
-        for a, b in zip(self.quad_types, other.quad_types):
+        for a, b in zip(self._quad_types, other._quad_types):
             if not (a == b or None in (a, b)):
                 return False
         return True
@@ -199,7 +199,7 @@ def normal_surfaces(self):
                                           for i, qv in enumerate(eqns.vertex_solutions())]
     return self._cache['normal_surfaces']
 
-def normal_boundary_slopes(self, kabaya=False):
+def normal_boundary_slopes(self, subset='all'):
     """
     For a one-cusped manifold, returns all the nonempty boundary slopes of
     spun normal surfaces.  Provided the triangulation supports a
@@ -212,16 +212,24 @@ def normal_boundary_slopes(self, kabaya=False):
     >>> M.normal_boundary_slopes()
     [(16, -1), (20, -1), (37, -2)]
 
-    If the flag ``kabaya`` is set, then it only returns boundary slopes
-    associated to vertex surfaces with a quad in every tetrahedron; by
-    Theorem 1.1. of `[DG] <http://arxiv.org/abs/1102.4588>`_ these
-    are all strict boundary slopes.
+    If the ``subset`` flag is set to ``'kabaya'``, then it only
+    returns boundary slopes associated to vertex surfaces with a quad
+    in every tetrahedron; by Theorem 1.1. of `[DG]
+    <http://arxiv.org/abs/1102.4588>`_ these are all strict boundary
+    slopes.
 
-    >>> N = snappy.Manifold('s000')
+    >>> N = Manifold('m113')
     >>> N.normal_boundary_slopes()
-    [(1, 0), (2, -1), (4, 1), (6, 1), (10, 3), (14, 5)]
-    >>> N.normal_boundary_slopes(kabaya=True)
-    [(14, 5)]
+    [(1, 1), (1, 2), (2, -1), (2, 3), (8, 11)]
+    >>> N.normal_boundary_slopes('kabaya')
+    [(8, 11)]
+
+    If the ``subset`` flag is set to ``'brasile'`` then it returns
+    only the boundary slopes that are associated to vertex surfaces
+    giving isolated rays in the space of embedded normal surfaces.
+
+    >>> N.normal_boundary_slopes('brasile')
+    [(1, 2), (8, 11)]
     """
     if not self.is_orientable():
         raise ValueError('Manifold must be orientable')
@@ -229,8 +237,22 @@ def normal_boundary_slopes(self, kabaya=False):
         raise ValueError('More than 1 cusp, so need to look at the surfaces directly.')
 
     surfaces = self.normal_surfaces()
-    if kabaya:
+    if subset is 'kabaya':
         surfaces = [S for S in surfaces if min(S.coefficients()) > 0]
+    elif subset is 'brasile':
+        isolated_surfaces = []
+        for S in surfaces:
+            isolated = True
+            for F in surfaces:
+                if S != F and S.is_compatible(F):
+                    isolated = False
+                    break
+            if isolated:
+                isolated_surfaces.append(S)
+        surfaces = isolated_surfaces
+    else:
+        if subset != 'all':
+            raise ValueError("Subset must be one of 'all', 'kabaya', and 'brasile'")
     
     slopes = {normalize_slope(S.boundary_slopes()) for S in surfaces}
     slopes.discard( (0, 0) )
@@ -240,4 +262,3 @@ if __name__ == "__main__":
     import doctest
     names = {'Manifold':snappy.Manifold}
     doctest.testmod(extraglobs=names)
-    

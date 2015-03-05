840c4fc74fd9bc22cb842cc6d57441597fd5ac0c
Marc Culler <culler@math.uic.edu>
2015-03-05 12:50:13 -0600

    Fixing warnings on the old Apple gcc compiler.

--- cython/warnings.h
+++ cython/warnings.h
@@ -1,6 +1,12 @@
 #ifdef __APPLE__
+#ifdef __clang__
 #pragma clang diagnostic ignored "-Wunused-function"
 #pragma clang diagnostic ignored "-Wsometimes-uninitialized"
+#else
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-function"
+#pragma GCC diagnostic ignored "-Wuninitialized"
+#endif
 #endif
 #ifdef __linux__
 #pragma GCC diagnostic ignored "-Wunused-function"

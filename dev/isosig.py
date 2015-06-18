31f894f7d79192658a397823e797fbd6e048ec08
Nathan Dunfield <nathan@dunfield.info>
2015-06-17 21:58:45 -0500

    Better decoration for isosigs

--- dev/isosig.py
+++ dev/isosig.py
@@ -13,13 +13,10 @@ are supported.
 
 A simple valid decorated isosig for a two-cusped manifold is::
 
-    eLMkbcddddedde[1,0,1,0,0,-1,0,1,1,-1]
+eLMkbcddddedde[1,0,1,0,0,-1,0,1,1,-1]
 
-To match the original aesthetic of the isosig, when all integers in
-the list are in range(-31, 33) then there is also more condensed
-decoration::
-
-    eLMkbcddddedde:babaabaAbA
+Here, the bare isosig is what preceeds the semicolon; what follows is
+an encoded version of the 5n integers mentioned above.
 
 Note: An isosig is an invariant of a triangulation of an *unoriented*
 manifold.  For an amphicheiral manifold M, it can happen that
@@ -32,26 +29,87 @@ import snappy
 import re, string
 
 # Used between the base isosig and the condensed version. 
-separator = ':'
+separator = ';'
 
 # Pattern matching dectorated isosigs
 
 base64_pat = '([a-zA-Z0-9\+\-]+)'
-isosig_pattern = re.compile(base64_pat + '(\[[,\-0-9]+\])$')
-isosig_condensed_pattern = re.compile(base64_pat + separator + base64_pat + '$')
+isosig_pattern = re.compile(base64_pat + separator + base64_pat + '$')
 
-# Used for the condensed version
+# We store lists of integers as base64 strings.  
 
-small_ints = range(0, 26) + range(-1, -27, -1) + range(26, 33) + range(-27, -32, -1)
 base64_letters = string.ascii_letters + '0123456789+-'
-small_int_to_letter = dict(zip(small_ints, base64_letters))
-letter_to_small_int = dict(zip(base64_letters, small_ints))
+base64_lower = string.ascii_lowercase + '01234+'
+base64_upper = string.ascii_uppercase + '56789-'
+in_one = string.ascii_lowercase[:16] + string.ascii_lowercase[1:16].upper()
 
-def encode_decoration(decorations, compress):
-    if compress and -31 <= min(decorations) and max(decorations) <= 32:
-        return separator + ''.join([small_int_to_letter[d] for d in decorations])
-    else:
-        return repr(decorations).replace(' ', '')
+int_to_letter = dict(enumerate(base64_letters))
+letter_to_int = dict((a, i) for i, a in enumerate(base64_letters))
+
+def encode_nonnegative_int(x):
+    """
+    Regina's base64 encoding scheme, described in the appendix to
+    http://arxiv.org/abs/1110.6080
+    """
+    assert x >= 0
+    six_bits = []
+    while True:
+        low_six = x & 63
+        six_bits.append(low_six)
+        x = x >> 6
+        if x == 0:
+            break
+    return ''.join([int_to_letter[b] for b in six_bits])
+
+def decode_nonnegative_int(s):
+    return sum( letter_to_int[a] << 6*i for i, a in enumerate(s)) 
+    
+def encode_int(x):
+    """
+    Encodes an integer in the range [-2**90 + 1, 2**90 - 1] with a "stop"
+    at the end so a concatenation of such encodings is easily
+    decodable.  The basic format is:
+    
+    if x in [0...15], encode as a single letter in [a...p]
+    if x in [-15...-1] encode as a single letter in [P...B]
+
+    Otherwise, the first letter specifies the length of
+    encode_nonnegative_int(abs(x)) as well as sign(x), followed by the
+    encoding of abs(x).
+    """
+    if 0 <= x < 16:
+        return base64_letters[x]
+    if -15 <= x < 0:
+        return base64_letters[abs(x) + 26]
+    encoded_xabs = encode_nonnegative_int(abs(x))
+    L = len(encoded_xabs)
+    try:
+        if x > 0:
+            return base64_lower[16 + L] + encoded_xabs
+        if x < 0:
+            return base64_upper[16 + L] + encoded_xabs
+    except IndexError:
+        raise ValueError('The given integer is too large to encode')
+
+def encode_integer_list(L):
+    return ''.join(map(encode_int, L))
+
+def decode_integer_list(encoded):
+    ans = []
+    while len(encoded):
+        s = encoded[0]
+        sign = 1 if s in base64_lower else -1
+        if s in in_one:
+            ans.append(sign*letter_to_int[s.lower()])
+            encoded = encoded[1:]
+        else:
+            if sign == 1:
+                L = base64_lower.index(s) - 16
+            else:
+                L = base64_upper.index(s) - 16
+            current, encoded = encoded[1:L+1], encoded[L+1:]
+            ans.append(sign*decode_nonnegative_int(current))
+    return ans
 
 # Some helper functions
 
@@ -71,7 +129,7 @@ def as_two_by_two_matrices(L):
 
 # main two functions
     
-def decorated_isosig(manifold, condense=False):
+def decorated_isosig(manifold):
     isosig = manifold.triangulation_isosig()
     N = snappy.Triangulation(isosig)
     N.set_peripheral_curves('combinatorial')
@@ -82,20 +140,15 @@ def decorated_isosig(manifold, condense=False):
         decorations = inverse_perm(tri_iso.cusp_images())
     for A in tri_iso.cusp_maps():
         decorations += [A[0, 0], A[1, 0], A[0, 1], A[1, 1]]
-    return isosig + encode_decoration(decorations, condense)
+    return isosig + separator + encode_integer_list(decorations)
     
 def from_decorated_isosig(spec):
     match = isosig_pattern.match(spec)
     if match:
         isosig, decorations = match.groups()
-        dec = eval(decorations)
     else:
-        match = isosig_condensed_pattern.match(spec)
-        if match:
-            isosig, decorations = match.groups()
-            dec = [letter_to_small_int[d] for d in decorations]
-        else:
-            raise ValueError('Did not provide a valid dectorated isosig')
+        raise ValueError('Did not provide a valid dectorated isosig')
+    dec = decode_integer_list(decorations)
     N = snappy.Manifold(isosig)
     N.set_peripheral_curves('combinatorial')
     n = N.num_cusps()
@@ -136,15 +189,22 @@ def main_test():
                 snappy.HTLinkExteriors(filter='cusps>3 and volume<14')]
     for census in censuses:
         for M in census:
-            isosig = decorated_isosig(M, True)
+            isosig = decorated_isosig(M)
             N = from_decorated_isosig(isosig)
             assert same_peripheral_curves(M, N)
             print(isosig)
-            isosig = decorated_isosig(M, False)
-            N = from_decorated_isosig(isosig)
-            assert same_peripheral_curves(M, N)
-            print(isosig)
+
+def test_integer_list_encoder(trys=1000, length=100, max_entry=2**90):
+    import random
+    for i in range(trys):
+        entries = [random.randrange(-max_entry, max_entry) for i in range(length)]
+        entries += [random.randrange(-15, 16) for i in range(length)]
+        random.shuffle(entries)
+        assert decode_integer_list(encode_integer_list(entries)) == entries
+            
     
 if __name__ == '__main__':
+    #test_integer_list_encoder()
     main_test()
+        
 

fe65a77c3cea6824b522345954e71694fac1fe82
Nathan Dunfield <nathan@dunfield.info>
2015-07-21 07:12:01 -0500

    Cleanup: fixed bug and hacked around doctest matrix hack

--- python/snap/__init__.py
+++ python/snap/__init__.py
@@ -38,7 +38,7 @@ def trace_field_gens(manifold, fundamental_group_args = []):
         sage: traces = M.trace_field_gens()
         sage: traces.find_field(100, 10, optimize=True)    # doctest: +NORMALIZE_WHITESPACE
         (Number Field in z with defining polynomial x^2 + 1,
-        <ApproxAN: -1.0*I>, [z + 1, -z, -z - 1])
+        <ApproxAN: -1.0*I>, [z + 1, z, z + 1])
     """
     def func(prec):
         return polished_holonomy(manifold, prec,

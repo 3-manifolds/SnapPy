24fcd9ab5983b6d5f30df5e3e228729ae4ea54cd
Nathan Dunfield <nathan@dunfield.info>
2016-01-15 14:41:39 -0600

    Toy example showing how to use Cython classes across multiple files

--- dev/min_cython/setup.py
+++ dev/min_cython/setup.py
@@ -1,25 +1,22 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 import os, glob
 
-code_dirs = [ ('.c', 'c_library'), ('.pyx', 'cython_code'), ('.pxd','cython_code')]
 
-sources = []
-for  code_type, directory in code_dirs:
-    sources += glob.glob(os.path.join(directory, '*'+code_type))
-
-print sources
+c_code = glob.glob('c_library/*.c')
+cython_code = glob.glob('cython_code/*.pyx')
+headers = ['c_headers', 'cython_headers']
 
 
-
-
-ext_modules = [Extension(name = "cytest",
-                         sources = sources,
-                         include_dirs = ['headers']
-                         )]
-
+ext_modules = []
+for cython_file in cython_code:
+    ext_modules.append(
+        Extension(name = os.path.basename(cython_file)[:-4], 
+                  sources = [cython_file] + c_code,
+                  include_dirs = headers)
+    )
+                        
 setup(
-  cmdclass = {'build_ext': build_ext},
-  ext_modules = ext_modules
+  ext_modules = cythonize(ext_modules, include_path=headers)
 )

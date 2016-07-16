1fcb81c7f1203146810b0a6dd6409c42eb822c21
Nathan Dunfield <nathan@dunfield.info>
2016-07-16 18:08:03 -0500

    Now can remove excess variables from the extended ptolemy var

--- dev/extended_ptolemy/extended.py
+++ dev/extended_ptolemy/extended.py
@@ -8,7 +8,53 @@ import peripheral
 import peripheral.link
 
 directed_edges = [(a, b) for a in range(4) for b in range(4) if a < b]
+
 edge_to_var = {e:i for i, e in enumerate(directed_edges)}
+
+t3m_edge_to_tuple = {t3m.E01:(0,1), t3m.E02:(0,2), t3m.E03:(0,3),
+                         t3m.E12:(1,2), t3m.E13:(1,3), t3m.E23:(2,3)}
+
+to_t3m_edge = {(0,1):t3m.E01, (0,2):t3m.E02, (0,3):t3m.E03,
+                         (1,2):t3m.E12, (1,3):t3m.E13, (2,3):t3m.E23}
+
+def arrow(mcomplex, tet, face, edge):
+    return t3m.Arrow(to_t3m_edge[edge], t3m.TwoSubsimplices[face],
+                     mcomplex.Tetrahedra[tet])
+
+def faces_around_edge(mcomplex, tet, edge):
+    face = min(set(range(4)) - set(edge))
+    ans = [(tet, face, edge)]
+    A0 = arrow(mcomplex, tet, face, edge)
+    A = A0.copy().next()
+    while A != A0:
+        tet = A.Tetrahedron.Index
+        face = t3m.FaceIndex[A.Face]
+        edge = t3m_edge_to_tuple[A.Edge]
+        ans.append((tet, face, edge))
+        A.next()
+    return ans
+
+def lex_first_edge_starts(mcomplex):
+    """
+    Returns a list of containing tuples of the form (tet, face, edge),
+    one at each edge.
+    """    
+    T = mcomplex
+    ans = []
+    for edge in T.Edges:
+        poss_starts = []
+        for C in edge.Corners:
+            t = C.Tetrahedron.Index
+            e = t3m_edge_to_tuple[C.Subsimplex]
+            poss_starts.append((t, e))
+        ans.append(min(poss_starts))
+    return sorted(ans)
+
+def arrows_around_edges(manifold):
+    T = t3m.Mcomplex(manifold)
+    starts = lex_first_edge_starts(T)
+    ans = []
+    return [faces_around_edge(T, tet, edge) for tet, edge in starts]
 
 def parse_ptolemy_edge(var):
     c, index, tet = var.split('_')
@@ -57,6 +103,7 @@ def peripheral_cohomology_basis(manifold):
     return PeripheralOneCocycle(m), PeripheralOneCocycle(l)
 
 
+
 class EdgeGluings(object):
     def __init__(self, gen_obs_class):
         assert gen_obs_class._N == 2
@@ -66,7 +113,7 @@ class EdgeGluings(object):
         primary_faces = [parse_ptolemy_face(x[2]) for x in
                          M._ptolemy_equations_identified_face_classes()]
         ptolemy_idents = M._ptolemy_equations_identified_coordinates(2, gen_obs_class.H2_class)
-        self.edge_gluings = []
+        self.edge_gluings = edge_gluings = dict()
 
         for i, (tet0, face0) in enumerate(primary_faces):
             for j in range(3):
@@ -75,9 +122,9 @@ class EdgeGluings(object):
                 tet0alt, edge0 = parse_ptolemy_edge(edge_var_0)
                 tet1, edge1 = parse_ptolemy_edge(edge_var_1)
                 perm = gluings[tet0][1][face0] 
-                face1 = perm[face0]                
-                self.edge_gluings.append(
-                    [(tet0, face0, edge0), (tet1, face1, edge1), sign])
+                face1 = perm[face0]
+                edge_gluings[tet0, face0, edge0] = [(tet1, face1, edge1), sign]
+                edge_gluings[tet1, face1, edge1] = [(tet0, face0, edge0), sign]
                 
                 # Sanity checks that our enumeration of the face
                 # idenifications and edges agrees with
@@ -103,48 +150,50 @@ def extended_ptolemy_equations(manifold, gen_obs_class=None):
     
     n = manifold.num_tetrahedra()
     tet_vars = [x + repr(d) for d in range(n) for x in 'abcdef']
-    R = PolynomialRing(QQ, ['M', 'L', 'z'] + tet_vars)
-    #R = PolynomialRing(QQ, tet_vars + ['z', 'M', 'L'])
-    tet_vars = [R(x) for x in tet_vars]
-    M, L = R('M'), R('L')
+    def var(tet, edge):
+        return tet_vars[6*tet + directed_edges.index(edge)]
+
+    all_arrows = arrows_around_edges(manifold)
+    independent_vars = [var(a[0][0], a[0][2]) for a in all_arrows]
+    assert 'a0' in independent_vars
+    nonzero_cond_vars = [v.swapcase() for v in independent_vars]
+    R = PolynomialRing(QQ, ['M', 'L', 'm', 'l'] + independent_vars + nonzero_cond_vars)
+    M, L, m, l= R('M'), R('L'), R('m'), R('l')
 
     def var(tet, edge):
         return tet_vars[6*tet + directed_edges.index(edge)]
 
-    rels = []
+    in_terms_of_indep_vars = {v:R(v) for v in independent_vars}
+    edge_gluings = EdgeGluings(gen_obs_class)
+
+    for around_one_edge in arrows_around_edges(manifold):
+        tet0, face0, edge0 = around_one_edge[0]
+        indep_var = R(var(tet0, edge0))
+        sign, m_e, l_e = 1, 0, 0
+        for tet1, face1, edge1 in around_one_edge[:-1]:
+            (tet2, face2, edge2), a_sign = edge_gluings[tet1, face1, edge1]
+            sign = a_sign * sign
+            m_e = m_e - sum(m_star[tet1, face1, e] for e in edge1)
+            l_e =  l_e - sum(l_star[tet1, face1, e] for e in edge1)
+            mvar = M if m_e > 0 else m
+            lvar = L if l_e > 0 else l
+            dep_var = var(tet2, edge2)
+            in_terms_of_indep_vars[dep_var] = sign*(mvar**abs(m_e))*(lvar**abs(l_e))*indep_var
+
+    tet_vars = [in_terms_of_indep_vars[v] for v in tet_vars]
+    rels = [R('a0') - 1, M*m - 1, L*l - 1]
     for tet in range(n):
-        a, b, c, d, e, f = [R(x + repr(tet)) for x in 'abcdef']
+        a, b, c, d, e, f = tet_vars[6*tet:6*(tet+1)]
         rels.append(c*d + a*f - b*e)
-        
-    for (tet0, face0, (u, v)), (tet1, face1, (x, y)), sign in EdgeGluings(gen_obs_class):
-        m_u = m_star[tet0, face0, u]
-        m_v = m_star[tet0, face0, v]
-        l_u = l_star[tet0, face0, u]
-        l_v = l_star[tet0, face0, v]
-        m = m_u + m_v
-        l = l_u + l_v
-        v0 = var(tet0, (u, v))
-        v1 = var(tet1, (x, y))
-        LHS, RHS = v0, sign*v1
 
-        if m < 0:
-            LHS *= M**-m
-        elif m > 0:
-            RHS *= M**m
+    # These last equations ensure the ptolemy coordinates are nonzero.
+    # For larger numbers of tetrahedra, this appears to make computing
+    # Groebner basis much faster even though there are extra variables
+    # compared the approach where one uses a single variable.
+    
+    for v in independent_vars:
+        rels.append(R(v) * R(v.swapcase()) - 1)
 
-        if l < 0:
-            LHS *= L**-l
-        elif l > 0:
-            RHS *= L**l
-
-        rels.append(LHS - RHS)
-
-    non_zero_rel = R('z')
-    for v in tet_vars:
-        non_zero_rel *= v
-    rels.append(non_zero_rel - 1)
-
-    rels.append(R('a0') - 1)
     return R.ideal(rels)
 
 def apoly_via_sage(manifold):
@@ -164,21 +213,13 @@ def sample_apoly_points_via_giac_rur(manifold, n):
     I = I + [p]
     return giac_rur.rational_unimodular_representation(I)
 
-def rur_for_dehn_filling(manifold, m, l):
+def rur_for_dehn_filling(manifold, a, b):
     import giac_rur
     I = extended_ptolemy_equations(manifold)
     R = I.ring()
-    M, L = R('M'), R('L')
-    RHS, LHS = 1, 1
-    if m < 0:
-        LHS *= M**(-m)
-    elif m > 0:
-        RHS *= M**m
-    if l < 0:
-        LHS *= L**(-l)
-    elif l > 0:
-        RHS *= L**l    
-    I = I + [LHS - RHS]
+    mvar = R('M') if a > 0 else R('m')
+    lvar = R('L') if b > 0 else R('l')
+    I = I + [mvar**abs(a) * lvar**abs(b) - 1]
     return giac_rur.rational_unimodular_representation(I)
 
 def test_as_cusped(manifold):

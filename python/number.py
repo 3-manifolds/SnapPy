d1bac1b983c78b5474db1690e419e3fe72a21408
Nathan Dunfield <nathan@dunfield.info>
2017-01-02 11:45:28 -0600

    Making work with SageMath 7.5

--- python/number.py
+++ python/number.py
@@ -2,47 +2,8 @@
 # RealField or ComplexField, with Sage Integers or Rationals, and with
 # other SnapPyNumbers.
 
-try:
-    from sage.libs.pari.gen import gen
-    try:
-        from sage.libs.pari.gen import pari
-        from sage.libs.pari.gen import (prec_words_to_dec,
-                                        prec_words_to_bits,
-                                        prec_bits_to_dec,
-                                        prec_dec_to_bits)
-    except ImportError: # Sage 6.1 or later needs the following
-        from sage.libs.pari.pari_instance import pari
-        from sage.libs.pari.pari_instance import (prec_words_to_dec,
-                                                  prec_words_to_bits,
-                                                  prec_bits_to_dec,
-                                                  prec_dec_to_bits)
-    from sage.all import PariError
-    shut_up  = lambda : None
-    speak_up = lambda : None   
-    _within_sage = True
-    
-except ImportError:
-    try:
-        from cypari.gen import pari
-        from cypari.gen import (
-            gen, PariError,
-            prec_words_to_dec,
-            prec_words_to_bits,
-            prec_bits_to_dec,
-            prec_dec_to_bits)
-        shut_up  = lambda : pari.shut_up()
-        speak_up = lambda : pari.speak_up()
-    except ImportError:
-        from cypari import pari
-        from cypari.gen import gen, PariError
-        from cypari.pari_instance import (
-            prec_words_to_dec,
-            prec_words_to_bits,
-            prec_bits_to_dec,
-            prec_dec_to_bits)
-        shut_up  = lambda : None
-        speak_up = lambda : None
-    _within_sage = False
+from .sage_helper import _within_sage
+from .pari import *
 
 import re
 strip_zeros = re.compile('(.*\..*?[0-9]{1})0*$')

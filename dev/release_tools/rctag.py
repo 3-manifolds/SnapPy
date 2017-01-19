aa6640dcbd2964e3c4c72fb69ecb630f61adb5ae
Nathan Dunfield <nathan@dunfield.info>
2017-01-19 09:52:52 -0600

    Improved rctag.py command

--- dev/release_tools/rctag.py
+++ dev/release_tools/rctag.py
@@ -8,8 +8,11 @@ Add a "release candidate" tag to wheels/eggs/tarballs.
 import sys, os, re, argparse
 parser = argparse.ArgumentParser(description='adds rc tags for uploading to testpypi.',
                                  epilog=docs, formatter_class=argparse.RawDescriptionHelpFormatter)
-parser.add_argument('-r', '--rctag', help='index for rc tag to be appended to version (e.g. -r2 -> rc2)',
-                    action='store', required=True)
+
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-r', '--rctag', help='index of rc tag to be added to version (e.g. -r2 -> rc2)',
+                    action='store')
+group.add_argument('-f', '--final', help='strip any rc tags', action='store_true')
 parser.add_argument('files', nargs='+')
 
 suffixes = re.compile('(.tar.gz|.zip|.whl|.egg)$')
@@ -22,7 +25,13 @@ if __name__ == '__main__':
             print('Skipping %s' % file)
         name = os.path.basename(file)
         path = os.path.dirname(file)
-        tag = 'rc' + args.rctag 
+        if args.final:
+            tag = ''
+        else:
+            tag = 'rc' + args.rctag 
         new_name = version_tag.sub('\g<1>-\g<2>%s\g<4>'%tag, name, 1)
-        print('%s --> %s' % (name, new_name))
-        os.rename(os.path.join(path, name), os.path.join(path, new_name))
+        if name != new_name:
+            print('%s --> %s' % (name, new_name))
+            os.rename(os.path.join(path, name), os.path.join(path, new_name))
+        else:
+            print('Skipping: %s' % name)

c3036e86e60c1e75e1fcc4ccfe3e683b79de0d12
Matthias Goerner <enischte@gmail.com>
2017-04-18 10:43:32 -0700

    Fixes to isomorphism signature headers: making char* const and adding kernel_namespace.h to the header since the c file defined the functions in the namespace already.

--- kernel/addl_code/isomorphism_signature.h
+++ kernel/addl_code/isomorphism_signature.h
@@ -8,6 +8,8 @@
 
 #include "SnapPea.h"
 
+#include "kernel_namespace.h"
+
 /* Compute the isomorphism signature for a given triangulation  */
 
 char* get_isomorphism_signature(Triangulation*);
@@ -15,6 +17,8 @@ char* get_isomorphism_signature(Triangulation*);
 /* Compute a triangulation from an isomorphism signature, return NULL if 
  * isomorphism signature is invalid */
 
-Triangulation* triangulation_from_isomorphism_signature(char*);
+Triangulation* triangulation_from_isomorphism_signature(const char*);
+
+#include "end_namespace.h"
 
 #endif

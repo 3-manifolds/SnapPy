6c5fdac07a763333b42126614060e594c8ccb3ab
Marc Culler <Marc Culler@localhost>
2017-05-31 11:55:29 -0500

    Adding new versions of togl/win32VC* which are statically linked against msvcr100.dll, so we don't have to install a 2010 vcredist package.

--- windows_exe/make.py
+++ windows_exe/make.py
@@ -29,7 +29,7 @@ os.system(this_python + " setup.py install")
 # Now build the .exe
 
 os.chdir("windows_exe")
-os.system("rm -rf build dist InstallSnappy-Python2.exe InstallSnappy-Python3.exe")
+os.system("rm -rf build dist InstallSnappy-Python%s.exe"%sys.version_info.major)
 if sys.version_info.major == 2:
     os.system(this_pyinstaller + " SnapPy_py2.spec")
     os.system("iscc InnoSnapPy_py2.iss")

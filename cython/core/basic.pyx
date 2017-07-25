8e95c4163dc3bbb24a0a881ae82ebd4a5b45c111
Nathan Dunfield <nathan@dunfield.info>
2017-07-25 15:11:13 -0500

    Made uAcknowledge silent again as there were too many pointless annoying warnings.

--- cython/core/basic.pyx
+++ cython/core/basic.pyx
@@ -250,8 +250,8 @@ cdef public void uLongComputationEnds() except*:
             UI_callback(interrupted=True)
 
 cdef public void uAcknowledge(const_char_ptr message):
-    sys.stderr.write(to_str(<char *> message))
-    sys.stderr.write('\n')
+    #sys.stderr.write(to_str(<char *> message))
+    #sys.stderr.write('\n')
     return
 
 cdef public void uAbortMemoryFull():

e2f725ac13c38f43e8880fb1eb1a9ddf2f451cd1
Nathan Dunfield <nathan@dunfield.info>
2017-07-26 10:07:39 -0500

    Updated pickle-test script for Python 3

--- dev/pickle-test.py
+++ dev/pickle-test.py
@@ -1,7 +1,8 @@
-import cPickle as pickle
 import snappy
 import multiprocessing
-import 
+import time
+
+n = 1000
 
 def complex_volume(M):
     return M.high_precision().complex_volume()
@@ -10,21 +11,26 @@ def normal_surfaces(M):
     return len(M.normal_surfaces())
 
 def test_one_core():
-    i = 0
-    for M in snappy.OrientableCuspedCensus(tet=9)[:1000]:
-        complex_volume(M)
-	i += 1
-    print i
+    return [complex_volume(M)
+            for M in snappy.OrientableCuspedCensus(tet=9)[:n]]
 
 def test_many_cores(cores=8):
-    p = multiprocessing.Pool(8)
-    manifolds = list(snappy.OrientableCuspedCensus(tet=9)[:1000])
-    print len(manifolds)
-    print len(p.map(complex_volume, manifolds))
+    pool = multiprocessing.Pool(8)
+    manifolds = list(snappy.OrientableCuspedCensus(tet=9)[:n])
+    return pool.map(complex_volume, manifolds)
     
 if __name__ == '__main__':
-    #test_many_cores()
-    test_one_core()
+    start = time.time()
+    one_ans = test_one_core()
+    elapsed = time.time() - start
+    print('One core took %.1f seconds' % elapsed)
 
+    start = time.time()
+    quick_ans = test_many_cores()
+    elapsed = time.time() - start
+    print('Eight cores took %.1f seconds' % elapsed)
+
+    print('Max difference was %f' % max(abs(a - b) for a, b in zip(one_ans, quick_ans)))
+    
 
 

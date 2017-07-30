b7f9a43b853eada546e629c8d9113c03650e6480
Nathan Dunfield <nathan@dunfield.info>
2017-07-30 09:27:22 -0500

    Again, the classic blunder...

---
+++ python/exceptions.py
@@ -0,0 +1,6 @@
+# Exceptions from the SnapPea kernel
+class SnapPeaFatalError(Exception):
+    """
+    This exception is raised by SnapPy when the SnapPea kernel
+    encounters a fatal error.
+    """

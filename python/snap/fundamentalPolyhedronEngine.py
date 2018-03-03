a8aa66fe7b6c04512590f58cafedcaac7997adaf
Matthias Goerner <enischte@gmail.com>
2018-03-03 13:25:50 -0800

    Adding comments to FundamentalPolyhedronEngine.

--- python/snap/fundamentalPolyhedronEngine.py
+++ python/snap/fundamentalPolyhedronEngine.py
@@ -310,6 +310,25 @@ class FundamentalPolyhedronEngine(McomplexEngine):
             self.mcomplex.GeneratorMatrices[-g] = _adjoint2(m)
 
     def matrices_for_presentation(self, G, match_snappea = False):
+        """
+        Given the result of M.fundamental_group(...) where M is the
+        cooresponding SnapPy.Manifold, return the matrices for that
+        presentation of the fundamental polyhedron.
+
+        The GeneratorMatrices computed here are for the face-pairing
+        presentation with respect to the fundamental polyhedron.
+        That presentation can be simplfied by M.fundamental_group(...)
+        and this function will compute the matrices for the simplified
+        presentation from the GeneratorMatrices.
+
+        If match_snappea is True, it will flip the signs of some of
+        the matrices to match the ones in the given G (which were determined
+        by the SnapPea kernel).
+
+        This makes the result stable when changing precision (when normalizing
+        matrices with determinant -1, sqrt(-1) might jump between i and -i when
+        increasing precision).
+        """
         
         num_generators = len(self.mcomplex.GeneratorMatrices) / 2
         matrices = [ self.mcomplex.GeneratorMatrices[g + 1]

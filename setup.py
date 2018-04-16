5e57edbe8c9ac35a2b5d735134c9222ea698d38d
Marc Culler <Marc Culler@localhost>
2018-04-16 14:25:25 -0500

    Make DirichletDomain.save work in Python 3.  Make DirichletDomains picklable.  Make it so we don't have to run python setup.py build twice after editing .pyx files in cython.core.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ documentation for snappy module, e.g.
   sudo python -m pip install "sphinx>=1.3"
 
 """
-import os, platform, shutil, site, subprocess, sys, sysconfig
+import os, platform, shutil, site, subprocess, sys, sysconfig, re
 from os.path import getmtime, exists
 from distutils.util import get_platform
 from distutils.ccompiler import get_default_compiler
@@ -430,8 +430,13 @@ try:
         cythonize(cython_sources,
                   compiler_directives={'embedsignature': True})
         cython_cpp_sources = [file for file in cython_cpp_sources if exists(file)]
+        new = max(os.path.getmtime(file) for file in glob('cython/core/*.pyx'))
+        for file in glob('cython/*.o'):
+            if os.path.getmtime(file) < new:
+                os.unlink(file)
         cythonize(cython_cpp_sources, language='c++',
                   compiler_directives={'embedsignature': True})
+        
 except ImportError:
     for file in cython_sources:
         base = os.path.splitext(file)[0]

a13b2c8e6473168fd72a9747f52e4be670ea804c
Marc Culler <culler@users.noreply.github.com>
2018-05-07 14:49:43 -0500

    Allow any number of tetrahedra.  Deal with subtleties related to unsigned chars vs chars.  Add a test with a big triangulation.

--- cython/core/pickle.pyx
+++ cython/core/pickle.pyx
@@ -11,13 +11,20 @@
 # (The peripheral curves are not preserved for manifolds which are not
 # orientable or not complete.)
 #
-# These functions are limited to manifolds with at most 2^15 - 1
-# tetrahedra and at most 255 cusps.  Also, the normal coordinates for
-# the peripheral curves are assumed to be in the interval [-128, 127]
+# These functions are limited to manifolds with at most 255 cusps
+# such that the normal coordinates for the peripheral curves are assumed
+# to be in the interval [-128, 127].  Also the Dehn filling coefficients
+# must be integers in that interval.  If these conditions do not hold
+# then the _to_string method can be used as an alternative. 
 #
 # Note that the byte sequences produced by pickle_triangulation
 # are very likely to contain null bytes, so care must be taken
 # in the code when converting between Python and C strings.
+
+cdef IS_HUGE = <unsigned char> 0x80
+cdef IS_BIG = <unsigned char> 0x40
+cdef IS_COMPLETE = <unsigned char> 0x20
+cdef HAS_SMALL_FILLING = <unsigned char> 0x10 
 
 cdef pickle_triangulation(c_Triangulation *tri):
     """
@@ -25,27 +32,35 @@ cdef pickle_triangulation(c_Triangulation *tri):
     and then serializing it.
     """
     cdef TriangulationData* tri_data
-    cdef int i, is_big, use_cusp_data
-    cdef char buf[5]
+    cdef int i, flag, use_cusp_data
+    cdef unsigned char buf[5]
 
     triangulation_to_data(tri, &tri_data)
 
     # Every pickle starts with b'pickle:'
     result = b'pickle:'
 
-    is_big = 1 if tri_data.num_tetrahedra > 127 else 0
-    # The first byte indicates the orientability type and whether the triangulation
-    # is big.
-    buf[0] = <char>tri_data.orientability
-    if is_big:
-        # A big triangulation is flagged by setting bit 7 of the first byte
-        # number as the first byte of the pickle.
-        buf[0] |= 0x80
-        buf[1] = <char>(tri_data.num_tetrahedra >> 8)
-        buf[2] = <char>(tri_data.num_tetrahedra & 0xff)
+    # The first byte uses the two least signficant bits to indicate the
+    # orientability type and other bits as flags.
+    buf[0] = <unsigned char>tri_data.orientability
+    # NOTE: tri_data.num_tetrahedra is a positive signed int.
+    if tri_data.num_tetrahedra > 65535:
+        flag = IS_HUGE
+        buf[0] |= IS_HUGE
+        buf[1] = tri_data.num_tetrahedra >> 24
+        buf[2] = (tri_data.num_tetrahedra >> 16) & 0xff
+        buf[3] = (tri_data.num_tetrahedra >> 8) & 0xff
+        buf[4] = tri_data.num_tetrahedra & 0xff
+        i = 5
+    elif tri_data.num_tetrahedra > 255:
+        flag = IS_BIG
+        buf[0] |= IS_BIG
+        buf[1] = tri_data.num_tetrahedra >> 8
+        buf[2] = tri_data.num_tetrahedra & 0xff
         i = 3
     else:
-        buf[1] = <char>tri_data.num_tetrahedra
+        flag = 0
+        buf[1] = tri_data.num_tetrahedra &0xff
         i = 2
 
     # We only use the cusp data if all cusps are complete.  Otherwise, when
@@ -64,30 +79,37 @@ cdef pickle_triangulation(c_Triangulation *tri):
         buf[i] = buf[i+1] = 0
     result += buf[:i+2]
     for i in range(tri_data.num_tetrahedra):
-        result += pickle_tetrahedron_data(&tri_data.tetrahedron_data[i],
-                                          is_big)
+        result += pickle_tetrahedron_data(&tri_data.tetrahedron_data[i], flag)
     return result + bytes(tri_data.name)
 
-cdef pickle_tetrahedron_data(c_TetrahedronData* data, int is_big):
+cdef pickle_tetrahedron_data(c_TetrahedronData* data, int flag):
     cdef int i, j, k, v, f
     cdef Py_ssize_t n = 0
-    cdef short x, mask, bit
-    cdef int count, curve
+    cdef unsigned short mask, bit
+    cdef int x, count, curve
     cdef char curve_buf[16]
-    cdef char buffer[16]
+    cdef unsigned char buffer[16]
 
     result = bytes()
-    # If this tet is in a big triangulation then we use 2 bytes in
-    # network order to represent the indices of its neighbors.
-    if is_big:
+    # If this tet is in a huge or big triangulation then we use 4 or 2
+    # bytes in network order to represent the indices of neighbors.
+    if flag & IS_HUGE:
         for i in range(4):
-            x = <short>data.neighbor_index[i]
-            buffer[n] = <char>(x >> 8)
-            buffer[n + 1] = <char>(x & 0xff)
+            x = data.neighbor_index[i]
+            buffer[n] = (x >> 24)
+            buffer[n + 1] = (x >> 16) & 0xff
+            buffer[n + 2] = (x >> 8) & 0xff
+            buffer[n + 3] = x & 0xff
+            n += 4
+    elif flag & IS_BIG:
+        for i in range(4):
+            x = data.neighbor_index[i]
+            buffer[n] = (x >> 8)
+            buffer[n + 1] = x & 0xff
             n += 2
     else:
         for i in range(4):
-            buffer[n] = <char>data.neighbor_index[i]
+            buffer[n] = data.neighbor_index[i]
             n += 1
 
     # Pack each permutation into a byte, like SnapPea does.
@@ -112,7 +134,7 @@ cdef pickle_tetrahedron_data(c_TetrahedronData* data, int is_big):
     for i in range(2):
         for j in range(2):
             mask = 0
-            count = 2
+            count = 0
             bit = 1
             for v in range(4):
                 for f in range(4):
@@ -122,8 +144,11 @@ cdef pickle_tetrahedron_data(c_TetrahedronData* data, int is_big):
                         curve_buf[count] = <char>curve
                         count += 1
                     bit = bit << 1
-            curve_buf[0] = <char>(mask >> 8)
-            curve_buf[1] = <char>(mask & 0xff)
+            buffer[0] = mask >> 8
+            buffer[1] = mask & 0xff
+            # Trust Cython to copy both chars and unsigned chars into our
+            # bytes object without changing any bits.
+            result += buffer[:2]
             result += curve_buf[:count]
     return result
 
@@ -135,80 +160,91 @@ cdef c_Triangulation* unpickle_triangulation(bytes pickle) except *:
     cdef c_TetrahedronData* tets = NULL
     cdef c_CuspData* cusps = NULL
     cdef c_Triangulation *tri
-    cdef char* p
+    cdef unsigned char* p
     cdef TriangulationData tri_data
-    cdef int i, num, is_big
+    cdef int i, num_tets, num_cusps
     cdef int n = 0
 
     header, tri_pickle = pickle.split(b':', 1)
     assert header == b'pickle', 'Invalid pickle byte sequence'
     p = tri_pickle
-    is_big = 1 if p[0] & 0x80 else 0
     tri_data.solution_type = not_attempted
     tri_data.volume = <Real> 0.0
-    tri_data.orientability = <c_Orientability>(p[0] & 0x7f)
+    tri_data.orientability = <c_Orientability>(p[0] & 0x3)
     n = 1
-    if is_big:
-        num = p[n] << 8 | p[n+1]
+    if p[0] & IS_HUGE:
+        num_tets = (<int>p[n]) << 24 | (<int>p[n+1]) << 16 | (<int>p[n+2]) << 8 | (<int>p[n+3])
+        n += 4
+    elif p[0] & IS_BIG:
+        num_tets = (<int>p[n]) << 8 | (<int>p[n+1])
         n += 2
     else:
-        num = p[n]
+        num_tets = p[n]
         n += 1
-    tri_data.num_tetrahedra = num
+    tri_data.num_tetrahedra = num_tets
     tri_data.num_or_cusps = p[n]
     tri_data.num_nonor_cusps = p[n+1]
     tri_data.CS_value_is_known = 0
     tri_data.CS_value = <Real>0.0
     n += 2
 
-    cdef num_cusps = tri_data.num_or_cusps + tri_data.num_nonor_cusps
+    num_cusps = tri_data.num_or_cusps + tri_data.num_nonor_cusps
     if num_cusps > 0:
         # Use malloc (not mymalloc) to allocate memory for the cusp
         # data.  We free the memory before returning.
         cusps = <c_CuspData*>malloc(num_cusps*sizeof(c_CuspData))
-        for i in range(tri_data.num_or_cusps):
-            cusps[i].topology = torus_cusp
-            cusps[i].m = <Real>0.0
-            cusps[i].l = <Real>0.0
-        for i in range(tri_data.num_or_cusps, num_cusps):
-            cusps[i].topology = Klein_cusp
-            cusps[i].m = <Real>0.0
-            cusps[i].l = <Real>0.0
+        if cusps:
+            for i in range(tri_data.num_or_cusps):
+                cusps[i].topology = torus_cusp
+                cusps[i].m = <Real>0.0
+                cusps[i].l = <Real>0.0
+            for i in range(tri_data.num_or_cusps, num_cusps):
+                cusps[i].topology = Klein_cusp
+                cusps[i].m = <Real>0.0
+                cusps[i].l = <Real>0.0
+        else:
+            raise RuntimeError, 'Failed to allocate memory for cusps'
     tri_data.cusp_data = cusps
 
     # Use malloc (not mymalloc) to allocate memory for the tetrahedra
     # data.  We free the memory before returning.
-    tets = <c_TetrahedronData*>malloc(num*sizeof(c_TetrahedronData))
+    tets = <c_TetrahedronData*>malloc(num_tets*sizeof(c_TetrahedronData))
+    if tets == NULL:
+        free(cusps)
+        raise RuntimeError, 'Failed to allocate memory for tets.'
     for i in range(tri_data.num_tetrahedra):
-        n = unpickle_tetrahedron_data(&tets[i], tri_pickle, n, is_big)
+        n = unpickle_tetrahedron_data(&tets[i], tri_pickle, n, p[0])
     tri_data.tetrahedron_data = tets
 
     # Create a new bytes object py_name containing the triangulation name,
     py_name = tri_pickle[n:]
     # then set tri_data.name to point to the internal buffer of py_name.
     tri_data.name = py_name
-    # Cython takes care of the details.
+    # Hopefully, Cython takes care of the details.
 
     data_to_triangulation(&tri_data, &tri)
-    if tets:
-        free(tets)
-    if cusps:
-        free(cusps)
+    free(tets)
+    free(cusps)
     return tri
 
 cdef int unpickle_tetrahedron_data(
-    c_TetrahedronData *data, bytes pickle, int start, int is_big) except *:
+    c_TetrahedronData *data, bytes pickle, int start, int flag) except *:
     cdef int i, j, v, f
     cdef int n = start
     cdef unsigned char perm
     cdef unsigned short mask, bit
     # Cython magic to allow using the internal buffer of the bytes object.
-    cdef char* p = pickle
+    cdef unsigned char* p = pickle
 
     # Get the neighbors.
-    if is_big:
+    if flag & IS_HUGE:
         for i in range(4):
-            data.neighbor_index[i] = <int>p[n] << 8 | <int>p[n+1]
+            data.neighbor_index[i] = ((<int>p[n]) << 24 | (<int>p[n+1]) << 16 |
+                                      (<int>p[n+2]) << 8 | (<int>p[n+3]))
+            n += 4
+    elif flag & IS_BIG:
+        for i in range(4):
+            data.neighbor_index[i] = (<int>p[n] << 8) | (<int>p[n+1])
             n += 2
     else:
         for i in range(4):
@@ -237,7 +273,10 @@ cdef int unpickle_tetrahedron_data(
             for v in range(4):
                 for f in range(4):
                     if mask & bit:
-                        data.curve[i][j][v][f] = p[n]
+                        # Curve data is signed.  The effect of casting an
+                        # unsigned char to a char is implementation-defined.
+                        # This will fail if any bits are changed.
+                        data.curve[i][j][v][f] = <char>p[n]
                         n += 1
                     else:
                         data.curve[i][j][v][f] = 0

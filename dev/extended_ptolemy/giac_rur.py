95ef4965efc3403a3f39ae3c8e5559378d9476d5
Matthias Goerner <enischte@gmail.com>
2018-06-02 15:25:07 -0700

    Making giac_rur.py work with both giacpy and sage?s giac interface.

--- dev/extended_ptolemy/giac_rur.py
+++ dev/extended_ptolemy/giac_rur.py
@@ -1,17 +1,43 @@
-from sage.all import QQ, PolynomialRing, NumberField
-import giacpy
-giac = giacpy.libgiac
+from sage.all import QQ, PolynomialRing, NumberField, var
 
+try:
+    # Old giacpy
+    from giacpy import libgiac as giac
+except ImportError:
+    try:
+        # giacpy was renamed to giacpy_sage
+        from giacpy_sage import libgiac as giac
+    except ImportError:
+        try:
+            # Giac became part of SageMath but giacpy_sage did not.
+            from sage.all import giac
+        except ImportError as e:
+            raise ImportError(e.args[0] +
+                              'Probably your version of SageMath is too old '
+                              'to ship with Giac.')
+        
 def rational_unimodular_representation(ideal):
     R = ideal.ring()
     vars = list(R.gens())
     J = giac(ideal.gens())
     rur = J.gbasis(vars, 'rur')
-    if rur[0] != 'rur':
-        raise ValueError('Could not find RUR. Is the variety 0-dimensional?')
+    # Yikes: giacpy_sage vs the giac interface give different types.
+    # So using repr.
+    if repr(rur[0]) != 'rur':
+        raise ValueError(('Could not find RUR, got %r instead. '
+                          'Is the variety 0-dimensional?') % rur[0])
+
+    # [x.sage() for rur[4:]] doesn't work with the giac interface (it tries
+    # to convert the slicing to a giac expression that giac does not understand)
+    #
+    # However, rur.sage()[0] gave some trouble, too.
+    #
+    # So we convert with .sage() after having checked we have rur, but before
+    # we inspect the other items.
+    rur = rur.sage()
 
     # Defining polynomial of number field, as a symbolic expression.
-    p = rur[2].sage()
+    p = rur[2]
     assert len(p.variables()) == 1
     v = p.variables()[0]
 
@@ -25,7 +51,7 @@ def rational_unimodular_representation(ideal):
         K = NumberField(q, 'a')
 
         def toK(f):
-            return K(T(S(f.sage())))
+            return K(T(S(f)))
 
         denom = toK(rur[3])
         rep = [toK(f)/denom for f in rur[4:]]

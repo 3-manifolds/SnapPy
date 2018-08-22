3d5d50c5dcbc9a9b0ae068d3a7e02feabb6fccd2
Fr?d?ric Chapoton <fchapoton2@gmail.com>
2018-08-22 16:55:30 +0000

    fix python3 details

--- dev/holonomy.py
+++ dev/holonomy.py
@@ -3,7 +3,7 @@ from sage.all import RealField, ComplexField, I
 
 M = snappy.ManifoldHP('m004')
 shapes = M.tetrahedra_shapes('rect')
-M.set_tetrahedra_shapes(filled_shapes=shapes, fillings=[(1,0)])
+M.set_tetrahedra_shapes(filled_shapes=shapes, fillings=[(1, 0)])
 RR = RealField(212)
 CC = ComplexField(25)
 pi = RR.pi()
@@ -13,9 +13,9 @@ for i in range(201):
     if i % 25 == 0:
         shapes = M.tetrahedra_shapes()
         z = shapes[1]
-        print i, CC(z['rect']), CC(z['log'])
+        print(i, CC(z['rect']), CC(z['log']))
 
 print(M.solution_type())
-print(map(CC, M.tetrahedra_shapes('rect')))
+print([CC(sh) for sh in M.tetrahedra_shapes('rect')])
 
     

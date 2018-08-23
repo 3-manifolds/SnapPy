2590a0c0c2c07f18e380cf670e2d2debb64a40a1
Frédéric Chapoton <5454848+fchapoton@users.noreply.github.com>
2018-08-23 15:56:34 +0000

    test_mflds.py : python3 fixes

--- dev/get_tri_refactor/test_mflds.py
+++ dev/get_tri_refactor/test_mflds.py
@@ -61,18 +61,18 @@ def get_triangulation_tester():
          'L13n9331(3,4)(2,3)(2,1)', 'K7_1', '6_1',
          '5^2_1(3,4)(1,-2)', '8_3^3', 'L104001', '12n123', '16n1235',
          'b++RL', 'b-+RRL', 'b+-RL', 'b--RRL',
-         'Braid[1,2,-1,-2]', 'DT:'+repr(M.DT_code()), 'DT[4,6,2]',
-         'DT['+M.DT_code(alpha=True) + ']',
-         'DT:'+M.DT_code(alpha=True),
+         'Braid[1,2,-1,-2]', 'DT:' + repr(M.DT_code()), 'DT[4,6,2]',
+         'DT[' + M.DT_code(alpha=True) + ']',
+         'DT:' + M.DT_code(alpha=True),
          'Bundle(S_{1,1}, [a_0, B_1])', 'Splitting(S_{1,0}, [b_1, A_0], [a_0,B_1])',
          ]
 
     for spec in specs:
         M = snappy.Manifold(spec)
-        print M, M.volume(),M.homology()
+        print(M, M.volume(), M.homology())
 
     for spec in specs:
         M = snappy.Triangulation(spec)
-        print M, M.homology()
+        print(M, M.homology())
 
 doctest.testmod()

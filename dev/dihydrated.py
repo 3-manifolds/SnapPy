b89b4bce411220ebdef667d05d418c6988b1076b
Frédéric Chapoton <5454848+fchapoton@users.noreply.github.com>
2018-08-26 08:38:49 +0000

    dihydrated.py : python3 fix for map

--- dev/dihydrated.py
+++ dev/dihydrated.py
@@ -1,21 +1,22 @@
 import snappy, string
 
+
 def str_to_int(x):
     return string.ascii_lowercase.index(x)
 
+
 def rehydrate(dehydration):
-    dehydration = map(str_to_int, dehydration)
+    dehydration = list(map(str_to_int, dehydration))
     num_tet = dehydration[0]
     i = 0
     which_tets = []
-    while 4*i < num_tet + 3:
-        which_tets.append(16*dehydration[2*i + 1]  + dehydration[2*i + 2])
+    while 4 * i < num_tet + 3:
+        which_tets.append(16 * dehydration[2 * i + 1]  + dehydration[2 * i + 2])
         i += 1
     which_gluings = dehydration[-num_tet:]
     return bytearray([num_tet] + which_tets + which_gluings)
-    
-                         
+
 
 b = rehydrate('cabbbbaeil')
-#M = snappy.Manifold('empty')
-#M._from_bytes(str(b))
+# M = snappy.Manifold('empty')
+# M._from_bytes(str(b))

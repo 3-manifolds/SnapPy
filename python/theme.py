4bf5bd0afbc956412e38a93646df5e4f0a4e50fd
Marc Culler <culler@users.noreply.github.com>
2019-07-14 09:04:27 -0500

    Make the readonly entry widgets in the browser look like labels.

--- python/theme.py
+++ python/theme.py
@@ -21,28 +21,14 @@ class SnapPyStyle:
                 test = Tk_._default_root.winfo_rgb('systemWindowBackgroundColor')
                 self.windowBG = 'systemWindowBackgroundColor'
                 self.groupBG = 'systemWindowBackgroundColor1'
+                self.subgroupBG = 'systemWindowBackgroundColor2'
             except:
                 # These will be wrong in dark mode
                 self.windowBG = '#ececec'
                 self.groupBG = '#e3e3e3'
+                self.subgroupBG = '#dbdbdb'
         else:
-            self.windowBG = self.groupBG = ttk_style.lookup('TLabelframe', 'background')
+            self.windowBG = ttk_style.lookup('TLabelframe', 'background')
+            self.groupBG = self.subgroupBG = self.windowBG
         self.font_info = fi = Font(font=ttk_style.lookup('TLabel', 'font')).actual()
         fi['size'] = abs(fi['size']) # Why would the size be negative???
-
-    # def configure(self):
-    #     ttk_style = self.ttk_style
-
-# def SnapPyStyle(root):
-#     if root is None:
-#         if Tk_._default_root is None:
-#             root = Tk_.Tk(className='snappy')
-#             root.withdraw()
-#         else:
-#             root = Tk_._default_root
-#     try:
-#         return root.style
-#     except AttributeError:
-#         root.style = style = _SnapPyStyle()
-#         style.configure()
-#         return style 

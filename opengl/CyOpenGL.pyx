1aa9e2d13dd4604dbbb97d0b69be3542d4cde30d
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2019-09-07 20:16:19 -0400

    Making GL_context class into a method cyglSetStandardLighting. Renaming non-OpenGL methods prefixed by gl.

--- opengl/CyOpenGL.pyx
+++ opengl/CyOpenGL.pyx
@@ -68,69 +68,68 @@ cdef class vector3:
     def __truediv__(self, scalar):
         return vector3([self.x/scalar, self.y/scalar, self.z/scalar])
 
-cdef class GL_context:
+def cyglSetStandardLighting():
     """
     Sets up our default OpenGL environment.
     """
 
-    def __cinit__(self):
-        # Lighting intensities and location
-        cdef GLfloat* ambient = [0.75, 0.75, 0.75, 1.0]
-        cdef GLfloat* lightdiffuse = [0.8, 0.8, 0.8, 1.0]
-        cdef GLfloat* lightspecular = [0.3, 0.3, 0.3, 1.0]
-        # 2 units from the center, up and to the right
-        # we should be able to control the light
-        cdef GLfloat* lightposition0 = [0.3, 0.5, 3.0, 1.0]
-        cdef GLfloat* lightposition1 = [0.3, -0.5, -3.0, 1.0]
-
-        ## Set parameters that apply to all objects:
-        # Remove hidden stuff
-        glEnable(GL_DEPTH_TEST)
-        # Allow transparency
-        # glEnable(GL_ALPHA_TEST)
-        glEnable(GL_BLEND)
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-        # Enable anti-aliasing of points lines and polygons
-        glEnable(GL_POINT_SMOOTH)
-        glEnable(GL_LINE_SMOOTH)
-        #Below call is deprecated and causes odd behavior on some systems.
-        #glEnable(GL_POLYGON_SMOOTH)
-        # Use lights and materials to determine colors
-        glEnable(GL_LIGHTING)
-        # Make the Color command control ambient and diffuse material colors
-        glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
-        glEnable(GL_COLOR_MATERIAL)
-        # Use interpolated shading (although colors are constant on faces)
-        glShadeModel(GL_SMOOTH)
-        # Define the counter-clockwise (outer) face to be the front.
-        glFrontFace(GL_CCW)
-        # Rasterize front and back Faces
-        glDisable(GL_CULL_FACE)
-        ## Set up lighting
-        # Allow different properties on fronts and backs
-        glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, 1.0)
-        # Compute specular reflections from the eye
-        glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, True)
-        # Ambient light intensity for the entire scene
-        glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient)
-        # Enable two lights, with attenuation
-        glEnable(GL_LIGHT0)
-        glLightfv(GL_LIGHT0, GL_POSITION, lightposition0)
-        glLightfv(GL_LIGHT0, GL_DIFFUSE, lightdiffuse)
-        glLightfv(GL_LIGHT0, GL_SPECULAR, lightspecular)
-        glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION,  1.0)
-        glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.1)
-        glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, 0.08)
-        glDisable(GL_LIGHT1)
-        glLightfv(GL_LIGHT1, GL_POSITION, lightposition1)
-        glLightfv(GL_LIGHT1, GL_DIFFUSE, lightdiffuse)
-        glLightfv(GL_LIGHT1, GL_SPECULAR, lightspecular)
-        glLightf(GL_LIGHT1, GL_CONSTANT_ATTENUATION,  1.0)
-        glLightf(GL_LIGHT1, GL_LINEAR_ATTENUATION, 0.1)
-        glLightf(GL_LIGHT1, GL_QUADRATIC_ATTENUATION, 0.08)
-        # Use the Model View Matrix
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
+    # Lighting intensities and location
+    cdef GLfloat* ambient = [0.75, 0.75, 0.75, 1.0]
+    cdef GLfloat* lightdiffuse = [0.8, 0.8, 0.8, 1.0]
+    cdef GLfloat* lightspecular = [0.3, 0.3, 0.3, 1.0]
+    # 2 units from the center, up and to the right
+    # we should be able to control the light
+    cdef GLfloat* lightposition0 = [0.3, 0.5, 3.0, 1.0]
+    cdef GLfloat* lightposition1 = [0.3, -0.5, -3.0, 1.0]
+    
+    ## Set parameters that apply to all objects:
+    # Remove hidden stuff
+    glEnable(GL_DEPTH_TEST)
+    # Allow transparency
+    # glEnable(GL_ALPHA_TEST)
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    # Enable anti-aliasing of points lines and polygons
+    glEnable(GL_POINT_SMOOTH)
+    glEnable(GL_LINE_SMOOTH)
+    #Below call is deprecated and causes odd behavior on some systems.
+    #glEnable(GL_POLYGON_SMOOTH)
+    # Use lights and materials to determine colors
+    glEnable(GL_LIGHTING)
+    # Make the Color command control ambient and diffuse material colors
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
+    glEnable(GL_COLOR_MATERIAL)
+    # Use interpolated shading (although colors are constant on faces)
+    glShadeModel(GL_SMOOTH)
+    # Define the counter-clockwise (outer) face to be the front.
+    glFrontFace(GL_CCW)
+    # Rasterize front and back Faces
+    glDisable(GL_CULL_FACE)
+    ## Set up lighting
+    # Allow different properties on fronts and backs
+    glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, 1.0)
+    # Compute specular reflections from the eye
+    glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, True)
+    # Ambient light intensity for the entire scene
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient)
+    # Enable two lights, with attenuation
+    glEnable(GL_LIGHT0)
+    glLightfv(GL_LIGHT0, GL_POSITION, lightposition0)
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightdiffuse)
+    glLightfv(GL_LIGHT0, GL_SPECULAR, lightspecular)
+    glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION,  1.0)
+    glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.1)
+    glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, 0.08)
+    glDisable(GL_LIGHT1)
+    glLightfv(GL_LIGHT1, GL_POSITION, lightposition1)
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, lightdiffuse)
+    glLightfv(GL_LIGHT1, GL_SPECULAR, lightspecular)
+    glLightf(GL_LIGHT1, GL_CONSTANT_ATTENUATION,  1.0)
+    glLightf(GL_LIGHT1, GL_LINEAR_ATTENUATION, 0.1)
+    glLightf(GL_LIGHT1, GL_QUADRATIC_ATTENUATION, 0.08)
+    # Use the Model View Matrix
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
 
 cdef class GLobject:
     """
@@ -988,7 +987,7 @@ cdef class HoroballScene:
 
 # Methods to translate and rotate our scene.
 
-cdef glTranslateScene(s, x, y, mousex, mousey):
+cdef cyglTranslateScene(s, x, y, mousex, mousey):
     cdef GLdouble X, Y
     cdef GLdouble mat[16]
 
@@ -998,7 +997,7 @@ cdef glTranslateScene(s, x, y, mousex, mousey):
     glTranslatef(X, Y, 0.0)
     glMultMatrixd(mat)
 
-cdef glRotateScene(xcenter, ycenter, zcenter, Xangle, Yangle):
+cdef cyglRotateScene(xcenter, ycenter, zcenter, Xangle, Yangle):
     cdef GLdouble mat[16]
 
     glGetDoublev(GL_MODELVIEW_MATRIX, mat)
@@ -1259,8 +1258,8 @@ class OpenGLWidget(RawOpenGLWidget):
 
     def do_AutoSpin(self):
         self.makeCurrent()
-        glRotateScene(self.xcenter, self.ycenter, self.zcenter,
-                      self.Xangle, self.Yangle)
+        cyglRotateScene(self.xcenter, self.ycenter, self.zcenter,
+                        self.Xangle, self.Yangle)
         self.tkRedraw()
 
         if self.autospin:
@@ -1283,8 +1282,8 @@ class OpenGLWidget(RawOpenGLWidget):
         self.makeCurrent()
         self.Xangle = 0.5 * (event.x - self.xmouse)
         self.Yangle = 0.5 * (event.y - self.ymouse)
-        glRotateScene(self.xcenter, self.ycenter, self.zcenter,
-                      self.Xangle, self.Yangle)
+        cyglRotateScene(self.xcenter, self.ycenter, self.zcenter,
+                        self.Xangle, self.Yangle)
         self.tkRedraw()
         self.tkRecordMouse(event)
 
@@ -1293,7 +1292,7 @@ class OpenGLWidget(RawOpenGLWidget):
         Perform translation of scene.
         """
         self.makeCurrent()
-        glTranslateScene(0.05, event.x, event.y, self.xmouse, self.ymouse)
+        cyglTranslateScene(0.05, event.x, event.y, self.xmouse, self.ymouse)
         self.tkRedraw()
         self.tkRecordMouse(event)
 

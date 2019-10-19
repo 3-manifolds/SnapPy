f2f8d83555887b0586f790f7aefba09e9cd2d658
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2019-10-19 14:49:19 -0400

    Adding class RaytracingEngineData that extracts the information for the shader bindings from the manifold.

---
+++ dev/raytracing_data.py
@@ -0,0 +1,335 @@
+from snappy.snap import t3mlite as t3m
+
+from sage.all import matrix
+
+from math import cos, sin, cosh, sinh, sqrt
+
+from snappy.snap.kernel_structures import *
+from snappy.snap.mcomplex_base import *
+
+from snappy.verify.cuspCrossSection import *
+
+def convert_o13_to_o31_matrix(m):
+    return matrix([
+            [ m[(i+1)%4][(j+1)%4] for j in range(4) ]
+            for i in range(4) ])
+
+def convert_o13_to_o31_vec(v):
+    return [ v[(i+1)%4] for i in range(4) ]
+
+def convert_o31_to_o13_matrix(m):
+    return matrix([
+            [ m[(i+3)%4][(j+3)%4] for j in range(4) ]
+            for i in range(4) ])
+
+def convert_o31_to_o13_vec(v):
+    return [ v[(i+3)%4] for i in range(4) ]
+
+def unit_time_vector_to_O13_hyperbolic_translation(v):
+    def diag(i, j):
+        if i == j:
+            if i == 0:
+                return -1
+            else:
+                return 1
+        return 0
+
+    v1 = [1 + v[0]] + v[1:]
+
+    return matrix(
+        [[ x * y / (1 + v[0]) + diag(i, j)
+           for i, x in enumerate(v1) ]
+         for j, y in enumerate(v1) ])
+
+def unit_3_vector_and_distance_to_O13_hyperbolic_translation(v, d):
+    return unit_time_vector_to_O13_hyperbolic_translation(
+        [ cosh(d)] + [ sinh(d) * x for x in v])
+
+def O13_x_rotation(angle):
+    c = cos(angle)
+    s = sin(angle)
+    return matrix(
+        [[ 1.0, 0.0, 0.0, 0.0],
+         [ 0.0, 1.0, 0.0, 0.0],
+         [ 0.0, 0.0,   c,   s],
+         [ 0.0, 0.0,  -s,   c]])
+
+def O13_y_rotation(angle):
+    c = cos(angle)
+    s = sin(angle)
+    return matrix(
+        [[ 1.0, 0.0, 0.0, 0.0],
+         [ 0.0,   c, 0.0,  -s],
+         [ 0.0, 0.0, 1.0, 0.0],
+         [ 0.0,   s, 0.0,   c]])
+
+def O13_z_rotation(angle):
+    c = cos(angle)
+    s = sin(angle)
+    return matrix(
+        [[ 1.0, 0.0, 0.0, 0.0],
+         [ 0.0,   c,   s, 0.0],
+         [ 0.0,  -s,   c, 0.0],
+         [ 0.0, 0.0, 0.0, 1.0]])
+
+def complex_to_R13_light_vector(z):
+
+    if z == Infinity:
+        return [ 1.0, 1.0, 0.0, 0.0 ]
+
+    z_re = z.real()
+    z_im = z.imag()
+    z_abs_sqr = z_re ** 2 + z_im ** 2
+    denom = z_abs_sqr + 1
+
+    return [ 1.0,
+             (z_abs_sqr - 1) / denom,
+             2 * z_re / denom,
+             2 * z_im / denom ]
+
+def complex_and_height_to_R13_time_vector(z, t):
+    
+    z_re = z.real()
+    z_im = z.imag()
+    z_abs_sqr = z_re ** 2 + z_im ** 2
+    
+    denom = (z_abs_sqr + (t + 1) ** 2)
+    
+    poincare = [ (z_abs_sqr + t ** 2 - 1) / denom,
+                 (2 * z_re) / denom,
+                 (2 * z_im) / denom ]
+
+    poincare_rsqr = sum([x**2 for x in poincare])
+    klein_factor = 2.0 / (1 + poincare_rsqr)
+
+    return R13_normalise(
+        [ 1.0,
+          klein_factor * poincare[0],
+          klein_factor * poincare[1],
+          klein_factor * poincare[2] ])          
+
+def remove_column(m, k):
+    return [ [ m[i][j] for j in range(4) if j != k ]
+             for i in range(3) ]
+
+def R13_dot(u, v):
+    return -u[0]*v[0] + u[1]*v[1] + u[2]*v[2] + u[3]*v[3]
+
+def R31_dot(u, v):
+    return u[0]*v[0] + u[1]*v[1] + u[2]*v[2] - u[3]*v[3]
+
+def R13_normalise(v):
+    denom = sqrt(abs(R13_dot(v,v)))
+
+    return [ v[i] / denom for i in range(4) ]
+
+def O13_orthonormalize(m):
+    result = [ ]
+    for i in range(0,4):
+        v = m[(i+1) % 4]
+        for j in range(i):
+            s = R13_dot(v, result[j])
+            v = [ x - s * y for x, y in zip(v, result[j]) ]
+        result.append(R13_normalise(v))
+    return matrix([result[3]] + result[:3])
+
+def matrix3_det(m):
+    return (  m[0][0] * m[1][1] * m[2][2]
+            + m[0][1] * m[1][2] * m[2][0]
+            + m[0][2] * m[1][0] * m[2][1]
+            - m[0][2] * m[1][1] * m[2][0]
+            - m[0][0] * m[1][2] * m[2][1]
+            - m[0][1] * m[1][0] * m[2][2])
+
+def R13_plane_from_R13_light_vectors(light_vectors):
+    light_vectors = [ (-a, b, c, d) for a, b, c, d in light_vectors ]
+    return R13_normalise(
+        [ (-1) ** j * matrix3_det( remove_column(light_vectors, j) )
+          for j in range(4) ])
+
+def make_tet_planes(tet_vert_positions): #outward facing for positively oriented tetrahedra
+    v0, v1, v2, v3 = tet_vert_positions
+    return [ R13_plane_from_R13_light_vectors([v1, v3, v2]),
+             R13_plane_from_R13_light_vectors([v0, v2, v3]),
+             R13_plane_from_R13_light_vectors([v0, v3, v1]),
+             R13_plane_from_R13_light_vectors([v0, v1, v2]) ]
+
+def _compute_ideal_and_finite_point_on_horosphere_for_vertex(tet, V0, i):
+    V1, V2, V3 = t3m.VerticesOfFaceCounterclockwise[t3m.comp(V0)]
+
+    if i == 1:
+        V1, V2, V3 = V2, V3, V1
+    if i == 2:
+        V1, V2, V3 = V3, V1, V2
+
+    cusp_length = tet.horotriangles[V0].lengths[V0 | V1 | V2]
+    pts  = [ tet.SnapPeaIdealVertices[V] for V in [V0, V1, V2]]
+    if pts[0] != Infinity:
+
+        def invDiff(a, b):
+            if a == Infinity:
+                return 0
+            return 1 / (a - b)
+
+        pts[1] = invDiff(pts[1], pts[0])
+        pts[2] = invDiff(pts[2], pts[0])
+
+    base_length = abs(pts[2] - pts[1])
+    
+    # print(cusp_length)
+
+    if pts[0] != Infinity:
+        return pts[0], (pts[0], cusp_length / base_length)
+    else:
+        print(base_length / cusp_length)
+
+        return pts[0], (pts[1], base_length / cusp_length)
+
+def _compute_R13_horosphere_for_vertex(tet, V0, i):
+    ideal_point, (z, t) = _compute_ideal_and_finite_point_on_horosphere_for_vertex(
+        tet, V0, i)
+
+    light_vector = complex_to_R13_light_vector(ideal_point)
+    
+    horosphere_point = complex_and_height_to_R13_time_vector(z, t)
+
+    s = R13_dot(light_vector, horosphere_point)
+
+    return [ x / s for x in light_vector ]
+
+class RaytracingDataEngine(McomplexEngine):
+    @staticmethod
+    def from_manifold(manifold, areas = 0.1):
+        m = t3m.Mcomplex(manifold)
+
+        r = RaytracingDataEngine(m, manifold)
+
+        t = TransferKernelStructuresEngine(m, manifold)
+
+        t.choose_and_transfer_generators(
+            compute_corners = True,
+            centroid_at_origin = False)
+        t.reindex_cusps_and_transfer_peripheral_curves()
+        t.add_shapes(manifold.tetrahedra_shapes('rect'))
+
+        c = RealCuspCrossSection(m)
+        c.add_structures()
+        c.normalize_cusps(areas)
+
+        r._compute_O13_matrices()
+        r._add_O13_matrices_to_faces()
+        r._add_R13_vertices()
+        r._add_R13_planes_to_faces()
+
+        r._add_R13_horospheres_to_vertices()
+
+        return r
+
+    def __init__(self, mcomplex, snappy_manifold):
+        super(RaytracingDataEngine, self).__init__(mcomplex)
+        self.snappy_manifold = snappy_manifold
+
+    def _compute_O13_matrices(self):
+        G = self.snappy_manifold.fundamental_group(
+            simplify_presentation = False)
+        self.O13_matrices = { 0 : G.O31('') }
+        for i, g in enumerate(G.generators()):
+            j = i + 1
+            self.O13_matrices[ j] = G.O31(g)
+            self.O13_matrices[-j] = G.O31(g.upper())
+
+    def _add_O13_matrices_to_faces(self):
+        for tet in self.mcomplex.Tetrahedra:
+            tet.O13_matrices = {
+                F: self.O13_matrices[-g]
+                for F, g in tet.GeneratorsInfo.items() }
+
+    def _add_R13_vertices(self):
+        for tet in self.mcomplex.Tetrahedra:
+            tet.R13_vertices = {
+                V: complex_to_R13_light_vector(z)
+                for V, z in tet.SnapPeaIdealVertices.items() }
+
+    def _add_R13_planes_to_faces(self):
+        for tet in self.mcomplex.Tetrahedra:
+            planes = make_tet_planes(
+                [ tet.R13_vertices[v]
+                  for v in t3m.ZeroSubsimplices])
+            tet.R13_planes = {
+                F : plane
+                for F, plane in zip(t3m.TwoSubsimplices, planes) }
+
+    def _add_R13_horospheres_to_vertices(self):
+
+        for tet in self.mcomplex.Tetrahedra:
+            for V in t3m.ZeroSubsimplices:
+                print("=====")
+                print(tet.SnapPeaIdealVertices[V])
+                for i in range(3):
+                    print _compute_R13_horosphere_for_vertex(tet, V, i)
+
+        for tet in self.mcomplex.Tetrahedra:
+            tet.R13_horospheres = {
+                V : _compute_R13_horosphere_for_vertex(tet, V, 0)
+                for V in t3m.ZeroSubsimplices }
+
+    def get_initial_tet_num(self):
+        return self.mcomplex.ChooseGenInitialTet.Index
+
+    def get_uniform_bindings(self):
+        otherTetNums = [
+            tet.Neighbor[F].Index
+            for tet in self.mcomplex.Tetrahedra
+            for F in t3m.TwoSubsimplices ]
+
+        entering_face_nums = [
+            tet.Gluing[F][f]
+            for tet in self.mcomplex.Tetrahedra
+            for f, F in enumerate(t3m.TwoSubsimplices) ]
+
+        SO13tsfms = [
+            tet.O13_matrices[F]
+            for tet in self.mcomplex.Tetrahedra
+            for F in t3m.TwoSubsimplices ]
+
+        planes = [
+            tet.R13_planes[F]
+            for tet in self.mcomplex.Tetrahedra
+            for F in t3m.TwoSubsimplices ]
+
+        horospheres = [
+            tet.R13_horospheres[V]
+            for tet in self.mcomplex.Tetrahedra
+            for V in t3m.ZeroSubsimplices ]
+
+        return {
+            'otherTetNums' : ('int[]', otherTetNums),
+            'entering_face_nums' : ('int[]', entering_face_nums),
+            'SO13tsfms' : ('mat4[]', SO13tsfms),
+            'planes' : ('vec4[]', planes),
+            'horospheres' : ('vec4[]', horospheres)}
+
+    def fix_boost_and_tetnum(self, boost, tet_num):
+        
+        boost = O13_orthonormalize(boost)
+
+        entry_F = -1
+
+        for i in range(100):
+            pos = boost.transpose()[0]
+            tet = self.mcomplex.Tetrahedra[tet_num]
+
+            amount, F = max(
+                [ (R13_dot(pos, tet.R13_planes[F]), F)
+                  for F in t3m.TwoSubsimplices ])
+
+            if F == entry_F:
+                break
+            if amount < 0.0000001:
+                break
+            
+            boost = O13_orthonormalize(tet.O13_matrices[F] * boost)
+            tet_num = tet.Neighbor[F].Index
+            entry_F = F
+
+        return boost, tet_num
---
+++ dev/raytracing_shaders/fragment.glsl
@@ -0,0 +1,314 @@
+#version 150
+
+//--------------------------------------------
+//Global Variables
+//--------------------------------------------
+out vec4 out_FragColor;
+
+//-------------------------------------------
+//Translation & Utility Variables
+//--------------------------------------------
+uniform vec2 screenResolution;
+uniform float fov;
+
+uniform int tetNum;
+uniform mat4 currentBoost;
+uniform float currentWeight;
+
+uniform int maxSteps;
+uniform float maxDist;
+uniform int subpixelCount;
+uniform float edgeThickness;
+uniform float contrast;
+uniform int perspectiveType;
+uniform int viewMode;
+uniform int multiScreenShot;
+uniform vec2 tile;
+uniform vec2 numTiles;
+//
+uniform vec4 planes[##arrayLength##];   // ##arrayLength## gets replaced when we loadShaders in Main.js
+uniform int otherTetNums[##arrayLength##]; 
+uniform int entering_face_nums[##arrayLength##]; 
+uniform float weights[##arrayLength##]; 
+uniform mat4 SO31tsfms[##arrayLength##];
+
+uniform float gradientThreshholds[5];
+uniform vec3 gradientColours[5];
+
+uniform vec4 horospheres[##arrayLength##];
+
+
+// inf             1
+//   v0 -------- v2
+//    | `.    .' |
+//    |   `. '   |
+//    |   . `.   |
+//    | .'    `. |
+//   v3 -------- v1
+// z               0
+
+  float R31_dot(vec4 u, vec4 v){
+    return u.x*v.x + u.y*v.y + u.z*v.z - u.w*v.w; // Lorentz Dot
+  }
+
+  float R31_norm_inv(vec4 v){
+    return inversesqrt(abs(R31_dot(v,v)));
+  }
+  
+  vec4 R31_normalise(vec4 v){
+    return v*R31_norm_inv(v);
+  }
+
+float geodesicParameterPlanes(vec4 samplePoint, vec4 dualPoint1, vec4 dualPoint2){
+  // "distance" from a geodesic defined by two (assumed perpendicular) geodesic planes, this is not quite distance, need to asinh(sqrt( result ))
+  float dot1 = -R31_dot(samplePoint, dualPoint1);
+  vec4 dualPointPerp = R31_normalise(dualPoint2 - R31_dot(dualPoint1, dualPoint2) * dualPoint1); // should be precalculated if this is a main feature
+  float dot2 = -R31_dot(samplePoint, dualPointPerp);
+  // float dot3 = -R31_dot(dualPoint1, dualPoint2);
+  return dot1*dot1 + dot2*dot2;
+  // return dot1*dot1 * (1.0 + dot3*dot3) + dot2*dot2 + 2.0*dot1*dot2*dot3;
+}
+
+float triangleBdryParam(vec4 samplePoint, int tetNum, int exit_face){
+  vec4 exit_dual_point = planes[4*tetNum + exit_face];
+  float smallest_p = 100000000.0;
+  for(int face=0; face<4; face++){
+      if(face != exit_face){  // find p when we hit that face
+          int index = 4*tetNum + face;
+          float new_p = geodesicParameterPlanes(samplePoint, exit_dual_point, planes[index]);
+          if(new_p < smallest_p){
+            smallest_p = new_p;
+          }   
+      }
+  }
+  return smallest_p;
+}
+
+/// --- Ray-trace code --- ///
+
+  float hyp_dist(vec4 u, vec4 v){
+    float bUV = -R31_dot(u,v);
+    if (bUV < 1.0) {return 0.0;}
+    else {return acosh(bUV);}  
+  } 
+
+float param_to_isect_line_with_horosphere(vec4 line_start, vec4 line_dir, vec4 horosphere)
+{
+    float start_dot = R31_dot(horosphere, line_start);
+    float dir_dot = R31_dot(horosphere, line_dir);
+    
+    float a = dir_dot * dir_dot + 1;
+    float b = 2 * dir_dot * start_dot;
+    float c = start_dot * start_dot - 1;
+    
+    float disc = b * b - 4 * a * c;
+    if (disc < 0) {
+        return 200000000.0;
+    }
+    
+    float result = (-b - sign(a) * disc) / (2 * a);
+    if (result < 0) {
+        return 200000000.0;
+    }
+
+    return result;
+}
+
+float param_to_isect_line_with_plane(vec4 line_start, vec4 line_dir, vec4 plane){
+    float denom = R31_dot(plane, line_dir);
+    if(denom == 0.0){ return 200000000.0; }  // bigger than the initial smallest_p value we will accept
+    /// solve: R31_dot(plane, line_start + p * line_dir) = 0
+    ///        R31_dot(plane, line_start) + p * R31_dot(plane, line_dir) = 0
+    return (-R31_dot(plane, line_start)) / denom;
+  }
+
+bool horosphere_hit = false;
+
+vec4 ray_trace_through_hyperboloid_tet(vec4 init_pos, vec4 init_dir, int tetNum, int entry_face, out int exit_face){
+
+    ///Given shape of a tet and a ray, find where the ray exits and through which face
+    float smallest_p = 100000000.0;
+    for(int face=0; face<4; face++){
+        if(face != entry_face){  // find p when we hit that face
+            int index = 4*tetNum + face;
+            if(R31_dot(init_dir, planes[index]) > 0.0){ 
+                float p = param_to_isect_line_with_plane(init_pos, init_dir, planes[index]);
+                // if ((-10000.0 <= p) && (p < smallest_p)) {
+                if (p < smallest_p) {  
+                    /// negative values are ok if we have to go backwards a little to get through the face we are a little the wrong side of
+                    /// Although this can apparently get caught in infinite loops in an edge
+
+                    /// if we are on an edge then we don't in fact move as we go through this tet: t = 0.0
+                    /// also allow tiny negative values, which will come up from floating point errors. 
+                    /// surface normals check should ensure that even in this case we make progress through 
+                    /// the triangles around an edge
+                    smallest_p = p;
+                    exit_face = face;
+                }
+            }
+        }
+    }
+
+    for (int vertex = 0; vertex < 4; vertex++) {
+        float p = param_to_isect_line_with_horosphere(init_pos, init_dir, horospheres[4 * tetNum + vertex]);
+        if (p < smallest_p) {
+            horosphere_hit = true;
+        }
+    }
+
+    return R31_normalise( init_pos + smallest_p * init_dir );
+}
+
+float ray_trace(vec4 init_pt, vec4 init_dir, float dist_to_go, int tetNum){
+    int entry_face = -1;   /// starts off with no entry face
+    int exit_face = -1;
+    float total_face_weight = 0.0;
+    vec4 new_pt;
+    int index;
+    mat4 tsfm;
+    vec4 new_dir;
+    for(int i=0; i<maxSteps; i++){
+      new_pt = ray_trace_through_hyperboloid_tet(init_pt, init_dir, tetNum, entry_face, exit_face);
+      dist_to_go -= hyp_dist(init_pt, new_pt);
+      if (dist_to_go <= 0.0){ break; }
+      if(edgeThickness > 0.00001){
+        if(triangleBdryParam(new_pt, tetNum, exit_face) < edgeThickness){ break; }}
+        // in fact pow(sinh(radius in hyperbolic units),2.0). However, sinh^2 is monotonic for 
+        // positive values so we get correct behaviour by comparing without the sinh^2. 
+      index = 4*tetNum + exit_face;
+      total_face_weight += weights[ index ];
+      entry_face = entering_face_nums[ index ];
+      tsfm = SO31tsfms[ index ];
+      tetNum = otherTetNums[ index ];
+
+      new_dir = init_dir + R31_dot(init_dir, new_pt) * new_pt; // orthonormal decomp, no normalisation yet
+      init_pt = new_pt * tsfm;  
+      init_dir = R31_normalise( new_dir * tsfm ); 
+    }
+    if(viewMode == 0){ return total_face_weight; } // Cannon-Thurston
+    else if(viewMode == 1){ return 0.5*maxDist - dist_to_go; } // Distance
+    else{ return float(tetNum);}
+}
+
+/// --- Graph-trace code --- ///
+
+float amountOutsideTetrahedron(vec4 v, int tetNum, out int biggest_face) {
+  float biggest_amount = -100000.0;
+  float amount;
+  for(int i = 0; i < 4; i++){
+    amount = R31_dot( v, planes[4*tetNum + i] );
+    if( amount > biggest_amount ){
+      biggest_amount = amount;
+      biggest_face = i;
+    }
+  }
+  return biggest_amount; 
+}
+
+// Get point at distance dist on the geodesic from u in the direction vPrime
+vec4 pointOnGeodesic(vec4 u, vec4 vPrime, float dist){
+  return u*cosh(dist) + vPrime*sinh(dist);
+}
+
+float graph_trace(inout vec4 goal_pt, inout int tetNum, out mat4 tsfm){ // tsfm is matrix to send goal_pt to its image in the tetrahedron coordinates it is in
+  // similar function to ray_trace, but different algorithm
+  float total_face_weight = 0.0;
+  int entry_face = -1;
+  int index;
+  int biggest_face;
+  tsfm = mat4(1.0);
+  for(int i=0; i<maxSteps; i++){
+      if ( amountOutsideTetrahedron(goal_pt, tetNum, biggest_face) > 0.0000001 && biggest_face != entry_face ){
+        index = 4*tetNum + biggest_face;
+        entry_face = entering_face_nums[ index ];
+        tetNum = otherTetNums[ index ];
+        total_face_weight += weights[ index ];
+        goal_pt *= SO31tsfms[ index ];
+        tsfm *= SO31tsfms[ index ];
+        // if (R31_dot(goal_pt, goal_pt) > -0.5){ return -1000.0; } // errors accumulate and we get junk!
+      }
+      else{ break; }
+    }
+    return total_face_weight;
+  }
+
+/// --- Colour gradient code --- ///
+
+int find_band(float t, float threshholds[5]){
+    for(int j=1;j<4;j++){
+        if(t < threshholds[j]){return j;}
+    }
+    return 4;
+}
+vec4 general_gradient(float t, float threshholds[5], vec3 colours[5]){
+    int i = find_band(t, threshholds);
+    return vec4( mix(colours[i-1], colours[i],(t - threshholds[i-1])/(threshholds[i] - threshholds[i-1]) ), 1.0);
+}
+
+/// --- Ray init pt and directions code --- ///
+
+vec4 get_ray_dir(vec2 xy){ 
+    xy = 0.2 * xy;
+    float z = 0.1/tan(radians(fov*0.5));
+    vec4 p =  R31_normalise(vec4(xy,-z,0.0));
+    return p;
+}
+
+float get_signed_count(vec2 xy){
+  vec4 init_pt;
+  vec4 init_dir;
+  float weight = 0.0;
+  if(perspectiveType == 0){ // material
+    init_pt = vec4(0.0,0.0,0.0,1.0);
+    init_dir = get_ray_dir(xy);
+    init_pt *= currentBoost;
+    init_dir *= currentBoost; 
+    weight = ray_trace(init_pt, init_dir, maxDist, tetNum);
+  }
+  else{ // ideal
+    float foo = 0.5*dot(xy, xy);
+    init_pt = vec4(xy.x, xy.y, foo, foo + 1.0);   // parabolic transformation magic by Saul
+    init_dir = vec4(xy.x, xy.y, foo - 1.0, foo);
+    init_pt *= currentBoost;
+    init_dir *= currentBoost; 
+    mat4 tsfm = mat4(1.0);
+    int currentTetNum = tetNum;  // gets modified inside graph_trace
+
+    weight = graph_trace(init_pt, currentTetNum, tsfm);  // get us to the tetrahedron containing init_pt
+    // init_pt *= tsfm;  // the point gets moved back in graph_trace
+    init_dir *= tsfm;  // move the direction back to here
+    if(viewMode != 0){ weight = 0.0; } // the other modes don't have a cumulative count from moving to the correct tetrahedron
+    weight += ray_trace(init_pt, init_dir, maxDist, currentTetNum);
+  }
+
+  if(viewMode == 0){ // Cannon-Thurston
+    weight += currentWeight;
+  }
+  return weight;
+}
+
+void main(){
+  vec2 xy = (gl_FragCoord.xy - 0.5*screenResolution.xy)/screenResolution.x;
+  if(multiScreenShot == 1){  // Return multiple 4096x4096 screenshots that can be combined in, e.g. Photoshop.
+    // Here screenResolution is really tileResolution;
+    xy = (xy + tile - 0.5*(numTiles - vec2(1.0,1.0))) / numTiles.x;
+  }
+  float total_weight = 0.0;
+  for(int i=0; i<subpixelCount; i++){
+    for(int j=0; j<subpixelCount; j++){
+      vec2 offset = ( (float(1+2*i), float(1+2*j))/float(2*subpixelCount) - vec2(0.5,0.5) ) / screenResolution.x;
+      total_weight += get_signed_count(xy + offset);
+    }
+  }
+  float weight = total_weight/float(subpixelCount*subpixelCount); // average over all subpixels
+
+  weight = contrast * weight;
+  weight = 0.5 + 0.5*weight/(abs(weight) + 1.0);  //faster than atan, similar
+  // weight = 0.5 + atan(0.3 * weight)/PI;  // between 0.0 and 1.0
+  out_FragColor = general_gradient(weight, gradientThreshholds, gradientColours);
+
+  if (horosphere_hit)  {
+      out_FragColor = vec4(1,0,0,1);
+  }
+
+}

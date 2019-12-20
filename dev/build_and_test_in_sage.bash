23f9b2968455effa65120911c9401586fe6f9812
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2019-12-20 10:35:14 -0800

    sagemath/sagemath:8.8 docker image has no git, so copying source into docker image instead of cloning it inside docker image.

--- dev/build_and_test_in_sage.bash
+++ dev/build_and_test_in_sage.bash
@@ -1,36 +1,54 @@
 #!/bin/bash
 
-# Fail if any of the steps below fail
 set -e
 
-echo "================================================"
+cp -r /snappy .
+cd snappy
+
+echo
+echo
+echo "==================================================================="
 echo "SageMath version"
 sage --version
 
-echo "================================================"
+echo
+echo
+echo "==================================================================="
 echo "Installing FXrays"
 sage -pip install -U FXrays wheel
 
-echo "================================================"
+echo
+echo
+echo "==================================================================="
 echo "Installing PLink"
 sage -pip install -U https://github.com/3-manifolds/PLink/archive/master.zip
 
-echo "================================================"
+echo
+echo
+echo "==================================================================="
 echo "Installing snappy_manifolds"
 sage -pip install -U https://bitbucket.org/t3m/snappy_manifolds/get/tip.zip
 
-echo "================================================"
+echo
+echo
+echo "==================================================================="
 echo "Installing spherogram"
 sage -pip install -U https://bitbucket.org/t3m/spherogram/get/tip.zip
 
-echo "================================================"
+echo
+echo
+echo "==================================================================="
 echo "Building SnapPy"
 sage -python setup.py build
 
-echo "================================================"
+echo
+echo
+echo "==================================================================="
 echo "Testing SnapPy"
 sage -python setup.py test
 
-echo "================================================"
+echo
+echo
+echo "==================================================================="
 echo "Building SnapPy docs"
 sage -python setup.py build_docs

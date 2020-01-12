4897a353d15dd6535b308c92903c066e4f015cce
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2020-01-12 13:59:12 -0800

    Hyperbolic raytracing: adding R13_time_vector_to_upper_halfspace.

--- dev/raytracing/hyperboloid_utilities.py
+++ dev/raytracing/hyperboloid_utilities.py
@@ -132,6 +132,17 @@ def complex_and_height_to_R13_time_vector(z, t):
           klein_factor * poincare[1],
           klein_factor * poincare[2] ])          
 
+def R13_time_vector_to_upper_halfspace(v):
+    klein = [ v[1] / v[0], v[2] / v[0], v[3] / v[0] ]
+    klein_sqr = sum([x**2 for x in klein])
+    poincare_factor = 1.0 / (1.0 + sqrt(1.0 - klein_sqr))
+    a, b, c = [ x * poincare_factor for x in klein ]
+
+    denom = (a - 1.0) ** 2 + b ** 2 + c ** 2
+    return  [                         2.0 * b / denom,
+                                      2.0 * c / denom,
+             (1.0 - a ** 2 - b ** 2 - c ** 2) / denom ]
+
 def remove_column(m, k):
     return [ [ m[i][j] for j in range(4) if j != k ]
              for i in range(3) ]

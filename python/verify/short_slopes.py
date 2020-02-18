0a82bbd6e7c0f3f30646bb046967843720990e6d
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2020-02-17 23:15:16 -0800

    verify.short_slopes: stricter checking of interval types.

--- python/verify/short_slopes.py
+++ python/verify/short_slopes.py
@@ -6,7 +6,7 @@ if _within_sage:
     from sage.all import gcd
 
     from sage.rings.real_mpfi import is_RealIntervalFieldElement
-
+    from sage.rings.complex_interval import is_ComplexIntervalFieldElement
 
     # python's sqrt only work for floats
     # They would fail or convert to float loosing precision
@@ -52,8 +52,20 @@ def short_slopes_from_translations(translations, length = 6):
     m_tran, l_tran = translations
 
     if _within_sage:
-        if is_RealIntervalFieldElement(m_tran):
+        if is_ComplexIntervalFieldElement(m_tran):
+            raise Exception("Meridian translation expected to be real")
+        if is_RealIntervalFieldElement(l_tran):
+            raise Exception("Longitude translation expected to be complex")
+
+        is_interval_1 = is_RealIntervalFieldElement(m_tran)
+        is_interval_2 = is_ComplexIntervalFieldElement(l_tran)
+
+        if is_interval_1 != is_interval_2:
+            raise Exception("Mismatch of non-intervals and intervals.")
+
+        if is_interval_1:
             return _verified_short_slopes_from_translations(translations, length)
+        
     
     return _unverified_short_slopes_from_translations(translations, length)
 

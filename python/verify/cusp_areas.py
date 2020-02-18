a09b5c1f8e5b1be4233300f5065eda713157795c
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2020-02-17 22:08:45 -0800

    Fix to verify.cusp_areas._unverified_unbiased_cusp_areas_from_cusp_area_matrix so that it works with SnapPy numbers that cannot be compared against None.

--- python/verify/cusp_areas.py
+++ python/verify/cusp_areas.py
@@ -165,16 +165,20 @@ def _find_minimal_stopper(cusp_area_matrix, assigned_areas):
 def _unverified_unbiased_cusp_areas_from_cusp_area_matrix(
                                                 cusp_area_matrix):
     num_cusps = cusp_area_matrix.dimensions()[0]
+    num_pending = num_cusps
 
     result = num_cusps * [ None ]
 
-    while None in result:
+    while num_pending > 0:
         stop_size, (i, j) = _find_minimal_stopper(cusp_area_matrix, result)
 
         if result[i] is None:
             result[i] = stop_size
-        if result[j] is None:
-            result[j] = stop_size
+            num_pending -= 1
+        if i != j:
+            if result[j] is None:
+                result[j] = stop_size
+                num_pending -= 1
 
     return result
 

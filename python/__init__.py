30c59686600b108fd9399269e3e8dfbe1346ce38
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2020-02-17 18:35:00 -0800

    Adding Manifold.cusp_area_matrix.

--- python/__init__.py
+++ python/__init__.py
@@ -238,6 +238,107 @@ def isometry_signature(
 Manifold.isometry_signature = isometry_signature
 ManifoldHP.isometry_signature = isometry_signature
 
+def cusp_area_matrix(manifold, method = 'trigDependentTryCanonize',
+                     verified = False, bits_prec = None):
+    
+    """
+    This function returns a matrix that can be used to check whether
+    cusp neighborhoods of given areas are disjoint: let a_i and a_j be
+    the areas of cusp neighborhoods about the cusps indexed i and j.
+    Then the cusp neighborhoods are disjoint (respectively, the cusp
+    neighborhood embeds if i and j are equal) if a_i * a_j is less
+    than or equal to the entry (i,j) of the cusp area matrix. Note
+    that the "if" becomes "if and only if" if we pick the "maximal
+    cusp area matrix".
+
+    This function can operate in different ways (determined by
+    ``method``). By default (``method='trigDependentTryCanonize'``),
+    it returns a result which can be suboptimal and non-deterministic
+    but is quicker to compute and sufficies for many applications::
+
+        >>> def print_matrix(m): # Overcome SnapPy/SageMath differences
+        ...     s = repr(m)
+        ...     s = s.replace('matrix([', '').replace('])', '').replace(',', '')
+        ...     print('\\n'.join([line.strip() for line in s.split('\\n')]))
+        >>> M = Manifold("s776")
+        >>> print_matrix(M.cusp_area_matrix()) # doctest: +NUMERIC12
+        [28.0000000000000 7.00000000000000 6.99999999999999]
+        [7.00000000000000 21.4375000000000 7.00000000000000]
+        [6.99999999999999 7.00000000000000 21.4375000000000]
+
+    If ``method='maximal'`` is specified, the result is the "maximal
+    cusp area matrix", thus it is optimal and an invariant of the
+    manifold with labeled cusps. Note that the "maximal cusp area
+    matrix" is only available as verified computation and thus
+    requires passing ``verified = True``::
+
+        sage: print_matrix(M.cusp_area_matrix(method = 'maximal', verified=True)) # doctest: +NUMERIC6
+        [28.0000000000?  7.0000000000?  7.0000000000?]
+        [ 7.0000000000?  28.000000000? 7.00000000000?]
+        [ 7.0000000000? 7.00000000000?   28.00000000?]
+        
+    If ``verified = True`` is specified and ``method`` is not ``maximal``,
+    the entries are all guaranteed to be less than the corresponding ones
+    in the maximal cusp area matrix (more precisely, the lower end point
+    of the interval is guaranteed to be less than the true value of the
+    corresponding maximal cusp area matrix entry)::
+    
+        sage: print_matrix(M.cusp_area_matrix(verified=True, bits_prec=70)) # doctest: +NUMERIC15
+        [ 28.000000000000000?  7.0000000000000000?  7.0000000000000000?]
+        [ 7.0000000000000000? 21.4375000000000000?  7.0000000000000000?]
+        [ 7.0000000000000000?  7.0000000000000000? 21.4375000000000000?]
+
+    For expert users:
+
+    Besides the two values above, ``method`` can be ``trigDependent``:
+    this result is also fast to compute by making the assumption that
+    cusp neighborhoods are not only disjoint but also in "standard
+    form" with respect to the triangulation (i.e., lifting a cusp
+    neighborhood to a horoball in the universal cover, it intersects a
+    geodesic tetrahedron in three but not four
+    faces). ``trigDependentTryCanonize`` is calling ``trigDependent``
+    after trying to "proto-canonize" (a copy of) the triangulation
+    since this often produces a matrix that is closer to the maximal
+    cusp area matrix, for example::
+
+        >>> M = Manifold("o9_35953")
+        >>> print_matrix(M.cusp_area_matrix(method = 'trigDependent')) # doctest: +NUMERIC9
+        [72.9848715318467 12.7560424258060]
+        [12.7560424258060 6.65567118002656]
+        >>> print_matrix(M.cusp_area_matrix(method = 'trigDependentTryCanonize')) # doctest: +NUMERIC9
+        [72.9848715318466 12.7560424258060]
+        [12.7560424258060 62.1043047674605]
+
+    Compare to maximal area matrix::
+
+        sage: M.cusp_area_matrix(method = 'maximal', verified = True, bits_prec = 100) # doctest: +NUMERIC15
+        [       72.984871531846664? 12.7560424258059765562778?]
+        [12.7560424258059765562778?     62.104304767460978078?]
+
+    """
+
+    if method == 'maximal':
+        if not verified:
+            raise NotImplementedError("Maximal cusp area matrix only "
+                                      "avaiable as verified computation. "
+                                      "Pass verified = True.")
+        return verify.verified_maximal_cusp_area_matrix(
+            manifold, bits_prec = bits_prec)
+    if method in ['trigDependent', 'trigDependentTryCanonize']:
+        if method == 'trigDependentTryCanonize':
+            manifold = manifold.copy()
+            manifold.canonize()
+
+        return verify.triangulation_dependent_cusp_area_matrix(
+            manifold, verified = verified, bits_prec = bits_prec)
+
+    raise RuntimeError("method passed to cusp_area_matrix must be "
+                       "'trigDependent', 'trigDependentTryCanonize', "
+                       "or 'maximal'.")
+
+Manifold.cusp_area_matrix = cusp_area_matrix
+ManifoldHP.cusp_area_matrix = cusp_area_matrix
+
 def cusp_translations(manifold, areas = None, canonize = True,
                       verified = False, bits_prec = None):
     """

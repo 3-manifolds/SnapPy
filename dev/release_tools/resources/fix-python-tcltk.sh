629ecaaf8bbd68444c4b62359864bd09e70435da
Nathan Dunfield <nathan@dunfield.info>
2020-09-07 22:12:30 -0500

    macOS frameworks Tk: improvements to fix-python-tcltk.sh

--- dev/release_tools/resources/fix-python-tcltk.sh
+++ dev/release_tools/resources/fix-python-tcltk.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 FIX_TK=false
 FIX_TCL=false
+# This last option is only needed when Python is started by a symbolic
+# link (e.g. in a virtualenv) or similar.
+FIX_TK_INTO_TCL=false
 PY=3.8
 TCLTK=8.6 # Changing this probably requires a new tkinter.
 
@@ -13,27 +16,35 @@ while true; do
 	--python) PY=$2 ; shift ; shift ;;
 	--tk) FIX_TK=true ; shift ;;
 	--tcl) FIX_TCL=true ; shift ;;
+	--tk-into-tcl) FIX_TK_INTO_TCL=true ; shift ;;
 	--) shift ;;
 	*) break ;;
     esac
 done
-if [ ${FIX_TK} == false ] && [ ${FIX_TCL} == false ] ; then
-    echo "Specify --tk or --tcl or both." >&2 ; exit 1
-fi
+#if [ ${FIX_TK} == false ] && [ ${FIX_TCL} == false ] ; then
+#    echo "Specify --tk or --tcl or both." >&2 ; exit 1
+#fi
 
 TK_FRAMEWORK=/Library/Frameworks/Tk.framework/Versions/${TCLTK}/
 TCL_FRAMEWORK=/Library/Frameworks/Tcl.framework/Versions/${TCLTK}/
 PYTHON_LIB=/Library/Frameworks/Python.framework/Versions/${PY}/lib/
+
+echo ${TK_FRAMEWORK}
+echo ${TCL_FRAMEWORK}
+echo ${PYTHON_LIB}
 echo
 if [ $FIX_TK == true ] ; then 
     echo Installing Tk ${TCLTK} in Python ${PY}.
-    sudo cp  ${TK_FRAMEWORK}Tk ${PYTHON_LIB}libtk${TCLTK}.dylib
-    sudo cp ${TK_FRAMEWORK}libtkstub${TCLTK}.a ${PYTHON_LIB}
-    sudo cp -R ${TK_FRAMEWORK}Resources/Scripts/ ${PYTHON_LIB}tk${TCLTK}/
+    cp ${TK_FRAMEWORK}Tk ${PYTHON_LIB}libtk${TCLTK}.dylib
+    cp ${TK_FRAMEWORK}libtkstub${TCLTK}.a ${PYTHON_LIB}
+    cp -R ${TK_FRAMEWORK}Resources/Scripts/ ${PYTHON_LIB}tk${TCLTK}/
 fi
 if [ $FIX_TCL == true ] ; then 
     echo Installing Tcl ${TCLTK} in Python ${PY}.
-    sudo cp  ${TCL_FRAMEWORK}Tcl ${PYTHON_LIB}libtcl${TCLTK}.dylib
-    sudo cp ${TCL_FRAMEWORK}libtclstub${TCLTK}.a ${PYTHON_LIB}
-    sudo cp -R ${TCL_FRAMEWORK}Resources/Scripts/ ${PYTHON_LIB}tcl${TCLTK}/
+    cp ${TCL_FRAMEWORK}Tcl ${PYTHON_LIB}libtcl${TCLTK}.dylib
+    cp ${TCL_FRAMEWORK}libtclstub${TCLTK}.a ${PYTHON_LIB}
+    cp -R ${TCL_FRAMEWORK}Resources/Scripts/ ${PYTHON_LIB}tcl${TCLTK}/
+fi
+if [ $FIX_TK_INTO_TCL == true ] ; then
+    sudo cp -R ${TK_FRAMEWORK}Resources/Scripts/ ${TCL_FRAMEWORK}Resources/Scripts/tk${TCLTK}/
 fi

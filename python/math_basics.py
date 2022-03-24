28c5e152924a026018c2567d65fd745919d98cf9
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2022-03-23 18:04:34 -0700

    Adding xgcd to math_basics.

--- python/math_basics.py
+++ python/math_basics.py
@@ -4,13 +4,14 @@ from functools import reduce
 import operator
 
 __all__ = [ 'prod',
+            'xgcd',
             'is_RealIntervalFieldElement',
             'is_Interval',
             'correct_min',
             'corred_max' ]
 
 if _within_sage:
-    from sage.all import prod
+    from sage.all import prod, xgcd
     from sage.rings.real_mpfi import is_RealIntervalFieldElement
     from sage.rings.complex_interval_field import is_ComplexIntervalField
 
@@ -34,6 +35,37 @@ else:
             return reduce(operator.mul, L)
         else:
             return 1
+
+    def xgcd(a, b):
+        """
+        Returns a triple ``(g,s,t)`` such that `g = s\cdot a+t\cdot b = \gcd(a,b)`.
+
+        >>> xgcd(56, 44)
+        (4, 4, -5)
+        >>> xgcd(5, 7)
+        (1, 3, -2)
+        >>> xgcd(-5, -7)
+        (1, -3, 2)
+        >>> xgcd(5, -7)
+        (1, 3, 2)
+        >>> xgcd(-5, 7)
+        (1, -3, -2)
+        """
+
+        old_r, r = a, b
+        old_s, s = 1, 0
+        old_t, t = 0, 1
+
+        while r != 0:
+            q = old_r // r
+            old_r, r = r, old_r - q * r
+            old_s, s = s, old_s - q * s
+            old_t, t = t, old_t - q * t
+
+        if old_r > 0:
+            return  old_r,  old_s,  old_t
+        else:
+            return -old_r, -old_s, -old_t
 
     def is_RealIntervalFieldElement(x):
         """

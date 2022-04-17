3-manifolds/SnapPy

commit f9879d04a68ee61721ae36487b09fb480db78bcb

Drilling: instead of modifying the list of start_pieces, modify the unprocessed start pieces itself.

python/drilling/moves.py
@@ -14,7 +14,6 @@ from typing import Sequence, Optional, Union, Tuple, List, Dict, Mapping
 __all__ = ['one_four_move', 'two_three_move']
 
 def one_four_move(given_pieces : Sequence[GeodesicPiece],
-                  start_pieces : Dict[int, GeodesicPiece],
                   verified : bool) -> Sequence[GeodesicPiece]:
 
     tet : Tetrahedron = given_pieces[0].tet
@@ -152,19 +151,17 @@ def one_four_move(given_pieces : Sequence[GeodesicPiece],
                     for new_tet in new_tets.values() ]
             else:
                 allowed_end_corners = [ (new_tets[end_subsimplex], end_subsimplex) ]
-            old_piece.replace_by(
-                _retrace_geodesic_piece(
-                    old_piece.index,
-                    new_tets,
-                    new_tets[start_subsimplex],
-                    start_subsimplex,
-                    end_cell_dimension,
-                    r13_endpoints[0],
-                    r13_endpoints[1],
-                    retrace_direction,
-                    start_pieces,
-                    verified,
-                    allowed_end_corners = allowed_end_corners))
+            _retrace_geodesic_piece(
+                old_piece,
+                new_tets,
+                new_tets[start_subsimplex],
+                start_subsimplex,
+                end_cell_dimension,
+                r13_endpoints[0],
+                r13_endpoints[1],
+                retrace_direction,
+                verified,
+                allowed_end_corners = allowed_end_corners)
 
     start_point : Endpoint = given_pieces[0].endpoints[0]
     end_point : Endpoint = given_pieces[-1].endpoints[1]
@@ -187,7 +184,6 @@ def one_four_move(given_pieces : Sequence[GeodesicPiece],
     return new_pieces
 
 def two_three_move(given_pieces : Sequence[GeodesicPiece],
-                   start_pieces : Dict[int, GeodesicPiece],
                    verified : bool) -> Sequence[GeodesicPiece]:
     """
     piece is assumed to go from a vertex to opposite face.
@@ -401,18 +397,16 @@ def two_three_move(given_pieces : Sequence[GeodesicPiece],
                 new_start_subsimplex = old_to_new_tets[j][i].image(start_subsimplex)
 
                 if new_start_subsimplex == new_face:
-                    old_piece.replace_by(
-                        _retrace_geodesic_piece(
-                            old_piece.index,
-                            new_tets,
-                            new_tet,
-                            new_face,
-                            end_cell_dimension,
-                            start_point, end_point,
-                            trace_end - trace_start,
-                            start_pieces,
-                            verified,
-                            allowed_end_corners = allowed_end_corners))
+                    _retrace_geodesic_piece(
+                        old_piece,
+                        new_tets,
+                        new_tet,
+                        new_face,
+                        end_cell_dimension,
+                        start_point, end_point,
+                        trace_end - trace_start,
+                        verified,
+                        allowed_end_corners = allowed_end_corners)
                     break
             else:
                 raise Exception("No match")
@@ -438,7 +432,7 @@ _swap_perms = { (f0, f1) : _swap_perm(i, j)
                 for j, f1 in enumerate(simplex.TwoSubsimplices) }
 
 def _retrace_geodesic_piece(
-        index : int,
+        old_piece : GeodesicPiece,
         tets : Union[Mapping[int,Tetrahedron], Sequence[Tetrahedron]],
         tet : Tetrahedron,
         face : int,
@@ -446,7 +440,6 @@ def _retrace_geodesic_piece(
         start_point,
         end_point,
         trace_direction : int,
-        start_pieces : Dict[int, GeodesicPiece],
         verified : bool,
         allowed_end_corners : Optional[Sequence[Tuple[Tetrahedron, int]]] = None):
 
@@ -455,6 +448,8 @@ def _retrace_geodesic_piece(
         epsilon = 0
     else:
         epsilon = compute_epsilon(RF)
+
+    index = old_piece.index
 
     direction = end_point - start_point
 
@@ -542,17 +537,20 @@ def _retrace_geodesic_piece(
                         "This is either due to a lack of precision or an "
                         "implementation bug.")
 
-            p = GeodesicPiece.create_and_attach(
-                index,
-                tet,
-                [ Endpoint(start_point + param * direction, face),
-                  Endpoint(end_point, end_cell) ][::trace_direction])
+            endpoints = [ Endpoint(start_point + param * direction, face),
+                          Endpoint(end_point, end_cell) ][::trace_direction]
 
             if dimension_end_cell == 3 and trace_direction < 0:
-                start_pieces[index] = p
+                old_piece.tet = tet
+                tet.geodesic_pieces.append(old_piece)
+                old_piece.endpoints = endpoints
+                pieces.append(old_piece)
+            else:
+                pieces.append(
+                    GeodesicPiece.create_and_attach(index, tet, endpoints))
 
-            pieces.append(p)
-            return pieces[::trace_direction]
+            old_piece.replace_by(pieces[::trace_direction])
+            return
 
         pieces.append(
             GeodesicPiece.create_and_attach(
@@ -577,7 +575,8 @@ def _retrace_geodesic_piece(
                 GeodesicPiece.create_face_to_vertex_and_attach(
                     index,
                     tet, Endpoint(start_point + hit_param * direction, face), trace_direction))
-            return pieces[::trace_direction]
+            old_piece.replace_by(pieces[::trace_direction])
+            return
     else:
         raise Exception(
             "Too many steps when re-tracing a geodesic piece. "

python/drilling/subdivide.py
@@ -18,26 +18,24 @@ def traverse_geodesics_to_subdivide(
         for piece in pieces:
             piece.tet.geodesic_pieces.append(piece)
 
-    start_pieces = { pieces[0].index : pieces[0] for pieces in all_pieces }
+    start_pieces = [ pieces[0] for pieces in all_pieces ]
 
-    for index in list(sorted(start_pieces.keys())):
-        debug.check_consistency_segments(debug.flatten_link_list(start_pieces[index]))
+    for start_piece in start_pieces:
+        debug.check_consistency_segments(debug.flatten_link_list(start_piece))
 
-    for index in list(sorted(start_pieces.keys())):
-        last_piece = _traverse_geodesic_to_subdivide(start_pieces[index], start_pieces, mcomplex.verified)
+    for start_piece in start_pieces:
+        last_piece = _traverse_geodesic_to_subdivide(start_piece, mcomplex.verified)
 
     return _find_and_index_all_tetrahedra(last_piece.tet)
 
 def _traverse_geodesic_to_subdivide(
         start_piece : GeodesicPiece,
-        start_pieces : Dict[int, GeodesicPiece],
         verified : bool) -> GeodesicPiece:
     
     debug.check_consistency_2(start_piece)
 
     end_piece, piece = one_four_move(
         [start_piece.prev, start_piece],
-        start_pieces,
         verified)
     
     debug.check_consistency_2(piece)
@@ -47,17 +45,17 @@ def _traverse_geodesic_to_subdivide(
 
         if piece.is_face_to_vertex():
             
-            piece = two_three_move([piece.prev, piece], start_pieces, verified)
+            piece = two_three_move([piece.prev, piece], verified)
 
             debug.check_consistency_2(piece)
 
             return piece
         
-        piece, next_piece = one_four_move([piece], start_pieces, verified)
+        piece, next_piece = one_four_move([piece], verified)
 
         debug.check_consistency_2(piece)
 
-        piece = two_three_move([piece.prev, piece], start_pieces, verified)
+        piece = two_three_move([piece.prev, piece], verified)
 
         debug.check_consistency_2(piece)
 

commit b021df061aefd927d999461c8bcfd856e33c9e5f

Drilling: fixing a nasty bug introduced in commit f9879d04 where the incorrect piece was assumed to be the start piece and modified in place.

python/drilling/__init__.py
@@ -492,4 +492,10 @@ def dummy_function_for_additional_doctests():
         ...
         snappy.drilling.exceptions.GeodesicCloseToCoreCurve: The given geodesic is very close to a core curve and might intersect it.
 
+    A particular tricky case in terms testing that the start piece is correctly
+    handled by 2-3 moves (in particular, commit f9879d04 introduced a bug):
+
+        >>> Manifold("m004").drill_words(['CAC','CCbC']).canonical_retriangulation().triangulation_isosig(ignore_orientation=False)
+        'qLvvLvAMQQQkcgimopkllmpkonnnpixcaelchapewetvrn_bcaaBbBBbaBaBbB'
+
     """

python/drilling/moves.py
@@ -357,7 +357,6 @@ def two_three_move(given_pieces : Sequence[GeodesicPiece],
             if next_old_piece:
                 old_endpoint = next_old_piece.endpoints[trace_end]
                 end_point = O13_embeddings[1-j] * old_endpoint.r13_point
-                next_old_piece.replace_by([])
             else:
                 old_endpoint = old_piece.endpoints[trace_end]
                 end_point = O13_embeddings[j] * old_endpoint.r13_point
@@ -393,12 +392,18 @@ def two_three_move(given_pieces : Sequence[GeodesicPiece],
                         for i, new_tet in enumerate(new_tets)
                         if old_to_new_tets[j][i].image(end_subsimplex) == simplex.TwoSubsimplices[j] ]
 
+            if next_old_piece:
+                piece_to_replace = next_old_piece
+                old_piece.replace_by([])
+            else:
+                piece_to_replace = old_piece
+
             for i, new_tet in enumerate(new_tets):
                 new_start_subsimplex = old_to_new_tets[j][i].image(start_subsimplex)
 
                 if new_start_subsimplex == new_face:
                     _retrace_geodesic_piece(
-                        old_piece,
+                        piece_to_replace,
                         new_tets,
                         new_tet,
                         new_face,
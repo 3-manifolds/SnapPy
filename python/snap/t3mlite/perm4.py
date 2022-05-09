de585f3b15af11adefa1480283082207498d7156
Frédéric Chapoton <chapoton@unistra.fr>
2022-05-09 08:07:55 +0200

    fix in perm4

--- python/snap/t3mlite/perm4.py
+++ python/snap/t3mlite/perm4.py
@@ -12,7 +12,7 @@ def _make_opp_dict():
     def swap(t):
         return (t[1], t[0])
     dic = {(0, 1): (2, 3), (2, 0): (1, 3), (1, 2): (0, 3)}
-    for k in dic:
+    for k in list(dic):
         dic[dic[k]] = k
         dic[swap(dic[k])] = swap(k)
         dic[swap(k)] = swap(dic[k])

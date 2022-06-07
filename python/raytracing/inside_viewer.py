e16483ac4d7496a6a133559777d4b132970d91d0
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2022-06-06 22:03:19 -0500

    Breaking long line.

--- python/raytracing/inside_viewer.py
+++ python/raytracing/inside_viewer.py
@@ -179,7 +179,13 @@ class InsideViewer(ttk.Frame):
                 row = row,
                 update_function = self.widget.recompute_raytracing_data_and_redraw,
                 index = i)
-            cusp_button = ttk.Button(frame, text = 'View', command = lambda which_cusp = i: self.set_camera_cusp_view(which_cusp), takefocus = 0)
+            cusp_button = ttk.Button(
+                frame,
+                text = 'View',
+                takefocus = 0,
+                command = (
+                    lambda which_cusp = i:
+                        self.set_camera_cusp_view(which_cusp)))
             cusp_button.grid(row = row, column = 3)
             cusp_view_buttons.append(cusp_button)
             row += 1

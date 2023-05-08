ed6276ad6358cff1faa35305f8a7840b21312d2c
Nathan Dunfield <nathan@dunfield.info>
2023-05-08 09:42:02 -0400

    Some additional input guards for hyperbolic_torsion

--- python/snap/nsagetools.py
+++ python/snap/nsagetools.py
@@ -466,29 +466,6 @@ def first_square_submatrix(A):
 class TorsionComputationError(Exception):
     pass
 
-
-@sage_method
-def hyperbolic_torsion(manifold, bits_prec=100, all_lifts=False, wada_conventions=False, phi=None):
-    """
-    Computes the hyperbolic torsion polynomial as defined in
-    `[DFJ] <http://arxiv.org/abs/1108.3045>`_::
-
-        sage: M = Manifold('K11n42')
-        sage: M.alexander_polynomial()
-        1
-        sage: tau = M.hyperbolic_torsion(bits_prec=200)
-        sage: tau.degree()
-        6
-    """
-
-    G = alpha = polished_holonomy(manifold, bits_prec=bits_prec, lift_to_SL2=True)
-    if not all_lifts:
-        return compute_torsion(G, bits_prec, alpha, phi, wada_conventions=wada_conventions)
-    else:
-        return [compute_torsion(G, bits_prec, beta, phi, wada_conventions=wada_conventions)
-                for beta in alpha.all_lifts_to_SL2C()]
-
-
 def fast_determinant_of_laurent_poly_matrix(A):
     """
     Return the determinant of the given matrix up to
@@ -654,6 +631,33 @@ def test_rep(G, phialpha):
 
 
 @sage_method
+def hyperbolic_torsion(manifold, bits_prec=100, all_lifts=False, wada_conventions=False, phi=None):
+    """
+    Computes the hyperbolic torsion polynomial as defined in
+    `[DFJ] <http://arxiv.org/abs/1108.3045>`_::
+
+        sage: M = Manifold('K11n42')
+        sage: M.alexander_polynomial()
+        1
+        sage: tau = M.hyperbolic_torsion(bits_prec=200)
+        sage: tau.degree()
+        6
+    """
+    if manifold.homology().betti_number() != 1:
+        raise ValueError('Algorithm needs H^1(M; Z) = Z to be able to compute torsion')
+    H = manifold.fundamental_group()
+    if H.num_generators() != H.num_relators() + 1:
+        raise ValueError('Algorithm to compute torsion requires a group presentation '
+                         'with deficiency one')
+    G = alpha = polished_holonomy(manifold, bits_prec=bits_prec, lift_to_SL2=True)
+    if not all_lifts:
+        return compute_torsion(G, bits_prec, alpha, phi, wada_conventions=wada_conventions)
+    else:
+        return [compute_torsion(G, bits_prec, beta, phi, wada_conventions=wada_conventions)
+                for beta in alpha.all_lifts_to_SL2C()]
+
+
+@sage_method
 def hyperbolic_SLN_torsion(manifold, N, bits_prec=100):
     """
     Compute the torsion polynomial of the holonomy representation lifted
@@ -664,6 +668,12 @@ def hyperbolic_SLN_torsion(manifold, N, bits_prec=100):
         sage: [M.hyperbolic_SLN_torsion(N).degree() for N in [2, 3, 4]]
         [18, 27, 36]
     """
+    if manifold.homology().betti_number() != 1:
+        raise ValueError('Algorithm needs H^1(M; Z) = Z to be able to compute torsion')
+    H = manifold.fundamental_group()
+    if H.num_generators() != H.num_relators() + 1:
+        raise ValueError('Algorithm to compute torsion requires a group presentation '
+                         'with deficiency one')
     G = alpha = polished_holonomy(manifold, bits_prec)
     phi = MapToGroupRingOfFreeAbelianization(G, alpha('a').base_ring())
     phialpha = PhiAlphaN(phi, alpha, N)

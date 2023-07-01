8f68a457fdd4d0cd4e6dbe2352424000dbecc8d4
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2023-07-01 16:48:29 -0700

    Drilling and dict: removing unnecessary redirection.

--- python/drilling/spatial_dict.py
+++ python/drilling/spatial_dict.py
@@ -48,18 +48,6 @@ def floor_as_integers(x) -> Sequence[int]:
         return [ int_f ]
 
 
-class _Entry:
-    """
-    A helper for SpatialDict.
-
-    The implementation of SpatialDict has the same instance of _Entry
-    stored for multiple keys so that updating the value for all keys
-    can be done by assigning the new value to _Entry.value only once.
-    """
-    def __init__(self, value):
-        self.value = value
-
-
 class SpatialDict:
     """
     A python dict-like object appropriate for using numerical points (e.g.,
@@ -93,10 +81,10 @@ class SpatialDict:
         reps_and_ikeys = self._representatives_and_ikeys(point)
 
         for rep, ikey in reps_and_ikeys:
-            for other_rep, entry in self._data.get(ikey, []):
+            for other_rep, value in self._data.get(ikey, []):
                 d = self.distance(rep, other_rep)
                 if d < self._right_distance_value:
-                    return entry.value
+                    return value
                 if not (self._left_distance_value < d):
                     raise InsufficientPrecisionError(
                         "Could neither verify that the two given tiles are "
@@ -105,9 +93,8 @@ class SpatialDict:
                         "Injectivty diameter about basepoint is: %r." % (
                             d, self._min_distance))
 
-        entry = _Entry(default)
         for rep, ikey in reps_and_ikeys:
-            self._data.setdefault(ikey, []).append((rep, entry))
+            self._data.setdefault(ikey, []).append((rep, default))
 
         return default
 

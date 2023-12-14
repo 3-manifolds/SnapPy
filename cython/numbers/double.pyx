30c8cffe09d12f9936c7c6295769b25f18b1c073
Marc Culler <culler@users.noreply.github.com>
2023-12-13 19:48:40 -0600

    Remove DEF HIGH_PRECISION and its associated IF directives.

---
+++ cython/numbers/double.pyx
@@ -0,0 +1,29 @@
+from .number import Number as RawNumber
+
+class Number(RawNumber):
+    _default_precision=53
+
+cdef Real2gen_direct(Real R):
+    """
+    Convert a Real to a pari gen of type t_REAL.
+    """
+    cdef double* qd = <double*>&R
+    return pari(qd[0])
+
+cdef Real2gen_string(Real R):
+    """
+    Convert a Real to a pari gen of type t_REAL.
+    This constructs the gen from the string representation of the real.
+    """
+    return pari(real_to_string(R))
+
+cdef Complex gen2Complex(g):
+    cdef Complex result
+    result.real, result.imag = g.real(), g.imag()
+    return result
+
+cdef Real2Number(Real R):
+    return Number(Real2gen(R))
+cdef Complex2Number(Complex C):
+    return Number(Complex2gen(C))
+

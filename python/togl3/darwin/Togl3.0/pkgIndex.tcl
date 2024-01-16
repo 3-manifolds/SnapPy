5a3f701db870ecc6646a8a6ca4dba0b937a3df5e
Marc Culler <culler@users.noreply.github.com>
2024-01-16 17:27:44 -0600

    Add togl3 for mac

--- python/togl3/darwin/Togl3.0/pkgIndex.tcl
+++ python/togl3/darwin/Togl3.0/pkgIndex.tcl
@@ -3,8 +3,8 @@
 #
 if {[package vsatisfies [package provide Tcl] 9.0-]} {
     package ifneeded Togl 3.0 \
-	    [list load [file join $dir libtcl9Togl3.0.so] [string totitle Togl]]
+	    [list load [file join $dir libtcl9Togl3.0.dylib] [string totitle Togl]]
 } else {
     package ifneeded Togl 3.0 \
-	    [list load [file join $dir libTogl3.0.so] [string totitle Togl]]
+	    [list load [file join $dir libTogl3.0.dylib] [string totitle Togl]]
 }

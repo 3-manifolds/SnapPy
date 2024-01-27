2aa8a9c61fe5e75a56bfe325fae0cb9a09b09fd5
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2024-01-27 13:22:18 -0800

    OpenGL: Starting to break up legacy classes into their own pyx files.

---
+++ opengl/legacy/orthoWidget.pyx
@@ -0,0 +1,28 @@
+class OpenGLOrthoWidget(OpenGLPerspectiveWidget):
+    """
+    A version of the widget that uses orthographic projection instead
+    of perspective.
+    """
+
+    def build_projection(self, width, height, t=1.0):
+        aspect = float(width)/float(height)
+        top = self.fovy/2
+        right = top*aspect
+        self.make_current()
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        if self.flipped:
+            glEnable(GL_LIGHT1)
+            glDisable(GL_LIGHT0)
+            glOrtho(-right, right, top, -top, 3.0, -3.0)
+        else:
+            glEnable(GL_LIGHT0)
+            glDisable(GL_LIGHT1)
+            glOrtho(-right, right, -top, top, -3.0, 3.0)
+
+    def tkTranslate(self, event):
+        """
+        Perform translation of scene.
+        """
+        self.redraw_if_initialized()
+        self.tkRecordMouse(event)

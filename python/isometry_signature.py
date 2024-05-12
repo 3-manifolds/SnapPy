635a02ca9d534827d10c89668ce93fa1eb44d527
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2024-05-12 00:19:48 -0700

    Extending isometry signature to closed manifolds.

--- python/isometry_signature.py
+++ python/isometry_signature.py
@@ -1,4 +1,9 @@
+from . import Triangulation
 from . import verify
+from .sage_helper import _within_sage
+from .math_basics import is_RealIntervalFieldElement
+from .exceptions import InsufficientPrecisionError
+from .geometric_structure.geodesic.exceptions import WordAppearsToBeParabolic
 
 def isometry_signature(
     manifold, of_link=False, verified=False,
@@ -47,14 +52,40 @@ def isometry_signature(
         sage: M.isometry_signature(verified = True, exact_bits_prec_and_degrees = None)
 
     """
-    if False in manifold.cusp_info('complete?'):
+
+    if any(manifold.cusp_info('complete?')):
+        return isometry_signature_cusped(
+            manifold,
+            of_link=of_link,
+            verified=verified,
+            interval_bits_precs=interval_bits_precs,
+            exact_bits_prec_and_degrees=exact_bits_prec_and_degrees,
+            verbose=verbose)
+    else:
+        return isometry_signature_closed(
+            manifold,
+            verified=verified,
+            interval_bits_precs=interval_bits_precs,
+            exact_bits_prec_and_degrees=exact_bits_prec_and_degrees,
+            verbose=verbose)
+
         raise ValueError('isometry_signature needs all cusps to be complete')
 
+def isometry_signature_cusped(
+        manifold, *,
+        of_link,
+        verified,
+        interval_bits_precs,
+        exact_bits_prec_and_degrees,
+        verbose):
+    if not all(manifold.cusp_info('complete?')):
+        manifold = manifold.filled_triangulation()
+
     retrig = manifold.canonical_retriangulation(
-         verified=verified,
-         interval_bits_precs=interval_bits_precs,
-         exact_bits_prec_and_degrees=exact_bits_prec_and_degrees,
-         verbose=verbose)
+        verified=verified,
+        interval_bits_precs=interval_bits_precs,
+        exact_bits_prec_and_degrees=exact_bits_prec_and_degrees,
+        verbose=verbose)
 
     if not retrig:
         return None
@@ -63,3 +94,177 @@ def isometry_signature(
                                        ignore_cusp_ordering=True,
                                        ignore_curve_orientations=True)
 
+def isometry_signature_closed(
+        manifold, *,
+        verified,
+        interval_bits_precs,
+        exact_bits_prec_and_degrees,
+        verbose):
+
+    if verbose:
+        print("Step 1: Finding shortest geodesics")
+
+    shortest_geodesics = find_shortest_geodesics_precisions(
+        manifold,
+        bits_precs=interval_bits_precs,
+        verified=verified,
+        verbose=verbose)
+    if shortest_geodesics is None:
+        return None
+
+    if verbose:
+        print("Step 2: Drill each geodesic for potential isometry signatures")
+
+    potential_signatures = []
+
+    for shortest_geodesic in shortest_geodesics:
+        if verbose:
+            print("Drilling ", shortest_geodesic)
+
+        drilled_manifold = drill_manifold_precisions(
+            manifold, shortest_geodesic,
+            bits_precs=interval_bits_precs,
+            verified=verified,
+            verbose=verbose)
+
+        if drilled_manifold is None:
+            return None
+
+        if not all(drilled_manifold.cusp_info('complete?')):
+            drilled_manifold = drilled_manifold.filled_triangulation()
+
+        if verbose:
+            print("Computing isometry signature of drilled manifold")
+
+        retrig = drilled_manifold.canonical_retriangulation(
+            verified=verified,
+            interval_bits_precs=interval_bits_precs,
+            exact_bits_prec_and_degrees=exact_bits_prec_and_degrees,
+            verbose=verbose)
+
+        if retrig is None:
+            return None
+
+        isosig = retrig.triangulation_isosig(decorated=False)
+
+        for slope in compute_meridian_slopes(isosig, retrig):
+            sig = (isosig, slope)
+            if verbose:
+                print("Potential isometry signature %s%r" % sig)
+            potential_signatures.append(sig)
+
+    isosig, (m, l) = min(potential_signatures, key=signature_key)
+
+    return '%s(%d,%d)' % (isosig, m, l)
+
+def find_shortest_geodesics_precisions(
+        manifold, *, bits_precs, verified, verbose):
+    for bits_prec in bits_precs:
+        if verbose:
+            print("Using precision %d to find shortest geodesics" % bits_prec)
+        try:
+            return find_shortest_geodesics(
+                manifold,
+                bits_prec=bits_prec,
+                verified=verified,
+                verbose=verbose)
+        except (InsufficientPrecisionError, ValueError):
+            pass
+    return None
+
+def find_shortest_geodesics(manifold, *, bits_prec, verified, verbose):
+    length_spectrum = manifold.length_spectrum_iter(
+        bits_prec=bits_prec, verified=verified)
+
+    is_first = True
+
+    words_to_drill = []
+
+    for geodesic in length_spectrum:
+        if is_first:
+            systole = geodesic.length.real()
+            cutoff = compute_cutoff(systole)
+            is_first = False
+            if verbose:
+                print("Systole: ", systole)
+                print("Cutoff for shortest geodesics: ", cutoff)
+
+        r = geodesic.length.real()
+
+        if verbose:
+            print("Word: ", geodesic.word)
+            print("Geodesic length: ", r)
+
+        if r > cutoff:
+            break
+
+        if r < cutoff:
+            if verbose:
+                print("Adding word to candidates")
+            words_to_drill.append(geodesic.word)
+            continue
+
+        raise InsufficientPrecisionError(
+            "Could not determine whether geodesic length is "
+            "less or greater than cutoff.\n"
+            "Cutoff: %r\n"
+            "Length: %r\n" % (cutoff, r))
+
+    return words_to_drill
+
+_cutoff_digits = 16
+
+def compute_cutoff(systole):
+    RF = systole.parent()
+
+    if _within_sage:
+        l = systole.log2()
+    else:
+        l = systole.log() / RF(2).log()
+
+    f = l.floor()
+
+    if is_RealIntervalFieldElement(l):
+        is_int, f_int = f.is_int()
+        if not is_int:
+            raise Exception("Not an integer")
+    else:
+        f_int = f
+
+    return systole + RF(2) ** (f_int - _cutoff_digits)
+
+def drill_manifold_precisions(
+        manifold, word, *,
+        bits_precs, verified, verbose):
+    for bits_prec in bits_precs:
+        try:
+            return manifold.drill_word(
+                word,
+                bits_prec=bits_prec,
+                verified=verified,
+                verbose=verbose)
+        except (InsufficientPrecisionError, ValueError, WordAppearsToBeParabolic):
+            pass
+
+    return None
+
+def compute_meridian_slopes(isosig, tri):
+    isosig_tri = Triangulation(isosig, remove_finite_vertices=False)
+    for iso in tri.isomorphisms_to(isosig_tri):
+        cusp_map, = iso.cusp_maps()
+        m = cusp_map[0,0]
+        l = cusp_map[1,0]
+
+        if l < 0:
+            yield (-m, -l)
+        elif l > 0:
+            yield ( m,  l)
+        elif m < 0:
+            yield (-m, -l)
+        else:
+            yield ( m,  l)
+
+def signature_key(signature):
+    isosig, (m, l) = signature
+
+    return (isosig, l, abs(m), -m)

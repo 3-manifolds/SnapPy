c1cfe6c754e1b690f9751726c5d0dd5217399d0d
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2024-07-17 00:26:04 -0700

    More test update

--- python/verify/cusp_areas.py
+++ python/verify/cusp_areas.py
@@ -18,8 +18,8 @@ def unbiased_cusp_areas_from_cusp_area_matrix(cusp_area_matrix):
         >>> def N(x): return number_to_native_number(Number(x))
         >>> from snappy.matrix import make_matrix
         >>> unbiased_cusp_areas_from_cusp_area_matrix(
-        ...             matrix([[N(10.0), N(40.0)],
-        ...                     [N(40.0), N(20.0)]])) # doctest: +NUMERIC9
+        ...             make_matrix([[N(10.0), N(40.0)],
+        ...                          [N(40.0), N(20.0)]])) # doctest: +NUMERIC9
         [3.1622776601683795, 4.47213595499958]
 
     """

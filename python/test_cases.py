3-manifolds/SnapPy

commit 20e7cc24084309f083b0f26cca58515b993ab6bc

Fixing a bug in isometry_signature coming from Triangulation vs TriangulationLP.

python/isometry_signature.py
@@ -1,4 +1,4 @@
-from . import Triangulation
+from . import Triangulation, TriangulationHP, ManifoldHP
 from . import verify
 from .sage_helper import _within_sage
 from .math_basics import is_RealIntervalFieldElement
@@ -264,7 +264,12 @@ def drill_manifold_precisions(
     raise err
 
 def compute_meridian_slopes(isosig, tri):
-    isosig_tri = Triangulation(isosig, remove_finite_vertices=False)
+    if isinstance(tri, TriangulationHP) or isinstance(tri, ManifoldHP):
+        isosig_tri = TriangulationHP(
+            isosig, remove_finite_vertices=False)
+    else:
+        isosig_tri = Triangulation(
+            isosig, remove_finite_vertices=False)
     for iso in tri.isomorphisms_to(isosig_tri):
         cusp_map, = iso.cusp_maps()
         m = cusp_map[0,0]

commit 130a946bf29dab21b566061512b27e36f4e95b34

An alternative fix to commit 20e7cc240 caused by trying to compare low-precision and high-precision structure.
Introducing helper functions that demote or promote Triangulation(HP) or Manifold(HP), respectively.

Using them to implement isomorphisms_to and is_isometric_to.

cython/core/manifold.pyx
@@ -1527,7 +1527,9 @@ cdef class Manifold(Triangulation):
             result.set_c_triangulation(c_triangulation)
             return result
 
-    def is_isometric_to(self, Manifold other, return_isometries=False):
+    def _is_isometric_to(self,
+                         Manifold other,
+                         return_isometries : bool = False):
         """
         Returns True if M and N are isometric, False if they not.  A
         RuntimeError is raised in cases where the SnapPea kernel fails
@@ -1611,6 +1613,15 @@ cdef class Manifold(Triangulation):
 
         return ans
 
+    def is_isometric_to(self,
+                        Manifold other,
+                        return_isometries : bool = False):
+        """
+        This is only here to make the tests that use SnapPy.Manifold work."
+        """
+        return self._is_isometric_to(
+            other, return_isometries = return_isometries)
+        
     def is_two_bridge(self) -> bool:
         """
         If the manifold is the complement of a two-bridge knot or link

cython/core/triangulation.pyx
@@ -966,7 +966,7 @@ cdef class Triangulation():
         free_triangulation_data(data)
         return result
 
-    def isomorphisms_to(self, Triangulation other not None):
+    def _isomorphisms_to(self, Triangulation other not None):
         """
         Returns a complete list of combinatorial isomorphisms between
         the two triangulations:
@@ -1003,6 +1003,12 @@ cdef class Triangulation():
             result = IsometryListToIsometries(isometries)
         free_isometry_list(isometries)
         return result
+
+    def isomorphisms_to(self, Triangulation other not None):
+        """
+        This is only here to make the tests that use SnapPy.Triangulation work."
+        """
+        return self._isomorphisms_to(other)
 
     def __dealloc__(self):
         if self.c_triangulation is not NULL:

python/__init__.py
@@ -30,6 +30,8 @@ from .exceptions import (SnapPeaFatalError,
                          InsufficientPrecisionError,
                          NonorientableManifoldError)
 
+from typing import Union, Tuple
+
 # Subclass to be able to monkey-patch
 class Triangulation(_TriangulationLP):
     __doc__ = _TriangulationLP.__doc__
@@ -98,13 +100,6 @@ class Manifold(_ManifoldLP, Triangulation):
     def low_precision(self):
         return self.copy()
 
-    def is_isometric_to(self, other, return_isometries=False):
-        __doc__ = _ManifoldLP.is_isometric_to.__doc__
-        if other.__class__ is ManifoldHP:
-            return _ManifoldHP.is_isometric_to(self.high_precision(), other,
-                                                  return_isometries)
-        return _ManifoldLP.is_isometric_to(self, other, return_isometries)
-
 # We want ManifoldHP to be a subclass of TriangulationHP.
 # See comment about Manifold and the diamond pattern.
 class ManifoldHP(_ManifoldHP, TriangulationHP):
@@ -134,13 +129,6 @@ class ManifoldHP(_ManifoldHP, TriangulationHP):
 
     def high_precision(self):
         return self.copy()
-
-    def is_isometric_to(self, other, return_isometries=False):
-        __doc__ = _ManifoldHP.is_isometric_to.__doc__
-        if other.__class__ is Manifold:
-            return _ManifoldHP.is_isometric_to(self, other.high_precision(),
-                                                   return_isometries)
-        return _ManifoldHP.is_isometric_to(self, other, return_isometries)
 
     def identify(self, extends_to_link=False):
         """
@@ -183,6 +171,73 @@ __all__ = ['Triangulation', 'Manifold', 'ManifoldHP', 'AbelianGroup',
            'NonalternatingKnotExteriors', 'SnapPeaFatalError',
            'InsufficientPrecisionError',
            'pari', 'twister', ]
+
+def _symmetrize_high_precision_manifold(
+        mfd1 : Union[Manifold, ManifoldHP],
+        mfd2 : Union[Manifold, ManifoldHP]
+              ) -> Union[Tuple[Manifold,   Manifold],
+                         Tuple[ManifoldHP, ManifoldHP]]:
+    """
+    Given a (potential) mix of two Manifold and ManifoldHP,
+    promote one to high precision if necessary and return
+    the result as pair.
+    """
+    resolved_mfd1 = mfd1
+    resolved_mfd2 = mfd2
+    high1 = isinstance(mfd1, ManifoldHP)
+    high2 = isinstance(mfd2, ManifoldHP)
+    if high1 and not high2:
+        resolved_mfd2 = ManifoldHP(mfd2)
+    if high2 and not high1:
+        resolved_mfd1 = ManifoldHP(mfd1)
+    return (resolved_mfd1, resolved_mfd2)
+
+def _symmetrize_low_precision_triangulation(
+        tri1 : Union[Triangulation, TriangulationHP],
+        tri2 : Union[Triangulation, TriangulationHP]
+              ) -> Union[Tuple[Triangulation,   Triangulation],
+                         Tuple[TriangulationHP, TriangulationHP]]:
+    """
+    Given a (potential) mix of two Triangulation and TriangulationHP,
+    demote one to low precision if necessary and return
+    the result as pair.
+    """
+    resolved_tri1 = tri1
+    resolved_tri2 = tri2
+    low1 = isinstance(tri1, Triangulation)
+    low2 = isinstance(tri2, Triangulation)
+    if low1 and not low2:
+        resolved_tri2 = Triangulation(tri2, remove_finite_vertices=False)
+    if low2 and not low1:
+        resolved_tri1 = Triangulation(tri1, remove_finite_vertices=False)
+    return (resolved_tri1, resolved_tri2)
+
+def is_isometric_to(self,
+                    other : Union[Manifold, ManifoldHP],
+                    return_isometries : bool = False):
+    resolved_self, resolved_other = (
+        _symmetrize_high_precision_manifold(
+            self, other))
+
+    return resolved_self._is_isometric_to(
+        resolved_other,
+        return_isometries=return_isometries)
+
+is_isometric_to.__doc__ = _ManifoldLP._is_isometric_to.__doc__
+Manifold.is_isometric_to = is_isometric_to
+ManifoldHP.is_isometric_to = is_isometric_to
+
+def isomorphisms_to(self, other):
+    resolved_self, resolved_other = (
+        _symmetrize_low_precision_triangulation(
+            self, other))
+
+    return resolved_self._isomorphisms_to(
+        resolved_other)
+
+isomorphisms_to.__doc__ = _TriangulationLP._isomorphisms_to.__doc__
+Triangulation.isomorphisms_to = isomorphisms_to
+TriangulationHP.isomorphisms_to = isomorphisms_to
 
 from . import snap
 snap.add_methods(Manifold)

python/isometry_signature.py
@@ -264,12 +264,7 @@ def drill_manifold_precisions(
     raise err
 
 def compute_meridian_slopes(isosig, tri):
-    if isinstance(tri, TriangulationHP) or isinstance(tri, ManifoldHP):
-        isosig_tri = TriangulationHP(
-            isosig, remove_finite_vertices=False)
-    else:
-        isosig_tri = Triangulation(
-            isosig, remove_finite_vertices=False)
+    isosig_tri = Triangulation(isosig, remove_finite_vertices=False)
     for iso in tri.isomorphisms_to(isosig_tri):
         cusp_map, = iso.cusp_maps()
         m = cusp_map[0,0]

python/test_cases.py
@@ -46,6 +46,56 @@ True
 
 >>> isinstance(ManifoldHP("m004"), TriangulationHP)
 True
+
+Low precision and high precision comparisons
+--------------------------------------------
+>>> M   = Manifold("m004")
+>>> Mhp = Manifold("m004")
+>>> N   = Manifold("m003")
+>>> Nhp = Manifold("m003")
+>>> M.is_isometric_to(M)
+True
+>>> M.is_isometric_to(Mhp)
+True
+>>> Mhp.is_isometric_to(M)
+True
+>>> Mhp.is_isometric_to(Mhp)
+True
+>>> M.is_isometric_to(N)
+False
+>>> M.is_isometric_to(Nhp)
+False
+>>> Mhp.is_isometric_to(N)
+False
+>>> Mhp.is_isometric_to(Nhp)
+False
+
+>>> O = Triangulation("mvvLALQQQhfghjjlilkjklaaaaaffffffff",
+... remove_finite_vertices = False)
+>>> O.has_finite_vertices()
+True
+>>> Ohp = TriangulationHP("mvvLALQQQhfghjjlilkjklaaaaaffffffff",
+... remove_finite_vertices = False)
+>>> Ohp.has_finite_vertices()
+True
+
+>>> len(O.isomorphisms_to(O))
+8
+>>> len(O.isomorphisms_to(Ohp))
+8
+>>> len(Ohp.isomorphisms_to(O))
+8
+>>> len(Ohp.isomorphisms_to(Ohp))
+8
+>>> len(M.isomorphisms_to(O))
+0
+>>> len(M.isomorphisms_to(Ohp))
+0
+>>> len(Mhp.isomorphisms_to(O))
+0
+>>> len(Mhp.isomorphisms_to(Ohp))
+0
+
 """
 
 if not __doc__:
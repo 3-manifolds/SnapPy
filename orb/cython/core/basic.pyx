508b31c835f6f18074305b5c268f126946c65e24
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2025-08-27 11:58:50 -0700

    Adding another missing file

---
+++ orb/cython/core/basic.pyx
@@ -0,0 +1,11 @@
+SolutionType = [
+    'not attempted',
+    'all tetrahedra positively oriented (Note that list_interface.cpp still checks that contains_flat_tetrahedra is false)',
+    'contains negatively oriented tetrahedra',
+    'contains flat tetrahedra',
+    'contains degenerate tetrahedra',
+    'unrecognized solution type',
+    'no solution found',
+    'Step failed (See my_hyperbolic_structure.c for interpretation)',
+    'Invalid solution (See my_hyperbolic_structure.c for interpretation)'
+]

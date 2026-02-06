24aee766c44775045e0acd71434bf6b90abac783
Shana <47409207+Shakugannotorch@users.noreply.github.com>
2026-02-05 21:27:10 -0800

    Add snappy_11_tets and snappy_16_knots to known packages (#142)

--- python/__init__.py
+++ python/__init__.py
@@ -436,7 +436,9 @@ snappy_module = sys.modules[__name__]
 database_objects = []
 known_manifold_packages = [('snappy_manifolds', True),
                            ('snappy_15_knots', False),
-                           ('nonexistent_manifolds', False)]
+                           ('nonexistent_manifolds', False),
+                           ('snappy_11_tets', False),
+                           ('snappy_16_knots', False)]
 
 for manifold_package, required in known_manifold_packages:
     table_dict = database.add_tables_from_package(manifold_package, required)
--- python/version.py
+++ python/version.py
@@ -1,2 +1,2 @@
-version = '3.3'
+version = '3.3.1b1'
 release_date = 'January 2026'
